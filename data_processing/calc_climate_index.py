078b28b8e07f0af0389f9b3dbb8ce19cf15a5b93
Damien Irving <d.irving@student.unimelb.edu.au>
2012-07-23 16:31:36 +1000

    Altered folder structure

---
+++ data_processing/calc_climate_index.py
@@ -0,0 +1,416 @@
+#!/usr/bin/env cdat
+
+"""
+GIT INFO: $Id$
+Filename:     calc_climate_index.py
+Author:       Damien Irving, d.irving@student.unimelb.edu.au
+Description:  Calculates the selected climate index
+
+Input:        List of netCDF files to plot
+Output:       Text file
+
+Updates | By | Description
+--------+----+------------
+23 February 2012 | Damien Irving | Initial version.
+
+
+Copyright CSIRO, 2012
+"""
+
+
+__version__= '$Revision$'   ## Get this working for Git
+
+
+### Import required modules ###
+
+import optparse
+from optparse import OptionParser
+import re
+import os
+import sys
+
+import cdms2 
+import genutil
+
+import numpy
+import numpy.ma as ma
+import math
+
+from datetime import datetime
+
+
+## Define global variables ##
+
+NINO1 = cdms2.selectors.Selector(latitude=(-10,-5,'cc'),longitude=(270,280,'cc'))   # the 'cc' means a closed interval [] e.g. [260,270]
+NINO2 = cdms2.selectors.Selector(latitude=(-5,0,'cc'),longitude=(270,280,'cc'))
+NINO12 = cdms2.selectors.Selector(latitude=(-10,0,'cc'),longitude=(270,280,'cc'))
+NINO3 = cdms2.selectors.Selector(latitude=(-5,5,'cc'),longitude=(210,270,'cc'))
+NINO34 = cdms2.selectors.Selector(latitude=(-5,5,'cc'),longitude=(190,240,'cc'))
+NINO4 = cdms2.selectors.Selector(latitude=(-5,5,'cc'),longitude=(160,210,'cc'))
+EMI_A = cdms2.selectors.Selector(latitude=(-10,10,'cc'),longitude=(165,220,'cc'))
+EMI_B = cdms2.selectors.Selector(latitude=(-15,5,'cc'),longitude=(250,290,'cc'))
+EMI_C = cdms2.selectors.Selector(latitude=(-10,20,'cc'),longitude=(125,145,'cc'))
+
+
+version_info = 'Created %s using %s \n' %(datetime.utcnow().isoformat(),__file__) #,__version__)  #%(datetime.utcnow().isoformat(), sys.argv[0])
+
+
+## Define relevant functions and classes ##
+
+
+class InputFile(object):
+    patterns = [
+        ('climate_index',
+         True,
+         re.compile(r'(?P<variable_name>\S+)_(?P<dataset>\S+)_(?P<level>\S+)_(?P<timescale>\S+)_(?P<grid>\S+).nc')),        
+    ]
+    def __init__(self, fname):
+        self.fname = fname
+        self.atts = {}
+
+        basename = os.path.basename(fname)
+        for name, isfile, pattern in InputFile.patterns:
+            m = pattern.match(basename)
+            if m:
+                self.atts = m.groupdict()
+                self.type = name
+                self.isfile = isfile
+                break
+        if not self.atts:
+           raise ValueError("Unknown file type: " + fname)
+
+    def __repr__(self):
+        return self.fname
+
+    def __getattr__(self, key):
+        try:
+            return super(InputFile, self).__getattr__(key)
+        except AttributeError:
+            if key in self.atts:
+                return self.atts[key]
+            else:
+                raise
+
+
+def define_region(region_name):
+    
+    # map string region to selector object
+    if isinstance(region_name, basestring):
+        if region_name in globals():
+            region = globals()[region_name]
+	else:
+            print "Region is not defined in code"
+            sys.exit(1)
+    
+    for selector_component in region.components():
+        if selector_component.id == 'lat':
+            minlat = selector_component.spec[0]
+            maxlat = selector_component.spec[1]
+        elif selector_component.id == 'lon':
+            minlon = selector_component.spec[0]
+            maxlon = selector_component.spec[1]
+
+    return region,minlat,maxlat,minlon,maxlon
+
+def calc_monthly_climatology(base_timeseries):
+    ntime_base = len(base_timeseries)
+    monthly_climatology_mean = numpy.zeros(12)
+    monthly_climatology_std = numpy.zeros(12)
+    for i in range(0,12):
+        monthly_climatology_mean[i] = numpy.mean(base_timeseries[i:ntime_base:12])
+	monthly_climatology_std[i] = numpy.std(base_timeseries[i:ntime_base:12])
+
+    return monthly_climatology_mean, monthly_climatology_std
+
+
+def calc_monthly_anomaly(complete_timeseries,base_timeseries,months):
+    """Calculates the monthly anomaly"""  # assumes that the base_timeseries starts in January
+    
+    # Calculate the monthly climatology #
+    
+    monthly_climatology_mean, monthly_climatology_std = calc_monthly_climatology(base_timeseries)
+    
+    # Calculate the monthly anomaly #
+    
+    ntime_complete = len(complete_timeseries)
+    monthly_anomaly = numpy.zeros(ntime_complete)
+    for i in range(0,ntime_complete):
+	month_index = months[i]
+	monthly_anomaly[i] = complete_timeseries[i] - monthly_climatology_mean[month_index-1]
+    
+    
+    return monthly_anomaly 
+
+
+def monthly_normalisation(complete_timeseries,base_timeseries,months):
+    """Calculates the monthly anomaly"""  # assumes that the base_timeseries starts in January
+    
+    # Calculate the monthly climatology #
+    
+    monthly_climatology_mean, monthly_climatology_std = calc_monthly_climatology(base_timeseries)
+
+    # Normalise the entire timeseries #
+    
+    ntime_complete = len(complete_timeseries)
+    monthly_normalised = numpy.zeros(ntime_complete)
+    for i in range(0,ntime_complete):
+	month_index = months[i]
+	monthly_normalised[i] = (complete_timeseries[i] - monthly_climatology_mean[month_index-1]) / monthly_climatology_std[month_index-1]
+    
+    
+    return monthly_normalised
+
+
+def calc_SAM(index,ifile,outfile_name):
+    """Calculates an index of the Southern Annular Mode"""
+    
+    # Read the input file #
+
+    try:
+        fin=cdms2.open(ifile.fname,'r')
+    except cdms2.CDMSError:
+        print 'Unable to open file: ', ifile
+	sys.exit(0)
+    
+    # Get the time axis information #
+    
+    time = fin.getAxis('time').asComponentTime()
+    years = []
+    months = []
+    
+    for ii in range(0,len(time)):
+        years.append(int(str(time[ii]).split('-')[0]))
+	months.append(int(str(time[ii]).split('-')[1]))
+    
+    # Read data, extract the required latitudes, calculate zonal mean anomalies #
+        
+    latitude = fin.getAxis('latitude')
+    var_complete=fin(ifile.variable_name,order='tyx')
+    var_base=fin(ifile.variable_name,time=('1981-01-01','2010-12-31'),order='tyx')
+    
+    lats = [-40,-65]
+    monthly_normalised_timeseries = {}
+    
+    for lat in lats: 
+    
+	index, value = min(enumerate(latitude), key=lambda x: abs(x[1]-float(lat)))
+	print 'File latitude for', lat, '=', value
+
+	complete_timeseries = numpy.mean(var_complete[:,index,:],axis=1)
+	base_timeseries = numpy.mean(var_base[:,index,:],axis=1)
+
+        monthly_normalised_timeseries[lat] = monthly_normalisation(complete_timeseries,base_timeseries,months)
+
+    
+    SAMI_timeseries = monthly_normalised_timeseries[-40] - monthly_normalised_timeseries[-65]
+
+
+    # Write the text file output #
+
+    fout = open(outfile_name,'w')
+    fout.write('SAM Index (as per Marshall 2003 and Gong & Wang 1999) \n')
+    fout.write('Base period = 1981-2010 \n')  
+    fout.write(version_info)
+    fout.write('Input file = '+ifile.fname+'\n')
+    fout.write(' YR   MON  SAMI \n') 
+
+    for ii in range(0,len(time)):
+        print >> fout, '%4i %3i %7.2f' %(years[ii],months[ii],SAMI_timeseries[ii])
+    
+    fout.close()
+
+
+def calc_IEMI(index,ifile,outfile_name):
+    """Calculates the Improved ENSO Modoki Index"""
+    
+    # Read the input file #
+
+    try:
+        fin=cdms2.open(ifile.fname,'r')
+    except cdms2.CDMSError:
+        print 'Unable to open file: ', ifile
+	sys.exit(0)
+    
+    # Get the time axis information #
+    
+    time = fin.getAxis('time').asComponentTime()
+    years = []
+    months = []
+    
+    for ii in range(0,len(time)):
+        years.append(int(str(time[ii]).split('-')[0]))
+	months.append(int(str(time[ii]).split('-')[1]))
+    
+    # Calculate the index #
+    
+    regions = ['EMI_A','EMI_B','EMI_C']
+    anomaly_timeseries = {}
+    
+    for reg in regions: 
+        
+	region = globals()[reg]
+	
+	var_complete=fin(ifile.variable_name,region,order='tyx')
+        var_base=fin(ifile.variable_name,region,time=('1981-01-01','2010-12-31'),order='tyx')
+
+        ntime_complete,nlats,nlons = numpy.shape(var_complete)
+        ntime_base,nlats,nlons = numpy.shape(var_base)
+
+        var_complete_flat = numpy.reshape(var_complete,(int(ntime_complete),int(nlats * nlons)))    # Flattens the spatial dimension
+        var_base_flat = numpy.reshape(var_base,(int(ntime_base),int(nlats * nlons)))
+
+       # Calculate the index #
+
+        complete_timeseries = numpy.mean(var_complete_flat,axis=1)
+        base_timeseries = numpy.mean(var_base_flat,axis=1)
+        
+	anomaly_timeseries[reg] = calc_monthly_anomaly(complete_timeseries,base_timeseries,months)	
+
+
+    IEMI_timeseries = 3*anomaly_timeseries['EMI_A'] - 2*anomaly_timeseries['EMI_B'] - anomaly_timeseries['EMI_C']
+
+
+    # Write the text file output #
+
+    fout = open(outfile_name,'w')
+    fout.write('Improved ENSO Modoki Index (as per Gen et al. 2010) \n')
+    fout.write('Base period = 1981-2010 \n')  
+    fout.write(version_info)
+    fout.write('Input file = '+ifile.fname+'\n')
+    fout.write(' YR   MON  IEMI \n') 
+
+    for ii in range(0,len(time)):
+        print >> fout, '%4i %3i %7.2f' %(years[ii],months[ii],IEMI_timeseries[ii])
+    
+    fout.close()
+    
+
+def calc_NINO(index,ifile,outfile_name):
+    """Calculates the NINO SST indices"""
+    
+    # Read the input file #
+
+    try:
+        fin=cdms2.open(ifile.fname,'r')
+    except cdms2.CDMSError:
+        print 'Unable to open file: ', ifile
+	sys.exit(0)
+    
+    # Get the time axis information #
+    
+    time = fin.getAxis('time').asComponentTime()
+    years = []
+    months = []
+    
+    for ii in range(0,len(time)):
+        years.append(int(str(time[ii]).split('-')[0]))
+	months.append(int(str(time[ii]).split('-')[1]))
+    
+    # Calculate each index #
+
+    region,minlat,maxlat,minlon,maxlon = define_region(index)
+
+    var_complete=fin(ifile.variable_name,region,order='tyx')
+    var_base=fin(ifile.variable_name,region,time=('1981-01-01','2010-12-31'),order='tyx')
+
+    ntime_complete,nlats,nlons = numpy.shape(var_complete)
+    ntime_base,nlats,nlons = numpy.shape(var_base)
+
+    var_complete_flat = numpy.reshape(var_complete,(int(ntime_complete),int(nlats * nlons)))    # Flattens the spatial dimension
+    var_base_flat = numpy.reshape(var_base,(int(ntime_base),int(nlats * nlons)))
+
+    # Calculate the index #
+
+    complete_timeseries = numpy.mean(var_complete_flat,axis=1)
+    base_timeseries = numpy.mean(var_base_flat,axis=1)
+
+    NINO_timeseries = calc_monthly_anomaly(complete_timeseries,base_timeseries,months)	
+
+
+    # Write the text file output #
+
+    fout = open(outfile_name,'w')
+    coords = ' (lat: %s to %s, lon: %s to %s)' %(str(minlat),str(maxlat),str(minlon),str(maxlon))
+    title = index+coords+'\n'
+    fout.write(title)
+    fout.write('Base period = 1981-2010 \n')  
+    fout.write(version_info)
+    fout.write('Input file = '+ifile.fname+'\n')
+    headers = ' YR   MON  %s \n' %(index)
+    fout.write(headers) 
+
+    for ii in range(0,len(time)):
+        print >> fout, '%4i %3i %7.2f' %(years[ii],months[ii],NINO_timeseries[ii])
+    fout.close()
+	    
+ 
+function_for_index = {
+    'NINO':        calc_NINO,
+    'IEMI':        calc_IEMI,
+    'SAMI':        calc_SAM,
+                	  }     
+
+def main(index,infile_name,outfile_name):
+    """Run the program"""
+
+    ## Get the input file attributes ##
+
+    infile = InputFile(infile_name)
+    
+    ## Initialise relevant index function ##
+    
+    calc_index = function_for_index[index[0:4]]
+
+    ## Calculate the index ##
+
+    calc_index(index,infile,outfile_name)
+    
+ 
+
+if __name__ == '__main__':
+
+    ### Help and manual information ###
+
+    usage = "usage: %prog [options] {}"
+    parser = OptionParser(usage=usage)
+
+    parser.add_option("-M", "--manual",action="store_true",dest="manual",default=False,help="output a detailed description of the program")
+    
+    (options, args) = parser.parse_args()            # Now that the options have been defined, instruct the program to parse the command line
+
+    if options.manual == True or len(args) != 3:
+	print """
+	Usage:
+            cdat calc_climate_index.py [-M] [-h] {index} {input file} {output file}
+
+	Options
+            -M  ->  Display this on-line manual page and exit
+            -h  ->  Display a help/usage message and exit
+
+        Pre-defined indices
+            NINO12, NINO3, NINO4, NINO34 
+	    IEMI, SAMI
+        
+	Note
+	    The base period is hard-wired as 1981-01-01 to 2010-12-31
+	
+	Example
+	    /opt/cdat/bin/cdat calc_climate_index.py NINO34 /work/dbirving/datasets/Merra/data/ts_Merra_surface_monthly_native.nc 
+	    /work/dbirving/processed/indices/ts_Merra_NINO34_monthly_native.txt
+	    
+	Author
+            Damien Irving, 26 Jun 2012.
+
+	Bugs
+            Please report any problems to: d.irving@student.unimelb.edu.au
+	"""
+	sys.exit(0)
+    
+    else:
+                
+        print 'Index:', args[0]
+        print 'Input file:', args[1]
+        print 'Output file:', args[2]
+
+        main(args[0],args[1],args[2])
+    
---
+++ data_processing/calc_streamfunction.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env cdat
+"""
+SVN INFO: $Id$
+Filename:     calc_streamfunction.py
+Author:       Damien Irving, irv033, Damien.Irving@csiro.au
+Description:  Calculates the streamfunction     
+
+Input:        U and V wind files
+Output:       A netCDF file containing the streamfunction
+
+
+Updates | By | Description
+--------+----+------------
+17 July 2012 | Damien Irving | Initial version.
+
+"""
+
+__version__= '$Revision$'
+
+
+### Import required modules ###
+
+import optparse
+from optparse import OptionParser
+import sys
+from datetime import datetime
+
+import cdms2 
+from cdms2 import MV2
+import numpy
+from numpy import *
+
+import sphere
+
+
+#print cdms2.getNetcdfShuffleFlag()
+#print cdms2.setNetcdfDeflateFlag()
+#print cdms2.setNetcdfDeflateLevelFlag()
+#cdms2.setNetcdfShuffleFlag(1)
+#cdms2.setNetcdfDeflateFlag(1)
+#cdms2.setNetcdfDeflateLevelFlag(1)
+
+
+def time_axis_check(axis1, axis2):
+    """Checks whether the time axes of the input files are the same"""
+    
+    start_time1 = axis1.asComponentTime()[0]
+    start_time1 = str(start_time1)
+    start_year1 = start_time1.split('-')[0]
+    
+    end_time1 = axis1.asComponentTime()[-1]
+    end_time1 = str(end_time1)
+    end_year1 = end_time1.split('-')[0]
+    
+    start_time2 = axis2.asComponentTime()[0]
+    start_time2 = str(start_time2)
+    start_year2 = start_time2.split('-')[0]
+    
+    end_time2 = axis2.asComponentTime()[-1]
+    end_time2 = str(end_time2)
+    end_year2 = end_time2.split('-')[0]
+
+    if (start_year1 != start_year2 or len(axis1) != len(axis2)):
+        sys.exit('Input files do not all have the same time axis')
+
+
+def xy_axis_check(axis1, axis2):
+    """Checks whether the lat or lon axes of the input files are the same""" 
+   
+    if (len(axis1) != len(axis2)):
+        sys.exit('Input files do not all have the same %s axis' %(axis1.id))
+
+	
+def main(fname_u, vname_u, fname_v, vname_v, fname_out, python):
+    """Run the program"""
+
+    ### Read the input data ###
+
+    ## U wind ##
+    infile_u = cdms2.open(fname_u)
+    u = infile_u(vname_u,order='tyx')
+
+    ## V wind ##
+    infile_v = cdms2.open(fname_v)
+    v = infile_v(vname_v,order='tyx')
+
+
+    ### Check that the input data are all on the same coordinate axes ###
+
+    ## Time ##
+
+    time_u = u.getTime()
+    time_v = v.getTime()
+    
+    time_axis_check(time_u, time_v)
+    
+    
+    ## Latitude ##
+    
+    lat_u = u.getLatitude()
+    lat_v = v.getLatitude()
+
+    xy_axis_check(lat_u, lat_v)
+
+    ## Longitude ##
+    
+    lon_u = u.getLongitude()
+    lon_v = v.getLongitude()
+
+    xy_axis_check(lon_u, lon_v)
+
+
+    ### Calculate the streamfunction (sf) and velocity potential (vp) ###
+        
+    ntime, nlats, nlons = numpy.shape(u)
+    
+    sf = numpy.zeros([ntime, nlats, nlons])
+    x = sphere.Sphere(lon_u,lat_u)
+    
+    for t in range(0,ntime):
+        sf[t,:,:], vp = x.sfvp(u[t,:,:],v[t,:,:])
+    
+    
+    ### Create output file ###
+    
+    if python == False:
+    
+	## Use cdms2 method ##
+
+	global_atts = {'title': 'Streamfunction',
+		       'contact': 'Damien Irving (d.irving@student.unimelb.edu.au)',
+		       'history': 'Calculated streamfunction from U and V wind',
+		       'sourcefiles': '%s, %s' %(fname_u,fname_v),
+		       'created': 'Created %s using %s' %(datetime.utcnow().isoformat(), sys.argv[0])
+		      }
+
+	outfile = cdms2.open(fname_out,'w')
+
+	for key, value in global_atts.iteritems():
+	    setattr(outfile, key, value)
+
+	axisInTime = outfile.copyAxis(u.getTime())
+	axisInLat = outfile.copyAxis(u.getLatitude())   # infile_u['latitude']
+	axisInLon = outfile.copyAxis(u.getLongitude())  # infile_u['longitude']
+
+
+	var = sf
+	var = MV2.array(var)
+	var = var.astype(numpy.float32)
+	var.setAxisList([axisInTime,axisInLat,axisInLon])
+	var.id = 'sf'
+
+	var_atts = {'name': 'Streamfunction',
+        	     'standard_name': 'sf',
+        	     'original_name': 'sf',
+        	     'units': 'm2 s-1',
+		     'history': 'Calculated streamfunction from U and V wind'
+		    } 
+
+	for key, value in var_atts.iteritems():
+	    setattr(var, key, value)
+
+
+	outfile.write(var)  
+
+    else:    
+    
+	## Use python method ##
+
+	import Scientific.IO.NetCDF
+	from Scientific.IO.NetCDF import NetCDFFile
+
+	outfile = NetCDFFile(fname_out, 'w') 
+
+	# Global attributes #
+
+	setattr(outfile,'Title','Streamfunction')
+	setattr(outfile,'contact','Damien Irving (d.irving@student.unimelb.edu.au)')
+	setattr(outfile,'history','Calculated streamfunction from U and V wind')
+	sourcefile_text = '%s, %s' %(fname_u,fname_v)
+	setattr(outfile,'sourcefiles',sourcefile_text)
+	creation_text = 'Created %s using %s' %(datetime.utcnow().isoformat(), sys.argv[0])
+	setattr(outfile,'created',creation_text)
+
+	# Dimensions #
+
+	outfile.createDimension('time',None)
+	outfile.createDimension('latitude',nlats)
+	outfile.createDimension('longitude',nlons)
+
+	times = outfile.createVariable('time',dtype('float32').char,('time',))
+	lats = outfile.createVariable('latitude',dtype('float32').char,('latitude',))
+	lons = outfile.createVariable('longitude',dtype('float32').char,('longitude',))
+
+	times[:] = u.getTime()
+	lats[:] = lat_u
+	lons[:] = lon_u    # What about the variable attributes?? use attList = dir(NetCDFVariable)  OR  attData = getattr(var,  attName) 
+
+	# Variable #
+
+	streamf = outfile.createVariable('sf',dtype('float32').char,('time','latitude','longitude'))
+	streamf.units = 'm2 s-1'
+	streamf.name = 'Streamfunction'
+	streamf.history = 'Calculated streamfunction from U and V wind'
+	streamf.standard_name = 'sf'
+
+	sf = sf.astype(numpy.float32)
+	streamf[:] = sf
+
+
+	outfile.close()
+	infile_u.close()
+	infile_v.close()
+
+
+if __name__ == '__main__':
+
+    ### Help and manual information ###
+
+    usage = "usage: %prog [options] {u_file} {u_name} {v_file} {v_name} {output_file}"
+    parser = OptionParser(usage=usage)
+
+    parser.add_option("-M", "--manual",action="store_true",dest="manual",default=False,help="output a detailed description of the program")
+    parser.add_option("-p", "--python",action="store_true",dest="python",default=False,help="use python (not cdms2) to create output file")
+
+    (options, args) = parser.parse_args()            # Now that the options have been defined, instruct the program to parse the command line
+
+    if options.manual == True or len(sys.argv) == 1:
+	print """
+	Usage:
+            calc_streamfunction.py [-M] [-h] [-p] {u_file} {u_name} {v_file} {v_name} {output_file}
+
+	Options
+            -M -> Display this on-line manual page and exit
+            -h -> Display a help/usage message and exit
+	    -p -> Use python (not cdms2) to create output file"
+
+	Description
+            Takes as input the u and v wind and outputs the streamfunction    
+
+	Environment
+            Need to load cdat
+
+	Author
+            Damien Irving, 17 Jul 2012.
+
+	Bugs
+            Please report any problems to: d.irving@student.unimelb.edu.au
+	"""
+	sys.exit(0)
+
+    else:
+
+        # Repeat the command line arguments #
+
+	print 'Input U wind file: ', args[0]
+	print 'Input V wind file: ', args[2]
+	print 'Ouput file: ', args[4]
+
+    fname_u, vname_u, fname_u, vname_u, fname_out = args  
+    
+    main(fname_u, vname_u, fname_u, vname_u, fname_out, options.python)
