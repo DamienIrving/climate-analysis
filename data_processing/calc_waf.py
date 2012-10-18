567a5d568ac2faba21918deaecd0e40e29cb12bf
Damien Irving <d.irving@student.unimelb.edu.au>
2012-10-18 16:27:19 +1100

    Improvements to calc_waf.py, but netCDF fill value problem remaims

--- data_processing/calc_waf.py
+++ data_processing/calc_waf.py
@@ -30,6 +30,7 @@ import optparse
 from optparse import OptionParser
 import sys
 from datetime import datetime
+import os
 
 import cdms2 
 from cdms2 import MV2
@@ -40,6 +41,13 @@ import array
 
 import Scientific.IO.NetCDF
 from Scientific.IO.NetCDF import NetCDFFile
+
+#import Numeric
+
+from scipy.io.numpyio import fwrite, fread
+
+import regrid2
+from regrid2 import Regridder
 
 
 
@@ -80,42 +88,61 @@ def read_climatology(fname,vname,nlat,nlon):
     months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
     monthly_climatology = numpy.zeros([12,nlat,nlon])
     for i in range(0,12):
-        data = infile(vname+'_'+months[i],order='tyx')
+        data = infile(vname+'_'+months[i],order='yx')
 	monthly_climatology[i,:,:] = data
     
-    inflie.close()
+    infile.close()
     
     return monthly_climatology
 
 
-def write_binary(data,outfile)
+def write_binary(data,outfile):
     """Takes a numpy data array and writes a corresponding binary data file"""
     
     fileobj = open(outfile, mode='wb')
-    outvalues = array.array('f', itemsize=4)
-    outvalues.fromlist( data.flat.tolist() )
-    outvalues.tofile(fileobj)
+    fwrite(fileobj, data.size, data)
     
     fileobj.close()
 
 
-def read_binary(infile,nlat,nlon)
+def read_binary(infile,dims):
     """Reads a binary data file and puts data in a numpy array"""
 
-    import Numeric as N
-
     fileobj = open(infile, mode='rb')
-    binvalues = array.array('f')
-    binvalues.read(fileobj, nlon * nlat)
-
-    data = N.array(binvalues, typecode=N.Float)
-
-    data = N.reshape(data, (nlat, nlon))
-
+    
+    datatype = 'f'
+    size = dims[0] * dims[1] * dims[2]
+    
+    read_data = fread(fileobj, size, datatype)
+    read_data = read_data.reshape(dims)
+    
     fileobj.close()
+    
+    return read_data
 
 
-def write_netcdf(fname_out,waf_data,time_axis,lat_axis,lon_axis,sourcefile_text,outvar)
+def regrid(data,oldgrid):
+    """Takes input data and regrids to a 2.5 degree global grid"""
+    
+    latitude = cdms2.createAxis(numpy.arange(-90,92.5,2.5,'f'),id='latitude')
+    latitude.units = 'degrees_north'
+    latitude.designateLatitude()
+
+    longitude = cdms2.createAxis(numpy.arange(0,360,2.5,'f'),id='longitude')
+    longitude.units = 'degrees_east'
+    longitude.designateLongitude()
+    longitude.modulo = 360.
+    longitude.topology = 'circular'
+
+    newgrid = cdms2.createRectGrid(latitude,longitude)                                                     
+
+    regridfunc = Regridder(oldgrid, newgrid)                                
+    new_data = regridfunc(data)
+    
+    return new_data,latitude,longitude
+    
+
+def write_netcdf(fname_out,waf_data,time_axis,lat_axis,lon_axis,sourcefile_text,outvar):
     """Writes the output netcdf file"""
     
     outfile = NetCDFFile(fname_out, 'w') 
@@ -151,6 +178,7 @@ def write_netcdf(fname_out,waf_data,time_axis,lat_axis,lon_axis,sourcefile_text,
     out_data.name = 'Wave activity flux, %s component'  %(outvar[-1])
     out_data.history = 'Calculated wave activity flux from U wind, V wind and geopotential height'
     out_data.standard_name = outvar
+    #out_data._FillValue = 9.999e+20  # or missing_value or fill_value????
 
     waf_data = waf_data.astype(numpy.float32)
     out_data[:] = waf_data
@@ -168,7 +196,7 @@ def main(fname_u, fname_uclim, vname_u, fname_v, fname_vclim, vname_v, fname_zg,
     ## u wind ##
     infile_u = cdms2.open(fname_u)
     u_data = infile_u(vname_u,order='tyx')
-
+    
     ## v wind ##
     infile_v = cdms2.open(fname_v)
     v_data = infile_v(vname_v,order='tyx')
@@ -207,10 +235,13 @@ def main(fname_u, fname_uclim, vname_u, fname_v, fname_vclim, vname_v, fname_zg,
     xy_axis_check(lon_u, lon_v)
     xy_axis_check(lon_u, lon_zg)
 
+    ## Find grid characteristics ##
+
+    ntime, nlat, nlon = numpy.shape(u_data)
+    input_grid = u_data.getGrid() 
+    
     
     ### Read the climatology data ###
-
-    ntime, nlat, nlon = numpy.shape(u)
     
     uclim_data = read_climatology(fname_uclim,vname_u,nlat,nlon)
     vclim_data = read_climatology(fname_vclim,vname_v,nlat,nlon)
@@ -219,41 +250,52 @@ def main(fname_u, fname_uclim, vname_u, fname_v, fname_vclim, vname_v, fname_zg,
     
     ### Regrid the data ###
     
-     
+    u_data_regrid, lat_regrid, lon_regrid = regrid(u_data,input_grid)
+    uclim_data_regrid, lat_regrid, lon_regrid = regrid(uclim_data,input_grid)
+    
+    v_data_regrid, lat_regrid, lon_regrid = regrid(v_data,input_grid)
+    vclim_data_regrid, lat_regrid, lon_regrid = regrid(vclim_data,input_grid)
+    
+    zg_data_regrid, lat_regrid, lon_regrid = regrid(zg_data,input_grid)
+    zgclim_data_regrid, lat_regrid, lon_regrid = regrid(zgclim_data,input_grid)
+    
     
     ### Create the binary data files for input into the Fortran wap script ### 
     
-    write_binary(u_data,'u.bin')
-    write_binary(uclim_data,'uclim.bin')
-    write_binary(v_data,'v.bin')
-    write_binary(vclim_data,'vclim.bin')
-    write_binary(zg_data,'zg.bin')
-    write_binary(zgclim_data,'zgclim.bin')
+    write_binary(u_data_regrid,'u.bin')
+    write_binary(uclim_data_regrid,'uclim.bin')
+    write_binary(v_data_regrid,'v.bin')
+    write_binary(vclim_data_regrid,'vclim.bin')
+    write_binary(zg_data_regrid,'zg.bin')
+    write_binary(zgclim_data_regrid,'zgclim.bin')
 
     
     ### Calculate the wave activity flux ###
     
-    os.system("./home/dbirving/data_processing/WAF/tnf_xy_onelevel.run << EOF")
-    os.system("250")
-    os.system("u.bin")
-    os.system("uclim.bin")
-    os.system("v.bin")
-    os.system("vclim.bin")
-    os.system("zg.bin")
-    os.system("zgclim.bin")
-    os.system("wafx.bin")
-    os.system("wafy.bin")
-    os.system("EOF") 
+    fout = open('answers.txt','w')
+    print >>fout, '250'
+    print >>fout, 'zg.bin'
+    print >>fout, 'zgclim.bin'
+    print >>fout, 'u.bin'
+    print >>fout, 'uclim.bin'
+    print >>fout, 'v.bin'
+    print >>fout, 'vclim.bin'
+    print >>fout, 'wafx.bin'
+    print >>fout, 'wafy.bin'
+
+    fout.close()
+    
+    os.system("/home/dbirving/data_processing/WAF/tnf_xy_onelevel.run < answers.txt")
     
     ### Write the output netCDF file ###
     
-    wafx_data = read_binary('wafx.bin',nlat,nlon)
-    wafy_data = read_binary('wafy.bin',nlat,nlon)
+    wafx_data = read_binary('wafx.bin',[ntime,73,144])
+    wafy_data = read_binary('wafy.bin',[ntime,73,144])
     
     sourcefile_text = '%s, %s, %s, %s, %s, %s' %(fname_u, fname_uclim, fname_v, fname_vclim, fname_zg, fname_zgclim)
     
-    write_netcdf(fname_wafx,wafx_data,time_u,lat_u,lon_u,sourcefile_text,'wafx')
-    write_netcdf(fname_wafy,wafy_data,time_u,lat_u,lon_u,sourcefile_text,'wafy')
+    write_netcdf(fname_wafx,wafx_data,time_u,lat_regrid,lon_regrid,sourcefile_text,'wafx')
+    write_netcdf(fname_wafy,wafy_data,time_u,lat_regrid,lon_regrid,sourcefile_text,'wafy')
 
 
     infile_u.close()
@@ -275,7 +317,7 @@ if __name__ == '__main__':
     if options.manual == True or len(sys.argv) == 1:
 	print """
 	Usage:
-            calc_waf.py [-M] [-h] [-p] {u_file} {u_clim_file} {u_name} 
+            calc_waf.py [-M] [-h] {u_file} {u_clim_file} {u_name} 
 	    {v_file} {v_clim_file} {v_name} {zg_file} {zg_clim_file} {zg_name}
 	    {output_wafx_file} {output_wafy_file}
 
@@ -286,7 +328,12 @@ if __name__ == '__main__':
 	Description
             Takes as input the u wind, v wind, geopotential height (zg)  
 	    and their climatologies and outputs the x and y 
-	    components of the wave activity flux 
+	    components of the wave activity flux.
+	
+	Assumptions (i.e. hard wired elements) 
+	    The WAF Fortran code is hard wired to produce output on a 
+	    global 2.5 by 2.5 deg grid (i.e. 73 lats, 144 lons)
+	    It is hard wired that the input data is from the 250 hPa level 
 	
 	Reference
 	    Takaya, K., Nakamura, H., 2001.
@@ -297,6 +344,20 @@ if __name__ == '__main__':
 	Environment
             Need to load cdat
 
+        Example (abyss.earthsci.unimelb.edu.au)
+	    /opt/cdat/bin/cdat calc_waf.py 
+	    /work/dbirving/datasets/Merra/data/ua_Merra_250hPa_monthly_native.nc
+	    /work/dbirving/datasets/Merra/data/processed/ua_Merra_250hPa_monthly-clim-1981-2010_native.nc
+	    ua
+	    /work/dbirving/datasets/Merra/data/va_Merra_250hPa_monthly_native.nc
+	    /work/dbirving/datasets/Merra/data/processed/va_Merra_250hPa_monthly-clim-1981-2010_native.nc
+	    va
+	    /work/dbirving/datasets/Merra/data/zg_Merra_250hPa_monthly_native.nc
+	    /work/dbirving/datasets/Merra/data/processed/zg_Merra_250hPa_monthly-clim-1981-2010_native.nc
+	    zg
+            /work/dbirving/datasets/Merra/data/processed/wafx_Merra_250hPa_monthly_native.nc
+	    /work/dbirving/datasets/Merra/data/processed/wafy_Merra_250hPa_monthly_native.nc
+	    
 	Author
             Damien Irving, 12 Oct 2012.
 
