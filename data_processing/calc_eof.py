7f3742a970420121cff13c63fd37c4f02eef9a6a
Damien Irving <d.irving@student.unimelb.edu.au>
2012-10-24 16:09:49 +1100

    calc_eof.py region function not yet tested, added named_region.sh and seas_vars.sh to compliment it

--- data_processing/calc_eof.py
+++ data_processing/calc_eof.py
@@ -22,6 +22,7 @@ import optparse
 from optparse import OptionParser
 import sys
 from datetime import datetime
+import os
 
 import numpy
 import math
@@ -35,20 +36,36 @@ import pyclimate.ncstruct
 
 ### Define functions ###
 
+
+def extract_region(infile_name,region):
+    """Creates a new file with the region of interest extracted"""
+    
+    ### Write a check for legitimate region name ###
+    
+    if region == 'entire':
+        regional_infile_name = infile_name
+    else:
+	function = '/home/dbirving/data_processing/named_region.sh'
+	regional_infile_name = infile_name.replace('.nc','.%s.nc' %(region))
+	os.system("%s %s %s %s" %(function,region,infile_name,regional_infile_name)) 
+        
+    return regional_infile_name 
+
+
 def deg2rad(d):
     """Converts from degrees to radians"""
     
     return d*math.acos(-1.)/180.
 
 
-def calc_EOF(infile_name,var_id,lat_id):
+def calc_EOF(regional_infile_name,var_id,lat_id):
     """Calculates the EOF using pyclimate - uses outdated modules because pyclimate is old"""
     # http://fisica.ehu.es/pyclimate/ExampleEOF
     
     import Numeric
     import Scientific.IO.NetCDF
     
-    fin = Scientific.IO.NetCDF.NetCDFFile(infile_name)
+    fin = Scientific.IO.NetCDF.NetCDFFile(regional_infile_name)
     in_data = fin.variables[var_id]
     areafactor = numpy.sqrt(numpy.cos(deg2rad(fin.variables[lat_id][:])))
     data = in_data[:,:,:,:] * areafactor[numpy.newaxis,numpy.newaxis,:,numpy.newaxis]
@@ -62,18 +79,19 @@ def calc_EOF(infile_name,var_id,lat_id):
     return areafactor, pcs, lambdas, eofs
 
 
-def write_eof(outfile_name,neofs,in_lat,in_lon,eof_output,lambdas):
+def write_eof(outfile_eof_name,neofs,in_lat,in_lon,eof_output,lambdas,infile_name,outfile_pc_name,region):
     """Writes output netCDF file with EOFs in it"""
     
     # Global attributes #
 
-    fout = netCDF4.Dataset(outfile_name,'w',format='NETCDF3_CLASSIC')
+    fout = netCDF4.Dataset(outfile_eof_name,'w',format='NETCDF3_CLASSIC')
 
-    setattr(fout,'Title','Empirical Orthogonal Function analysis')
+    setattr(fout,'Title','Empirical Orthogonal Function analysis over %s region' %(region))
     setattr(fout,'Contact','Damien Irving (d.irving@student.unimelb.edu.au)')
     setattr(fout,'History','EOF calculated using PyClimate software')
     setattr(fout,'Reference','http://www.pyclimate.org/')
-    setattr(fout,'Sourcefile',infile_name)
+    setattr(fout,'SourceFile',infile_name)
+    setattr(fout,'CompanionFiles',outfile_pc_name)
     creation_text = 'Created %s using %s' %(datetime.utcnow().isoformat(), sys.argv[0])
     setattr(fout,'Created',creation_text)
     setattr(fout,'Format','NETCDF3_CLASSIC')
@@ -110,37 +128,81 @@ def write_eof(outfile_name,neofs,in_lat,in_lon,eof_output,lambdas):
     fout.close()
 
 
-def main(infile_name,var_id,outfile_name,neofs):
+def write_pcs(outfile_pc_name,neofs,pcs,years,months,infile_name,outfile_eof_name,region):
+    """Writes an output text file for each of the Principle Components (pcs)"""
+    
+    for i in range(0,neofs):
+        
+	# File name #
+    
+        new_name = outfile_pc_name.replace('PC','PC'+str(i+1))
+        fout = open(new_name,'w')   
+	        
+	# Global attributes #
+	
+	fout.write('Title: Principle Component %s from EOF analysis over %s region \n' %(i+1,region))
+        fout.write('Contact: Damien Irving (d.irving@student.unimelb.edu.au) \n')
+        fout.write('History: EOF calculated using PyClimate software \n')
+        fout.write('Reference: http://www.pyclimate.org/ \n')
+        fout.write('Sourcefile: %s \n' %(infile_name))
+	fout.write('Companion EOF file: %s \n' %(outfile_eof_name))
+        creation_text = 'Created %s using %s \n' %(datetime.utcnow().isoformat(), sys.argv[0])
+        fout.write(creation_text)
+	
+	# Data #
+	
+	fout.write(' YR   MON  PC%s \n' %(i+1)) 
+        ntime = numpy.shape(pcs)[0]
+	for t in range(0,ntime):
+            print >> fout, '%4i %3i %7.2f' %(years[t],months[t],pcs[t,i])
+
+
+def main(infile_name,var_id,outfile_eof_name,outfile_pc_name,neofs,region):
     """Run the program"""
+    
+    ## Create a new file with the region of interest, if necessary ##
+    
+    regional_infile_name = extract_region(infile_name,region)
     
     ## Get input data details ##
 
-    fin = cdms2.open(infile_name)
+    fin = cdms2.open(regional_infile_name)
     
     in_data = fin(var_id)   # order='tzyx'
+    
+    # Lat/lon info #
+    
     in_lat = in_data.getLatitude()
     in_lon = in_data.getLongitude()
     
     lat_id = in_lat.id
     
+    # Time axis info #
+    
+    in_time = fin.getAxis('time').asComponentTime()
+    years = []
+    months = []
+    
+    for ii in range(0,len(in_time)):
+        years.append(int(str(in_time[ii]).split('-')[0]))
+        months.append(int(str(in_time[ii]).split('-')[1]))
+    
     fin.close()
     
     ## Calculate the EOF using outdated pyclimate routine ##
     
-    areafactor, pcs, lambdas, eofs = calc_EOF(infile_name,var_id,lat_id)
+    areafactor, pcs, lambdas, eofs = calc_EOF(regional_infile_name,var_id,lat_id)
 
     # Prepate output data #
     
     eof_output = numpy.zeros([neofs,len(in_lat),len(in_lon)])
     for i in range(0,neofs):
         eof_output[i,:,:] = (numpy.reshape(eofs[:,i],(len(in_lat),len(in_lon))) / areafactor[:,numpy.newaxis]).astype(numpy.float32)
-        #eof1_data = eof1_data.astype(numpy.float32)
 
     ## Write the output files ##
     
-    write_eof(outfile_name,neofs,in_lat,in_lon,eof_output,lambdas)
-    #write_pcs()
-
+    write_eof(outfile_eof_name,neofs,in_lat,in_lon,eof_output,lambdas,infile_name,outfile_pc_name,region)
+    write_pcs(outfile_pc_name,neofs,pcs,years,months,infile_name,outfile_eof_name,region)
 
 
 if __name__ == '__main__':
@@ -152,18 +214,24 @@ if __name__ == '__main__':
 
     parser.add_option("-M", "--manual",action="store_true",dest="manual",default=False,help="output a detailed description of the program")
     parser.add_option("-n", "--neofs",dest="neofs",type='int',default=5,help="Number of EOFs for output [default=5]")
+    parser.add_option("-r", "--region",dest="region",type='string',default='entire',help="Regional over which to calculate EOF [default=entire]")
     
     (options, args) = parser.parse_args()            # Now that the options have been defined, instruct the program to parse the command line
 
     if options.manual == True or len(sys.argv) == 1:
         print """
         Usage:
-            calc_eof.py [-M] [-h] {input_file} {input_variable} {output_file}
+            calc_eof.py [-M] [-h] [-n] [-r] {input_file} {input_variable} {output_eof_file} {output_pc_file}
 
         Options
             -M -> Display this on-line manual page and exit
             -h -> Display a help/usage message and exit
 	    -n -> Number of EOFs that are output [default = 5]
+	    -r -> Region over which to calculate the EOF [default = entire; i.e whole input region]
+
+        Note
+	    The output PC files will take the user supplied file name 
+	    and replace the string PC with PC1, PC2 etc 
 
         Description
 
@@ -176,7 +244,7 @@ if __name__ == '__main__':
             Need to load cdat
 
         Example (abyss.earthsci.unimelb.edu.au)
-	    /opt/cdat/bin/cdat calc_eof.py /work/dbirving/test_data/ncepslp.djf.nc djfslp test_eof.nc
+	    /opt/cdat/bin/cdat calc_eof.py /work/dbirving/test_data/ncepslp.djf.nc djfslp test_EOF.nc test_PC.txt
 	    
         Author
             Damien Irving, 12 Oct 2012.
@@ -191,8 +259,10 @@ if __name__ == '__main__':
         # Repeat the command line arguments #
 
         print 'Input file: ', args[0]
-        print 'output file: ', args[2]
+        print 'Output EOF file: ', args[2]
+	print 'Output PC file:', args[3]
+	print 'Region:', options.region
         
-        infile_name, var_id, outfile_name = args  
+        infile_name, var_id, outfile_eof_name, outfile_pc_name = args  
     
-        main(infile_name, var_id, outfile_name, options.neofs)
+        main(infile_name, var_id, outfile_eof_name, outfile_pc_name, options.neofs, options.region)
---
+++ data_processing/named_region.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+# $ Id: $
+#
+# Author: ( @csiro.au)
+# Date:   16/02/2011
+#
+# Description: 
+#
+# Copyright 2011, CSIRO
+#
+
+nargs=3
+function usage {
+    echo "USAGE: $0 input output"
+    echo "    region:     Region to extract"
+    echo "    input:      Input file"
+    echo "    output:     Output file"
+    exit 1
+}
+
+if [ $nargs -ne $# ] ; then
+  usage
+fi
+
+region=$1
+infile=$2
+outfile=$3
+
+if [ ! -f $infile ] ; then
+    echo "Input file doesn't exist: " $infile
+    usage
+fi
+
+if [ -f $outfile ] ; then
+   echo "Outfile exists. Skipping: " $outfile
+   exit 2
+fi
+
+inbase=`basename $infile`
+extn=`expr match "${inbase}" '.*\.\(.*\)'`
+if [ $extn = 'xml' ] ; then
+  tmp_in=$TMPDIR/xml_concat.$$.nc   # If I want to use this, I should override the default $TMPDIR
+  python $CCT/processing/cdml_cat/xml_to_nc.py None $infile $tmp_in   # xml_to_nc.py is in my git_repo
+  infile=$tmp_in
+fi
+
+if [ ! -f $infile ] ; then
+    echo "Conversion of file from XML catalogue failed."
+    exit 1
+fi
+
+case ${region} in
+    trop_ind_pac )
+      box="39,280,-25,25"
+      cdo sellonlatbox,${box} $infile $outfile ;;
+    eq_pacific )
+      box="120,280,-20,20"
+      cdo sellonlatbox,${box} $infile $outfile ;;
+    indian )
+      box="39,120,-25,25"
+      cdo sellonlatbox,${box} $infile $outfile ;;
+    indo-pac )
+      box="45,260,-55,-5"
+      cdo sellonlatbox,${box} $infile $outfile ;;
+    southern )
+      box="0,360,-70,-20"
+      cdo sellonlatbox,${box} $infile $outfile ;;
+    sh )
+      box="0,360,-90,0"
+      cdo sellonlatbox,${box} $infile $outfile ;;
+    nh )
+      box="0,360,0,90"
+      cdo sellonlatbox,${box} $infile $outfile ;;
+    australia )
+      box="110,156,-50,-5"
+      cdo sellonlatbox,${box} $infile $outfile ;;
+    * )
+      regionfile=/home/dbirving/data_processing/regions/${region}.txt   
+      if [ -f $regionfile ] ; then
+          cdo maskregion,$regionfile $infile $outfile
+      else
+        echo "Unknown region: $region"
+        exit 1
+      fi
+      ;;
+esac
+
+#rm $infile
+
+exit 0
