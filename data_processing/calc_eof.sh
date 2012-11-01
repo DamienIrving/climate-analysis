f8be5a43eb273b83e213f278f03fba3a626cbfb6
Damien Irving <d.irving@student.unimelb.edu.au>
2012-11-01 16:00:54 +1100

    Improved on EOF code

--- data_processing/calc_eof.sh
+++ data_processing/calc_eof.sh
@@ -9,16 +9,19 @@
 #              distributions, the time-series and the explained variance are all
 #              add to the output file.
 #
-# Copyright 2011, CSIRO
 #
 
 version="$Revision: 327 $"
 
-nargs=4
+nargs=5
 function usage {
-    echo "USAGE: $0 n var input output"
+    echo "USAGE: $0 n var var_long input output"
+    echo "    -r:         Select region [options: eqpacific]"
+    echo "    -s:         Select start date [e.g. 1979-01-01]"
+    echo "    -e:         Select end date [e.g. 2010-12-31]"
     echo "    n:          Number of EOFs to calculate"
     echo "    var:        Variable base name"
+    echo "    var_long:   Variable long name"
     echo "    input:      Input file"
     echo "    output:     Output file"
     echo ""
@@ -29,40 +32,19 @@ function usage {
     exit 1
 }
 
-if [ $nargs -ne $# ] ; then
-  usage
-fi
-
-np=$1
-varbase=$2
-infile=$3
-outfile=$4
-
-if [ ! -f $infile ] ; then
-    echo "Input file doesn't exist: " $infile
-    usage
-fi
-
-if [ -f $outfile ] ; then
-   echo "Outfile exists. Skipping: " $outfile
-   exit 2
-fi
 
 TMPDIR=/work/dbirving/temp_data
 MYDIR=/home/dbirving/data_processing
 
 
-# Select the desired region and time period
-
+# Parse the command line for optional arguments
 region_flag=false
 start_flag=false
 end_flag=false
-tmpregfile=$TMPDIR/tmp_reg.nc
-tmptimefile=$TMPDIR/tmp_time.nc
-while getopts ":r:t:" opt; do
+while getopts "r:s:e:" opt; do
   case $opt in
     r)
-      $MYDIR/named_region.sh $OPTARG ${infile} ${tmpregfile}  
+      region=$OPTARG  
       region_flag=true
       ;;
     s)
@@ -90,15 +72,44 @@ while getopts ":r:t:" opt; do
 done
 shift $((OPTIND-1))
 
-if (( region_flag )) && (( end_flag )) ; then
-  cdo seldate,${start_date},${end_date} ${tempregfile} ${tmptimefile}
+
+# Parse the command line for required arguments
+
+if [ $nargs -ne $# ] ; then
+  usage
+fi
+
+np=$1
+varbase=$2
+varlong=$3
+infile=$4
+outfile=$5
+
+if [ ! -f $infile ] ; then
+    echo "Input file doesn't exist: " $infile
+    usage
+fi
+
+if [ -f $outfile ] ; then
+   echo "Outfile exists. Skipping: " $outfile
+   exit 2
+fi
+
+
+# Select the desired region and time period
+tmpregfile=$TMPDIR/tmp_reg.nc
+tmptimefile=$TMPDIR/tmp_time.nc
+if (( ${region_flag} )) && (( ${end_flag} )) ; then
+  $MYDIR/named_region.sh ${region} ${infile} ${tmpregfile}
+  cdo seldate,${start_date},${end_date} ${tmpregfile} ${tmptimefile}
   infile=${tmptimefile}
-elif region_flag ; then
+elif [ ${region_flag} ] ; then
+  $MYDIR/named_region.sh ${region} ${infile} ${tmpregfile}
   infile=${tmpregfile}
 fi
 
 
-### Get the lat/lon varaible names....
+# Get the lat/lon varaible names....
 griddes=$TMPDIR/cdo_griddes.txt
 cdo griddes ${infile} > ${griddes} 2> /dev/null
 xname=`grep xname ${griddes} | sed -e"s/.*= \(.*\)$/\1/"`
@@ -150,8 +161,8 @@ for n in `seq ${np}`; do
       exit 1
     fi
     ncatted -h -a long_name,${var}_ts_eof${n},o,c,"eof_time_series" ${tmpcdoeofcoeff}${nf}_nospace.nc
-    ncatted -h -a standard_name,${var}_ts_eof${n},o,c,"eof_of_sea_surface_temperature" ${tmpcdoeofcoeff}${nf}_nospace.nc
-    ncatted -h -a name,${var}_ts_eof${n},o,c,"eof_of_sea_surface_temperature" ${tmpcdoeofcoeff}${nf}_nospace.nc
+    ncatted -h -a standard_name,${var}_ts_eof${n},o,c,"eof_of_${varlong}" ${tmpcdoeofcoeff}${nf}_nospace.nc
+    ncatted -h -a name,${var}_ts_eof${n},o,c,"eof_of_${varlong}" ${tmpcdoeofcoeff}${nf}_nospace.nc
     ncatted -h -a cell_methods,${var}_ts_eof${n},d,, ${tmpcdoeofcoeff}${nf}_nospace.nc
     ncks -A ${tmpcdoeofcoeff}${nf}_nospace.nc ${outfile}
     if [ 0 -ne $? ] ; then
@@ -168,8 +179,8 @@ for n in `seq ${np}`; do
       exit 1
     fi
     ncatted -h -a long_name,${var}_spatial_eof${n},o,c,"eof_spatial_pattern" ${tmpcdoeof2}_${n}.nc
-    ncatted -h -a standard_name,${var}_spatial_eof${n},o,c,"eof_of_sea_surface_temperature" ${tmpcdoeof2}_${n}.nc
-    ncatted -h -a name,${var}_spatial_eof${n},o,c,"eof_of_sea_surface_temperature" ${tmpcdoeof2}_${n}.nc
+    ncatted -h -a standard_name,${var}_spatial_eof${n},o,c,"eof_of_${varlong}" ${tmpcdoeof2}_${n}.nc
+    ncatted -h -a name,${var}_spatial_eof${n},o,c,"eof_of_${varlong}" ${tmpcdoeof2}_${n}.nc
     ncatted -h -a cell_methods,${var}_spatial_eof${n},d,, ${tmpcdoeof2}_${n}.nc
     ncks -C -v${var}_spatial_eof${n},${yname},${xname} -A ${tmpcdoeof2}_${n}.nc ${outfile}
     if [ 0 -ne $? ] ; then
@@ -186,8 +197,8 @@ for n in `seq ${np}`; do
       exit 1
     fi
     ncatted -h -a long_name,${var}_perc_eof${n},o,c,"eof_percent_explained" ${tmpperval}
-    ncatted -h -a standard_name,${var}_perc_eof${n},o,c,"eof_of_sea_surface_temperature" ${tmpperval}
-    ncatted -h -a name,${var}_perc_eof${n},o,c,"eof_of_sea_surface_temperature" ${tmpperval}
+    ncatted -h -a standard_name,${var}_perc_eof${n},o,c,"eof_of_${varlong}" ${tmpperval}
+    ncatted -h -a name,${var}_perc_eof${n},o,c,"eof_of_${varlong}" ${tmpperval}
     ncatted -h -a units,${var}_perc_eof${n},o,c,"percent" ${tmpperval}
     ncks -C -v${var}_perc_eof${n} -A ${tmpperval} ${outfile}
     if [ 0 -ne $? ] ; then
@@ -198,19 +209,20 @@ for n in `seq ${np}`; do
   done
 done
 
-rm ${tmpcdoeof1} ${tmpcdoeof2} ${tmppercvar}
-
-if region_flag ; then
-  rm ${tempregfile} 
-fi
-
-if end_flag ; then
-  rm ${temptimefile} 
-fi
+rm ${tmpcdoeof1} ${tmpcdoeof2} ${tmppercvar} ${griddes}
  
 
 # Set some metadata
 ncks -h -A -x $infile $outfile
 ncatted -h -a history,global,a,c,"\nEOFs calculated using $0 ($version) on `date`" $outfile
+
+if ${region_flag} ; then
+  rm ${tmpregfile} 
+fi
+
+if ${end_flag} ; then
+  rm ${tmptimefile} 
+fi
+
 
 exit 0
