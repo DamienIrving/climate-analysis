d29a96cfdae15d6dea36a0b1b2de2c4ade0399ff
Damien Irving <d.irving@student.unimelb.edu.au>
2013-01-28 11:54:00 +1100

    calc_eof.py with classes now functional

--- data_processing/calc_eof.py
+++ data_processing/calc_eof.py
@@ -18,11 +18,9 @@ __version__ = '$Revision$'     ## Change this for git (this is an svn command)
 
 ### Import required modules ###
 
-import optparse
 from optparse import OptionParser
 import sys
 from datetime import datetime
-import os
 import numpy
 
 import cdms2
@@ -34,10 +32,10 @@ import eof2
 
 ### Define globals ###
 
-eq_pacific = cdms2.selectors.Selector(latitude=(-30, 30, 'cc'),
-                                      longitude=(120, 280, 'cc'))
-sth_hem = cdms2.selectors.Selector(latitude=(-30, 30, 'cc'),
-                                   longitude=(120, 280, 'cc'))				     
+eqpacific = cdms2.selectors.Selector(latitude=(-30, 30, 'cc'),
+                                     longitude=(120, 280, 'cc'))
+sh = cdms2.selectors.Selector(latitude=(-30, 30, 'cc'),
+                              longitude=(120, 280, 'cc'))				     
 tropics = cdms2.selectors.Selector(latitude=(-30, 30, 'cc'),
                                    longitude=(0, 360, 'cc'))
 
@@ -49,16 +47,17 @@ class InputFile:
 
     def __init__(self, fname):
 
-        fin = cdms2.open(fname)
-        self.history = fin.attributes['history'] if ('history' in fin.attributes.keys()) else ''
         self.fname = fname
-
-        fin.close()
-   
-    def extract_data(self, var_id, region_name, time_bounds):
-         """Extracts data according to the desired time period and region"""
+	fin = cdms2.open(fname)
+        self.history = fin.attributes['history'] if ('history' in fin.attributes.keys()) else ''
+	self.region = None
+	self.infile = fin
+	
+    def extract_data(self, variable, region_name, time_bounds):
+        """Extracts data according to the desired time period and region"""
 
         self.region = region_name
+        infile = self.infile
 
         if region_name:
             try:
@@ -70,70 +69,68 @@ class InputFile:
             region = None 
 
         if time_bounds and region:
-            in_data = fin(var_id,region,time=(time_bounds[0],time_bounds[1]),squeeze=1)
+            indata = infile(variable, region, time=(time_bounds[0], time_bounds[1]), squeeze=1)
         elif time_bounds: 
-            in_data = fin(var_id,time=(time_bounds[0],time_bounds[1]),squeeze=1)
+            indata = infile(variable, time=(time_bounds[0], time_bounds[1]), squeeze=1)
         elif region:
-            in_data = fin(var_id,region,squeeze=1)
+            indata = infile(variable, region, squeeze=1)
         else:
-            in_data = fin(var_id,squeeze=1)
+            indata = infile(variable, squeeze=1)
 
-        return in_data
+        return indata
 
 
 class EofAnalysis:
     """Performs an EOF analysis, using the eof2 package: https://github.com/ajdawson/eof2"""
     
     def __init__(self, data, neofs):
-        self.neofs = nneofs
-	self.data = data
-	solver = eof2.Eof(data, weights='area')
-
+        self.neofs = neofs
+        self.data = data
+        self.solver = eof2.Eof(data, weights='area')
+	
     def eof(self, eof_scaling):
         """Calculates the EOF"""
 	
-	if eof_scaling == 3:
-            eofs = solver.eofsAsCorrelation(neofs=self.neofs)
+        if eof_scaling == 3:
+            eofs = self.solver.eofsAsCorrelation(neofs=self.neofs)
             eof_scaling_text = 'EOF scaled as the correlation of the PCs with the original field - used eofsAsCorrelation() function'
             eof_units = ''
-	else: 
-            eofs = solver.eofs(neofs=self.neofs,eofscaling=eof_scaling)
+        else: 
+            eofs = self.solver.eofs(neofs=self.neofs, eofscaling=eof_scaling)
             eof_units = ''
             if eof_scaling == 0:
-        	eof_scaling_text = 'None'
+                eof_scaling_text = 'None'
             elif eof_scaling == 1:
-        	eof_scaling_text = 'EOF is divided by the square-root of its eigenvalue'  
+                eof_scaling_text = 'EOF is divided by the square-root of its eigenvalue'  
             elif eof_scaling == 2:
-        	eof_scaling_text = 'EOF is multiplied by the square-root of its eigenvalue'        
+                eof_scaling_text = 'EOF is multiplied by the square-root of its eigenvalue'        
             else:
-        	print 'EOF scaling method not recongnised'
-        	sys.exit(1)
-        
-	return eofs, eof_scaling_text, eof_units
-    
-    
+                print 'EOF scaling method not recongnised'
+                sys.exit(1)
+
+        return eofs, eof_scaling_text, eof_units
+
     def pcs(self, pc_scaling):
-	"""Calculates the prinicple components"""
-	
-	pcs = solver.pcs(npcs=self.neofs, pcscaling=pc_scaling)
-	pc_units = ''
-	if pc_scaling == 0:
+        """Calculates the prinicple components"""
+
+        pcs = self.solver.pcs(npcs=self.neofs, pcscaling=pc_scaling)
+        pc_units = ''
+        if pc_scaling == 0:
             pc_scaling_text = 'None'
-	elif pc_scaling == 1:
+        elif pc_scaling == 1:
             pc_scaling_text = 'PC scaled to unit variance (divided by the square-root of its eigenvalue)'
-	elif pc_scaling == 2:
+        elif pc_scaling == 2:
             pc_scaling_text = 'PC multiplied by the square-root of its eigenvalue'    
-	else:
-	    print 'PC scaling method not recongnised'
-	    sys.exit(1)    
-    
-        return pcs, pc_scaling_text, pc_units
-    
-    
+        else:
+            print 'PC scaling method not recongnised'   
+            sys.exit(1)    
+        
+	return pcs, pc_scaling_text, pc_units 
+	
     def var_exp(self):
         """Calculates the variance explained"""
     
-        return solver.varianceFraction(neigs=self.neofs)
+        return self.solver.varianceFraction(neigs=self.neofs)
 
 
 def write_eofs(infile, eof_anal, outfile_name_eof, outfile_name_pc, eof_scaling):
@@ -150,11 +147,12 @@ def write_eofs(infile, eof_anal, outfile_name_eof, outfile_name_pc, eof_scaling)
     
     # Global attributes #
 
-    global_atts = {'title': 'Empirical Orthogonal Function analysis over %s region' %(region_name),
+    global_atts = {'title': 'Empirical Orthogonal Function analysis over %s region'  %(infile.region),
                    'contact': 'Damien Irving (d.irving@student.unimelb.edu.au)',
                    'reference': 'https://github.com/ajdawson/eof2',
-		   'history': '%s: Calculated EOF from %s using %s, format=NETCDF3_CLASSIC\n%s' %(datetime.now().strftime("%a %b %d %H:%M:%S %Y"),
-                   infile.fname,sys.argv[0],in_history)
+		   'companion_file': outfile_name_pc,
+		   'history': '%s: Calculated EOF from %s using %s, format=NETCDF3_CLASSIC\n%s'  %(datetime.now().strftime("%a %b %d %H:%M:%S %Y"),
+                   infile.fname, sys.argv[0], infile.history)
                   }
                       
     for key, value in global_atts.iteritems():
@@ -162,21 +160,21 @@ def write_eofs(infile, eof_anal, outfile_name_eof, outfile_name_pc, eof_scaling)
 
     # Variables #
 
-    axisInLat = outfile.copyAxis(eof_anal.data.getLatitude())  
-    axisInLon = outfile.copyAxis(eof_anal.data.getLongitude())  
+    outlat = outfile.copyAxis(eof_anal.data.getLatitude())  
+    outlon = outfile.copyAxis(eof_anal.data.getLongitude())  
     
-    for i in range(0,neofs):
-	var = eofs[i,:,:]
+    for i in range(0, eof_anal.neofs):
+	var = eofs[i, :, :]
 	var = cdms2.MV2.array(var)
 	var = var.astype(numpy.float32)
-	var.setAxisList([axisInLat,axisInLon])
+	var.setAxisList([outlat, outlon])
 	var.id = 'eof'+str(i+1)
 
 	var_atts = {'name': 'eof'+str(i+1),
                     'standard_name': 'eof'+str(i+1),
                     'units': eof_units, 
 		    'scaling': eof_scaling_text, 
-                    'variance_explained': '%7.4f' %(varfracs[i])
+                    'variance_explained': '%7.4f'  %(varfracs[i])
         	   } 
 
 	for key, value in var_atts.iteritems():
@@ -201,38 +199,39 @@ def write_pcs(infile, eof_anal, outfile_name_eof, outfile_name_pc, pc_scaling):
     years = []
     months = []
     
-    for ii in range(0,len(times)):
-        years.append(int(str(times[ii]).split('-')[0]))
-        months.append(int(str(times[ii]).split('-')[1]))
+    for i in range(0, len(times)):
+        years.append(int(str(times[i]).split('-')[0]))
+        months.append(int(str(times[i]).split('-')[1]))
     
     
     ## Write outfiles ##
     
-    for i in range(0,neofs):
+    for i in range(0, eof_anal.neofs):
         
 	# File name #
     
-        new_name = outfile_name_pc.replace('PC','PC'+str(i+1))
-        fout = open(new_name,'w')   
+        new_name = outfile_name_pc.replace('PC', 'PC'+str(i+1))
+        outfile = open(new_name, 'w')   
 	        
 	# Global attributes #
 	
-	fout.write('Title: Principle Component %s from EOF analysis over %s region \n' %(i+1,infile.region))
-        fout.write('Contact: Damien Irving (d.irving@student.unimelb.edu.au) \n')
-        fout.write('History: EOF calculated using eof2 cdat module \n')
-        fout.write('Reference: https://github.com/ajdawson/eof2 \n')
-        fout.write('Sourcefile: %s \n' %(infile,fname))
-	fout.write('Companion EOF file: %s \n' %(outfile_name_eof))
-        fout.write('Created %s using %s \n' %(datetime.now().strftime("%a %b %d %H:%M:%S %Y"), sys.argv[0]))
-	fout.write('Scaling: %s \n' %(pc_scaling_text))
-	fout.write('Units: %s \n' %(pc_units))
+	outfile.write('Title: Principle Component %s from EOF analysis over %s region \n'  %(i+1, infile.region))
+        outfile.write('Contact: Damien Irving (d.irving@student.unimelb.edu.au) \n')
+        outfile.write('History: EOF calculated using eof2 cdat module \n')
+        outfile.write('Reference: https://github.com/ajdawson/eof2 \n')
+        outfile.write('Sourcefile: %s \n'  %(infile.fname))
+	outfile.write('Companion EOF file: %s \n'  %(outfile_name_eof))
+        outfile.write('Created %s using %s \n'  %(datetime.now().strftime("%a %b %d %H:%M:%S %Y"), sys.argv[0]))
+	outfile.write('Scaling: %s \n'  %(pc_scaling_text))
+	outfile.write('Units: %s \n'  %(pc_units))
+	outfile.write('Variance explained: %7.4f \n'  %(varfracs[i]))
 	
 	# Data #
 	
-	fout.write(' YR   MON  PC%s \n' %(i+1)) 
+	outfile.write(' YR   MON  PC%s \n' %(i+1)) 
         ntime = numpy.shape(pcs)[0]
-	for t in range(0,ntime):
-            print >> fout, '%4i %3i %7.2f' %(years[t],months[t],pcs[t,i])
+	for t in range(0, ntime):
+            print >> outfile, '%4i %3i %7.2f'  %(years[t], months[t], pcs[t,i])
 
 
 def main(infile_name, var_id, outfile_name_eof, outfile_name_pc, neofs, region_name, time_bounds, eof_scaling, pc_scaling):
@@ -240,7 +239,7 @@ def main(infile_name, var_id, outfile_name_eof, outfile_name_pc, neofs, region_n
     
     infile = InputFile(infile_name)
     
-    eof_anal = EofAnalysis(infile.extract_data(var_id,region_name,time_bounds), neofs)
+    eof_anal = EofAnalysis(infile.extract_data(var_id, region_name, time_bounds), neofs)
     
     write_eofs(infile, eof_anal, outfile_name_eof, outfile_name_pc, eof_scaling)
     write_pcs(infile, eof_anal, outfile_name_eof, outfile_name_pc, pc_scaling)
@@ -253,19 +252,26 @@ if __name__ == '__main__':
     usage = "usage: %prog [options] {input_file} {input_variable} {output_file}"
     parser = OptionParser(usage=usage)
 
-    parser.add_option("-M", "--manual",action="store_true",dest="manual",default=False,help="output a detailed description of the program")
-    parser.add_option("-n", "--neofs",dest="neofs",type='int',default=5,help="Number of EOFs for output [default=5]")
-    parser.add_option("-r", "--region",dest="region",type='string',default=None,help="Region over which to calculate EOF [default=entire]")
-    parser.add_option("-t", "--time_bounds",dest="time_bounds",default=None,nargs=2,type='str',help="Period over which to calculate time mean [default = entire time period]")
-    parser.add_option("-e", "--eof_scaling",dest="eof_scaling",type='int',default=0,help="Scaling method applied to EOF post calculation [default = None]")
-    parser.add_option("-p", "--pc_scaling",dest="pc_scaling",type='int',default=0,help="Scaling method applied to EOF post calculation [default = None]")
+    parser.add_option("-M", "--manual", dest="manual", action="store_true", default=False,
+                       help="output a detailed description of the program")
+    parser.add_option("-n", "--neofs", dest="neofs", type='int', default=5,
+                       help="Number of EOFs for output [default=5]")
+    parser.add_option("-r", "--region", dest="region", type='string', default=None,
+                      help="Region over which to calculate EOF [default=entire]")
+    parser.add_option("-t", "--time_bounds", dest="time_bounds", type='str', nargs=2, default=None, 
+                      help="Period over which to calculate time mean [default = entire time period]")
+    parser.add_option("-e", "--eof_scaling", dest="eof_scaling", type='int', default=0,
+                      help="Scaling method applied to EOF post calculation [default = None]")
+    parser.add_option("-p", "--pc_scaling", dest="pc_scaling", type='int', default=0,
+                      help="Scaling method applied to EOF post calculation [default = None]")
     
-    (options, args) = parser.parse_args()            # Now that the options have been defined, instruct the program to parse the command line
+    (options, args) = parser.parse_args()            
 
     if options.manual == True or len(sys.argv) == 1:
         print """
         Usage:
-            eof_anal.py [-M] [-h] [-n] [-r] [-t] [-p] [-e] {input_file} {input_variable} {output_eof_file} {output_pc_file}
+            eof_anal.py [-M] [-h] [-n] [-r] [-t] [-p] [-e] {input_file} 
+	    {input_variable} {output_eof_file} {output_pc_file}
 
         Options
             -M -> Display this on-line manual page and exit
@@ -280,7 +286,7 @@ if __name__ == '__main__':
 	    The output PC files will take the user supplied file name and replace the string PC with PC1, PC2 etc 
         
 	Options
-	    region: eq_pacific, sth_hem, tropics
+	    region: eqpacific, sh, tropics
 	    
 	    eof_scaling:      
                 0 : Un-scaled EOFs (default)
