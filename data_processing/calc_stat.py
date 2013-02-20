40b061a49109f2dde4d10330163e10b716938b1d
Damien Irving <d.irving@student.unimelb.edu.au>
2013-02-20 14:43:46 +1100

    calc_stat.py now functional and compatable with netcdf_io.py

--- data_processing/calc_stat.py
+++ data_processing/calc_stat.py
@@ -40,15 +40,13 @@ import netcdf_io
 def temporal_axis_check(data1, data2):
     """Check whether the input data are on the same axis"""
     
-    # Ensure that both data have the same time axis #
-
     assert len(data1.getTime()) > 1.0
     assert len(data1.getTime()) == len(data2.getTime())    
 
     data1_hasspatial = 'yx' in data1.getOrder()
     data2_hasspatial = 'yx' in data2.getOrder()
 
-    if data1_hasspatial and 'yx' in data2_hasspatial:
+    if data1_hasspatial and data2_hasspatial:
         assert len(data1.getLatitude()) == len(data2.getLatitude())
         assert len(data1.getLongitude()) == len(data2.getLongitude())
 
@@ -67,34 +65,34 @@ def temporal_corr(data1, data2):
     
     data1_hasspat, data2_hasspat = temporal_axis_check(data1, data2)
 
-    primary_data, secondary_data = data2, data1 if (data2_hasspat 
-                                   and not data1_hasspat) else data1, data2
+    primary_data, secondary_data = (data2, data1) if (data2_hasspat \
+                                   and not data1_hasspat) else (data1, data2)
 
     if (data1_hasspat + data2_hasspat) >= 1:
-        output_axes = (primary_data.getTime(),
-                       primary_data.getLatitude(),
-                       primary_Data.getLongitude(),)
+        output_axes = (primary_data.getLatitude(),
+                       primary_data.getLongitude(),)
         nlats = len(primary_data.getLatitude())
         nlons = len(primary_data.getLongitude())
         tempcorr = numpy.zeros((nlats,nlons))
     
-	for y in range(0,nlats):
-	    for x in range(0,nlons):	
+	for y in xrange(nlats):
+	    for x in xrange(nlons):	
 		input1 = primary_data[:, y, x].compressed()
                 input2 = secondary_data[:, y, x].compressed() if (data1_hasspat 
                          + data2_hasspat) == 2 else secondary_data[:]
                 
                 if len(input1) == 0.0 or len(input2) == 0.0:
-	            tempcorr[ii,hh] = 1e20
+	            tempcorr[y ,x] = 1e20
 		else:
-	            tempcorr[ii,hh] = genutil.statistics.correlation(input1,
-                                      input2, centered=1, biased=1)
+	            tempcorr[y ,x] = genutil.statistics.correlation(input1,
+                                     input2, centered=1, biased=1)
 
-    else:
-        output_axes = primary_data.getTime()       
-        tempcorr = genutil.statistics.correlation(primary_data, 
-                   secondary_data, centered=1, biased=1)
-    
+    else:       
+        print genutil.statistics.correlation(primary_data, 
+              secondary_data, centered=1, biased=1)
+        sys.exit(0)
+
+
     attributes = {'id': 'tempcorr',
                   'long_name': 'temporal correlation',
                   'units': '',
@@ -106,32 +104,42 @@ def temporal_corr(data1, data2):
     return tempcorr, output_axes, attributes
     
 
-def main(metric, infile1, var1, infile2, var2, outfile, **selection):
+def main(metric, infile1, var1, infile2, var2, outfile, **kwargs):
     """Run the program."""
     
-    ## Read input data from both input files ##
+    # Prepate input data #
 
-    indata1 = netcdf_io.InputData(infile1, var1, **selection)
-    indata2 = netcdf_io.InputData(infile2, var2, **selection)
+    selectors = ['time', 'agg', 'region']
+    data_opts = {}
+    for item in selectors:
+        if kwargs[item]:
+            data_opts[item] = kwargs[item]
 
-    ## Calulcate the statistic
+    indata1 = netcdf_io.InputData(infile1, var1, **data_opts)
+    indata2 = netcdf_io.InputData(infile2, var2, **data_opts)
 
-    function_for_metric = {'tempcorr': tempcorr}
+    # Calulcate the statistic #
+
+    function_for_metric = {'tempcorr': temporal_corr}
     
     calc_stat = function_for_metric[metric]
-    stat, axes, atts = calc_stat(indata1, indata2)
+    stat, axes, atts = calc_stat(indata1.data, indata2.data)
 
-    ## Write the output file ##
+    # Write the output file #
  
-    netcdf_io.write_netcdf(outfile, metric, (indata1, indata2), (stat,), 
-                          (atts,), axes, clear_axes=True)   
+    indata_list = (indata1, indata2,)
+    outdata_list = (stat,)
+    outvar_atts_list = (atts,)
+    outvar_axes_list = (axes,)
+    
+    netcdf_io.write_netcdf(outfile, metric, 
+                           indata_list, outdata_list, 
+                           outvar_atts_list, outvar_axes_list, 
+                           clear_history=True)   
 
 
- 
 if __name__ == '__main__':
     
-    ## Help and manual information ##
-
     usage = "usage: %prog [options]"
     parser = OptionParser(usage=usage)
 
@@ -145,23 +153,23 @@ if __name__ == '__main__':
                       help="temporal aggregation selector")
 
 
-    (options, args) = parser.parse_args()            # Now that the options have been defined, instruct the program to parse the command line
+    (options, args) = parser.parse_args()     
 
-    if options.manual == True or len(args) != 5:
+    if options.manual == True or len(args) != 6:
 	print """
 	Usage:
             temporal_metrics.py [-M] {metric} {input_file1} {variable1} {input_file2} {variable2} {output_file}
 
 	Options:
-            -M or --manual      Display this on-line manual page and exit
-            -h or --help        Display a help/usage message and exit
-	    -r or --region      Spatial region over which to calculate statistic [default = entire]
-	    -t or --time        Time period selector [default = entire]. Tuple: (start_date, end_date)
-            -a or --agg         Temporal aggregation selector [default = None]. Tuple: (season, climatology)
-	    
+            -M or --manual     Display this on-line manual page and exit
+            -h or --help       Display a help/usage message and exit
+	    -r or --region     Spatial region over which to calculate statistic [default = entire]
+	    -t or --time       Time period selector [default = entire]. List/tuple: (start_date, end_date)
+            -a or --agg        Temporal aggregation selector [default = None]. List/tuple: (season, climatology)
+	   
 	Input arguments:
-	    {metric}            Temporal correlation ('tempcorr')
-	    {climatology}       True/False
+	    {metric}           Temporal correlation ('tempcorr')
+	    {climatology}      True/False
             {season}            
                     
 	Description:
@@ -176,8 +184,6 @@ if __name__ == '__main__':
 	sys.exit(0)
 
     else:
-    
-	##Read the input data and repeat back to user #
 
 	print 'Metric: ', args[0]
 	print 'Input file 1: ', args[1]
@@ -186,11 +192,6 @@ if __name__ == '__main__':
 	print 'Variable: ', args[4]
 	print 'Output file: ', args[5]
 
-	# Remove empty options #
-
-	for key in options:
-            if not options[key]:    
-        	del options[key]
-
-
-	main(args[0], args[1], args[2], args[3], args[4], args[5], options)
+	main(args[0], args[1], args[2], args[3], args[4], args[5], region = options.region,
+                                                                   time = options.time,
+                                                                   agg = options.agg)
