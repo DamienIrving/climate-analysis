fa75e4d011a20071cd606f281204bcb72a248f45
Damien Irving <damien_irving@yahoo.com.au>
2013-09-01 20:42:35 +1000

    Almost completed tests for angular distance

--- testing/unittest_vwind_rotation.py
+++ testing/unittest_vwind_rotation.py
@@ -33,21 +33,41 @@ class testAngularDistance(unittest.TestCase):
 
     def setUp(self):
         """Define the lat/lon data to be tested, ensuring all quadrants are 
-	covered."""
+        covered."""        
 
         self.lats = numpy.arange(-90.0, 120.0, 30.0)
-	self.lons = numpy.arange(0.0, 390.0, 30.0)  
+        self.lons = numpy.arange(0.0, 390.0, 30.0)  
+
+    def test_invalid_input(self):
+        """[test for failure]"""
+ 
+        pass                                       
+        #self.assertRaises(AssertionError, rot.rotation_matrix, 0.0, pi, 6.5)
 
     def test_zero_distance(self):
         """Test for the special case of no distance [test for success]"""
 	
-	for combo in itertools.product(self.lats, self.lons):
+        for combo in itertools.product(self.lats, self.lons):
             self.assertEqual(rot.angular_distance(combo[0], combo[1], combo[0], combo[1]), 0.0)	
 
-    def test_known_distances(self):
-        """Test for known distances"""
-	
-	pass
+    def test_known_distances_along_equator(self):
+        """Test for known distances along the equator [test for success]"""
+        
+        for lon1 in self.lons:
+            for lon2 in self.lons:
+                lon_diff = numpy.abs(numpy.rad2deg(lon1 - lon2))
+                dist = rot.angular_distance(0.0, lon1, 0.0, lon2)               
+                self.assertEqual(dist, lon_diff)	
+
+    def test_known_distances_along_meridians(self):
+        """Test for known distances along meridians [test for success]"""
+        
+        for lon in self.lons:
+            for lat1 in self.lats:
+                for lat2 in self.lats:
+                    lat_diff = numpy.abs(numpy.rad2deg(lat1 - lat2))
+                    dist = rot.angular_distance(lat1, lon, lat2, lon)               
+                    self.assertEqual(dist, lon_diff)	
 
 
 class testRotationAngle(unittest.TestCase):
@@ -56,20 +76,20 @@ class testRotationAngle(unittest.TestCase):
 
     def setUp(self):
         """Define the lat/lon data to be tested, ensuring all quadrants are 
-	covered."""
+        covered."""
 
         pass  
 
     def test_np_equal_sp(self):
         """Test for the special case where the pole hasn't moved [test for success]"""
+            
+        lat_axis = numpy.arange(-90, 95, 5)
+        lon_axis = numpy.arange(0, 365, 5)
+        lats, lons = nio.coordinate_pairs(lat_axis, lon_axis)
 	
-	lat_axis = numpy.arange(-90, 95, 5)
-	lon_axis = numpy.arange(0, 365, 5)
-	lats, lons = nio.coordinate_pairs(lat_axis, lon_axis)
-	
-	for comboAB in itertools.product(lats, lons):	
+        for comboAB in itertools.product(lats, lons):	
             angles = rot.rotation_angle(comboAB[0], comboAB[1], comboAB[0], comboAB[1], lats, lons, reshape=[len(lat_axis), len(lon_axis)])
-	    self.assertIsNone(numpy.testing.assert_allclose(angles, numpy.zeros([len(lat_axis), len(lon_axis)]), rtol=1e-07, atol=1e-07))
+            self.assertIsNone(numpy.testing.assert_allclose(angles, numpy.zeros([len(lat_axis), len(lon_axis)]), rtol=1e-07, atol=1e-07))
 
 
 
