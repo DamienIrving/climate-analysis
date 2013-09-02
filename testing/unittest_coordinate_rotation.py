47ff5976a428e490557a64493b438028793e1e41
Damien Irving <damien_irving@yahoo.com.au>
2013-09-02 17:25:05 +1000

    Improvements to testing code for rotation

--- testing/unittest_coordinate_rotation.py
+++ testing/unittest_coordinate_rotation.py
@@ -2,6 +2,7 @@
 A unit testing module for coordinate system rotation.
 
 Functions/methods tested:
+  coordinate_rotation.adjust_lon_range  
   coordinate_rotation.rotation_matrix
 
 """
@@ -19,6 +20,48 @@ sys.path.insert(0, module_dir)
 import coordinate_rotation as rot
 
 import pdb
+
+
+class testLonAdjust(unittest.TestCase):
+    """Test class for testing the adjustment of longitude values."""
+
+    def setUp(self):
+        """Define the test data."""        
+
+        self.data_degrees = numpy.array([0., 360., 378., 60., -30., -810.])
+        self.data_radians = numpy.array([0., 2*pi, 2.1*pi, pi/3., -pi/6., -4.5*pi])
+
+
+    def test_degrees_start0(self):
+        """Test for data in degrees, start point = 0.0 [test for success]"""
+        
+        result = rot.adjust_lon_range(self.data_degrees, radians=False, start=0.0)
+        answer = [0.0, 0.0, 18.0, 60.0, 330.0, 270.0] 
+        numpy.testing.assert_array_equal(result, answer)
+
+
+    def test_degrees_start180(self):
+        """Test for data in degrees, start point = -180 [test for success]"""
+
+        result = rot.adjust_lon_range(self.data_degrees, radians=False, start=-180.0)
+        answer = [0., 0., 18., 60., -30., -90.] 
+        numpy.testing.assert_array_equal(result, answer)	
+    
+    
+    def test_radians_start0(self):
+        """Test for data in radians, start point = 0.0 [test for success]"""
+
+        result = rot.adjust_lon_range(self.data_radians, start=0.0)
+        answer = [0., 0., 0.1*pi, pi/3., (11./6.)*pi, 1.5*pi] 
+        numpy.testing.assert_array_equal(result, answer)        
+        
+        
+    def test_radians_start180(self):
+        """Test for data in radians, start point = -pi [test for success]"""
+
+        result = rot.adjust_lon_range(self.data_radians, start=-pi)
+        answer = [0., 0., 0.1*pi, pi/3., -pi/6., -0.5*pi] 
+        numpy.testing.assert_array_equal(result, answer)	        
 
 
 class testTransformationMatrix(unittest.TestCase):
--- testing/unittest_vwind_rotation.py
+++ testing/unittest_vwind_rotation.py
@@ -10,7 +10,6 @@ import unittest
 
 from math import pi, sqrt
 import numpy
-import itertools
 
 import os
 import sys
@@ -36,7 +35,7 @@ class testAngularDistance(unittest.TestCase):
         covered."""        
 
         self.lats = numpy.arange(-90.0, 120.0, 30.0)
-        self.lons = numpy.arange(0.0, 390.0, 30.0)  
+        self.lons = numpy.arange(0.0, 390.0, 30.0)
 
     def test_invalid_input(self):
         """[test for failure]"""
@@ -71,122 +70,35 @@ class testAngularDistance(unittest.TestCase):
 
 
 class testRotationAngle(unittest.TestCase):
-    """Test class for testing the rotation angle associated
-    with rotating the meridional wind."""
+    """Test class for testing the angle associated with rotating 
+    the meridional wind. Tests focus on the original pole at 90N 0E.    
+    """
 
     def setUp(self):
-        """Define the lat/lon data to be tested, ensuring all quadrants are 
-        covered."""
+        """Define data to be tested."""
 
-        pass  
+        pass
+
 
     def test_np_equal_sp(self):
         """Test for the special case where the pole hasn't moved [test for success]"""
+
+        angles = rot.rotation_angle(90, 0, 90, 0, [-40, 90], [260, 0])
+        numpy.testing.assert_allclose(angles, [0., 0.], rtol=1e-07, atol=1e-07)
+
+
+    def test_plus90deg(self):
+        """Test for the special case of a +90 agree angle change [test for success]"""
+        
+        angles = rot.rotation_angle(90, 0, 0, 0, 0, 140)
+        numpy.testing.assert_allclose(angles, 90., rtol=1e-07, atol=1e-07)
+
             
-        lat_axis = numpy.arange(-90, 95, 5)
-        lon_axis = numpy.arange(0, 365, 5)
-        lats, lons = nio.coordinate_pairs(lat_axis, lon_axis)
-	
-        for comboAB in itertools.product(lats, lons):	
-            angles = rot.rotation_angle(comboAB[0], comboAB[1], comboAB[0], comboAB[1], lats, lons, reshape=[len(lat_axis), len(lon_axis)])
-            self.assertIsNone(numpy.testing.assert_allclose(angles, numpy.zeros([len(lat_axis), len(lon_axis)]), rtol=1e-07, atol=1e-07))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- #   def setUp(self):
-#        """Define the sets of Euler angles (phi, theta, psi) to be tested"""
-#
-#        self.angles = [[0.0, 0.0, 0.0],
-#                       [2 * pi, 2 * pi, 2 * pi],
-#                       [pi, pi, pi],
-#                       [0.0, 0.5 * pi, 0.3*pi],
-#                       [0.4 * pi, 1.2 * pi, 1.7 * pi]]
-#
-#                        
-#    def test_no_rotation(self):
-#        """Test no rotation for psi = theta = psi = 0.0 [test for success]"""
-#
-#        for phi, theta, psi in self.angles[0:2]:
-#            for inv in [True, False]:
-#                result = rot.rotation_matrix(phi, theta, psi, inverse=inv)
-#                self.assertIsNone(numpy.testing.assert_allclose(result, numpy.identity(3), rtol=1e-07, atol=1e-07)) 
-#                # an alternative is numpy.testing.assert_array_almost_equal()
-#
-#
-#    def test_known_value(self):
-#        """Test the rotation matrix for a known answer (derived by hand) [test for success]"""
-#        
-#	phi, theta, psi = [pi / 4, pi / 3, pi / 6]
-#        result = rot.rotation_matrix(phi, theta, psi, inverse=False)
-#        
-#	a = sqrt(3.0) / 2
-#	b = 0.5
-#	c = 1 / sqrt(2.0)
-#	answer = numpy.empty([3,3])
-#	answer[0, 0] = (a * c) - (b * c * b)
-#	answer[0, 1] = (a * c) + (b * c * b)
-#	answer[0, 2] = b * a
-#	answer[1, 0] = -(b * c) - (b * c * a)
-#	answer[1, 1] = -(b * c) + (b * c * a)
-#	answer[1, 2] = a * a
-#	answer[2, 0] = a * c
-#	answer[2, 1] = -(a * c)
-#	answer[2, 2] = b
-#
-#        self.assertIsNone(numpy.testing.assert_allclose(result, answer, rtol=1e-07, atol=1e-07)) 
-#
-#
-#    def test_known_inverse(self):
-#        """Test the inverse rotation matrix for a known answer (derived by hand) [test for success]"""
-#        
-#	phi, theta, psi = [pi / 4, pi / 3, pi / 6]
-#        result = rot.rotation_matrix(phi, theta, psi, inverse=True)
-#        
-#	a = sqrt(3.0) / 2
-#	b = 0.5
-#	c = 1 / sqrt(2.0)
-#	answer = numpy.empty([3,3])
-#	answer[0, 0] = (a * c) - (b * c * b)
-#	answer[0, 1] = -(b * c) - (b * c * a)
-#	answer[0, 2] = a * c
-#	answer[1, 0] = (a * c) + (b * c * b)
-#	answer[1, 1] = -(b * c) + (b * c * a)
-#	answer[1, 2] = -(a * c)
-#	answer[2, 0] = a * b
-#	answer[2, 1] = a * a
-#	answer[2, 2] = b
-#
-#        self.assertIsNone(numpy.testing.assert_allclose(result, answer, rtol=1e-07, atol=1e-07)) 
-#
-#
-#    def test_invalid_input(self):
-#        """Test input angles outside of [0, 2*pi] [test for failure]"""
-#
-#        self.assertRaises(AssertionError, rot.rotation_matrix, 0.0, pi, 6.5)
-#
-#
-#    def test_identity(self):
-#        """The transformation matrix, multiplied by its inverse, 
-#        should return the identity matrix [test for sanity]"""
-#
-#        for phi, theta, psi in self.angles:
-#            A = rot.rotation_matrix(phi, theta, psi, inverse=False)
-#            Ainv = rot.rotation_matrix(phi, theta, psi, inverse=True)
-#            product = numpy.dot(A, Ainv)
-#
-#            self.assertIsNone(numpy.testing.assert_allclose(product, numpy.identity(3), rtol=1e-07, atol=1e-07))
-#
+    def test_minus90deg(self):
+        """Test for the special case of a -90 agree angle change [test for success]"""
+        
+        angles = rot.rotation_angle(90, 0, 0, 270, 0, 190)
+        numpy.testing.assert_allclose(angles, -90., rtol=1e-07, atol=1e-07)
 
 
 if __name__ == '__main__':
