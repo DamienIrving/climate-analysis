95a88765b87510d8cf02a296090c10104f06d17d
Damien Irving <dbirving@student.unimelb.edu.au>
2013-11-29 12:46:31 +1100

    Added annual timescale to calc_eof.sh and plot_EOF.sh

--- data_processing/calc_eof.sh
+++ data_processing/calc_eof.sh
@@ -1,11 +1,20 @@
 path=/mnt/meteo0/data/simmonds/dbirving/Merra/data/processed
-infile=sf_Merra_250hPa_monthly_native.nc
+infile=sf_Merra_250hPa_monthly-anom-wrt-1979-2011_native.nc
 cdat=/usr/local/uvcdat/1.2.0/bin/cdat
+
+# Seasons
 
 for region in shextropics20 shextropics30 sh
 do
     for season in DJF MAM JJA SON
-    do
-        ${cdat} calc_eof.py --region ${region} --time 1979-01-01 2013-01-01 ${season} --eof_scaling 3 --pc_scaling 1 ${path}/${infile} sf ${path}/eof-sf_Merra_250hPa_monthly-${season}_native-${region}.nc
+    do 
+	${cdat} calc_eof.py --region ${region} --time 1979-01-01 2013-01-01 ${season} --eof_scaling 3 --pc_scaling 1 ${path}/${infile} sf ${path}/eof-sf_Merra_250hPa_monthly-anom-wrt-1979-2011-${season}_native-${region}.nc
     done
+done
+
+# Annual
+
+for region in shextropics20 shextropics30 sh
+do
+    ${cdat} calc_eof.py --region ${region} --time 1979-01-01 2013-01-01 none --eof_scaling 3 --pc_scaling 1 ${path}/${infile} sf ${path}/eof-sf_Merra_250hPa_monthly-anom-wrt-1979-2011-annual_native-${region}.nc
 done
--- visualisation/plot_EOF.sh
+++ visualisation/plot_EOF.sh
@@ -3,11 +3,11 @@ cdat=/usr/local/uvcdat/1.2.0/bin/cdat
 
 for region in shextropics20 shextropics30 sh
 do
-    for season in DJF MAM JJA SON
+    for season in annual #DJF MAM JJA SON
     do
-        infile=${path}/eof-sf_Merra_250hPa_monthly-${season}_native-${region}.nc
-	outfile=${path}/figures/eof-sf_Merra_250hPa_monthly-${season}_native-${region}.png
-        ${cdat} plot_EOF.py ${infile} ${region} --ofile ${outfile} --ticks -1 -0.8 -0.6 -0.4 -0.2 0.0 0.2 0.4 0.6 0.8 1.0
+        infile=${path}/eof-sf_Merra_250hPa_monthly-anom-wrt-1979-2011-${season}_native-${region}.nc
+	outfile=${path}/figures/eof-sf_Merra_250hPa_monthly-anom-wrt-1979-2011-${season}_native-${region}.png
+        echo ${cdat} plot_EOF.py ${infile} ${region} --ofile ${outfile} --ticks -1 -0.8 -0.6 -0.4 -0.2 0.0 0.2 0.4 0.6 0.8 1.0
     done
 done
 
