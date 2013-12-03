124ca015cacc70aa3bbd8750b5b5992c1e530cce
Damien Irving <d.irving@student.unimelb.edu.au>
2013-12-03 11:25:51 +1100

    Refactored plot_envelope.py and made sf and wind plotting optional

--- visualisation/plot_envelope.py
+++ visualisation/plot_envelope.py
@@ -23,30 +23,32 @@ import cdms2
 import MV2
 
 
-def main(inargs):
-    """Create the plot"""
+def extract_data(inargs):
+    """Extract input data and check that time axes match"""
 
-    # Read input data #
+    env_data = nio.InputData(inargs.env_file, inargs.env_var, 
+                             **nio.dict_filter(vars(inargs), ['time', 'region']))
+    env_times = env_data.data.getTime().asComponentTime()    
 
-    indata_env = nio.InputData(inargs.env_file, inargs.env_var, 
-                               **nio.dict_filter(vars(inargs), ['time',]))
-    indata_u = nio.InputData(inargs.u_file, inargs.u_var, 
-                             **nio.dict_filter(vars(inargs), ['time',]))
-    indata_v = nio.InputData(inargs.v_file, inargs.v_var, 
-                             **nio.dict_filter(vars(inargs), ['time',]))
-    indata_sf = nio.InputData(inargs.sf_file, inargs.sf_var, 
-                              **nio.dict_filter(vars(inargs), ['time',]))
+    opt_data = {}
+    for opt in ['uwind', 'vwind', 'sf']:    
+        try:
+            opt_data[opt] = nio.InputData(eval('inargs.'+opt+'[0]'), 
+                                          eval('inargs.'+opt+'[1]'), 
+                                          **nio.dict_filter(vars(inargs), ['time', 'region']))
+        except AttributeError:
+            opt_data[opt] = None
+    
+    if opt_data[opt]:
+        assert opt_data[opt].data.getTime().asComponentTime() == env_times, \
+        'Time axes must be the same for all input data'
 
-    env_times = indata_env.data.getTime().asComponentTime()
-    u_times = indata_u.data.getTime().asComponentTime()
-    v_times = indata_v.data.getTime().asComponentTime()
-    sf_times = indata_sf.data.getTime().asComponentTime()
+    return env_data, opt_data['uwind'], opt_data['vwind'], opt_data['sf']
 
-    assert env_times == u_times == v_times == sf_times, \
-    'Time axis must be the same for all input data'
 
-    # Restore env data to standard lat/lon grid #
-
+def restore_env(indata_env, inargs):
+    """Restore the rotated envelope data to a typical lat/lon grid"""
+    
     lat_axis = indata_env.data.getLatitude()
     lon_axis = indata_env.data.getLongitude()
     lats, lons = nio.coordinate_pairs(lat_axis[:], lon_axis[:])
@@ -64,6 +66,20 @@ def main(inargs):
         axis_list = [lat_axis, lon_axis]
     
     env_restored = cdms2.createVariable(env_restored, grid=grid, axes=axis_list)
+    
+    return env_restored
+ 
+
+def main(inargs):
+    """Create the plot"""
+
+    # Read input data #
+
+    indata_env, indata_u, indata_v, indata_sf = extract_data(inargs) 
+
+    # Restore env data to standard lat/lon grid #
+
+    env_restored = restore_env(indata_env, inargs)
 
     # Plot every time step #
    
@@ -78,31 +94,32 @@ def main(inargs):
 	quiv_scale = 300
 	quiv_width = 0.002
 
-    for date in env_times:
+    for date in indata_env.data.getTime().asComponentTime():
         year, month, day = str(date).split(' ')[0].split('-')
 	if inargs.timescale == 'monthly':
 	    date_abbrev = year+'-'+month
 	else:
 	    date_abbrev = year+'-'+month+'-'+day
 	    
-        env_data = env_restored(time=(date, date_abbrev), squeeze=1)
-        u_data = indata_u.data(time=(date, date_abbrev), squeeze=1)
-        v_data = indata_v.data(time=(date, date_abbrev), squeeze=1)
-        sf_data = indata_sf.data(time=(date, date_abbrev), squeeze=1)
+        env_data = [env_restored(time=(date, date_abbrev), squeeze=1),]
+        u_data = [indata_u.data(time=(date, date_abbrev), squeeze=1),] if indata_u else None
+        v_data = [indata_v.data(time=(date, date_abbrev), squeeze=1),] if indata_v else None
+        sf_data = [indata_sf.data(time=(date, date_abbrev), squeeze=1),] if indata_sf else None
 
         title = 'Wave envelope, 250hPa wind & sf anomaly, %s' %(date_abbrev)
         ofile = '%s_%s.png' %(inargs.ofile, date_abbrev)
 
-        plot_map.multiplot([env_data,],
+        plot_map.multiplot(env_data,
 		           ofile=ofile,
 		           title=title,
+			   region=inargs.region,
 		           units='$m s^{-1}$',
                            draw_axis=True,
 		           delat=30, delon=30,
 		           contour=True,
 		           ticks=tick_list, discrete_segments=inargs.segments, colourbar_colour=inargs.palette,
-        	           contour_data=[sf_data,], contour_ticks=inargs.sf_ticks,
-		           uwnd_data=[u_data,], vwnd_data=[v_data,], quiver_thin=9, key_value=keyval,
+        	           contour_data=sf_data, contour_ticks=inargs.sf_ticks,
+		           uwnd_data=u_data, vwnd_data=v_data, quiver_thin=9, key_value=keyval,
 		           quiver_scale=quiv_scale, quiver_width=quiv_width,
         	           projection=inargs.projection, 
         	           extend='max',
@@ -110,8 +127,8 @@ def main(inargs):
         	           image_size=inargs.image_size)
         
         tick_list = tick_list[0: -1]   # Fix for weird thing where it keeps appending to 
-	                                     # the end of the ticks list, presumable due to the 
-					     # extend = 'max' 
+	                               # the end of the ticks list, presumably due to the 
+				       # extend = 'max' 
 
 
 if __name__ == '__main__':
@@ -140,14 +157,16 @@ example (abyss.earthsci.unimelb.edu.au):
     parser.add_argument("np_lon", type=float, help="north pole longitude used for envelope extraction")
     parser.add_argument("pm_lat", type=float, help="prime meridian point latitude used for envelope extraction")
     parser.add_argument("pm_lon", type=float, help="prime meridian point longitude used for envelope extraction")
-    parser.add_argument("u_file", type=str, help="zonal wind anomaly file")
-    parser.add_argument("u_var", type=str, help="zonal wind anomaly variable")
-    parser.add_argument("v_file", type=str, help="meridional wind anomaly file")
-    parser.add_argument("v_var", type=str, help="meridional wind anomaly variable")
-    parser.add_argument("sf_file", type=str, help="streamfunction anomaly file")
-    parser.add_argument("sf_var", type=str, help="streamfunction anomaly variable")
-    parser.add_argument("ofile", type=str, help="name of output file (without the file ending - date will be tacked on)")
     parser.add_argument("timescale", type=str, choices=['daily', 'monthly'], help="timescale of the input data")
+    
+    parser.add_argument("--uwind", type=str, nargs=2, metavar=('FILE', 'VAR'),
+                        help="zonal wind anomaly file and variable")
+    parser.add_argument("--vwind", type=str, nargs=2, metavar=('FILE', 'VAR'),
+                        help="meridional wind anomaly file and variable")
+    parser.add_argument("--sf", type=str, nargs=2, metavar=('FILE', 'VAR'),
+                        help="streamfunction anomaly file and variable")
+    parser.add_argument("--ofile", type=str, default='test', 
+                        help="name of output file (without the file ending - date will be tacked on)")
     
     parser.add_argument("--time", type=str, nargs=3, metavar=('START_DATE', 'END_DATE', 'MONTHS'),
                         help="Time period [default = entire]")
@@ -162,6 +181,8 @@ example (abyss.earthsci.unimelb.edu.au):
                         help="Map projection [default: nsper]")
     parser.add_argument("--image_size", type=float, default=9, 
                         help="size of image [default: 9]")
+    parser.add_argument("--region", type=str, choices=nio.regions.keys(), default='world-psa',
+                        help="name of region to plot [default: world-psa]")
 
     parser.add_argument("--sf_ticks", type=float, nargs='*', default=[-30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30], 
                         help="list of tick marks for sf contours, or just the number of contour lines")
