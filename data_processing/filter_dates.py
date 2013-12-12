78d5806db9775dc7d97afb359dc5a1ceea9fc13e
Damien Irving <damien_irving@yahoo.com.au>
2013-12-12 14:32:07 +1100

    removed totals functionality from filter_dates.py - it is now only a filter

--- data_processing/filter_dates.py
+++ data_processing/filter_dates.py
@@ -1,7 +1,7 @@
 """
 Filename:     filter_dates.py
 Author:       Damien Irving, d.irving@student.unimelb.edu.au
-Description:  
+Description:  Take a list of dates and filter according to a supplied dataset
 
 """
 
@@ -48,14 +48,8 @@ def west_antarctica_filter(date_list, infile, var, threshold, select):
     
     # Generate the new date list
     new_date_list = output_selection.getTime().asComponentTime()
-    return map(lambda x: x.split()[0], new_date_list)
-
-
-def calc_totals():
-    """Calculate the total number of days for each month or season
-    (expressed as a percentage of the total number of days in the month/season)
-    """
     
+    return map(lambda x: x.split()[0], new_date_list)
 
 
 def read_dates(infile):
@@ -78,21 +72,14 @@ def main(inargs):
 
     # Perform additional filtering
     filters = {'west_antarctica': west_antarctica_filter,}
-    if inargs.filter.lower() != 'none':
-        filter_func = filters[inargs.filter]
-        new_date_list = stat_func(inargs.dates, 
-                                  inargs.data[0], inargs.data[1], 
-                                  inargs.threshold, inargs.selection)
-    else:
-        new_date_list = inargs.dates
-    
-    # Return the desired output type
-    if inargs.output == 'new_list':
-        write_dates(inargs.outfile, new_date_list)
-    else:
-        calc_totals()
-    
 
+    filter_func = filters[inargs.filter]
+    new_date_list = stat_func(inargs.dates, 
+                              inargs.infile, inargs.var, 
+                              inargs.threshold, inargs.selection)
+    
+    # Write output file
+    write_dates(inargs.outfile, new_date_list)
 
 
 if __name__ == '__main__':
@@ -105,28 +92,28 @@ author:
 
 """
 
-    description='Description'
+    description='Take a list of dates and filter according to a supplied dataset'
     parser = argparse.ArgumentParser(description=description,
                                      epilog=extra_info, 
                                      argument_default=argparse.SUPPRESS,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    parser.add_argument("dates", type=str, help="File containing dates of interest (one date per line)")
-    parser.add_argument("filter", type=str, choices=('none', 'west_antarctica'),  
+    parser.add_argument("dates", type=str, 
+                        help="File containing dates of interest (one date per line)")
+    parser.add_argument("filter", type=str, choices=('west_antarctica',),  
                         help="Method for filtering the list of dates (can be none)")
-    parser.add_argument("output", type=str, choices=('new_list', 'monthly_totals', 'seasonal_totals'), 
-                        help="Desired output")
-    
-    parser.add_argument("--data", type=str, nargs=2, metavar=('FILE', 'VARIABLE'),
-                        help="Data to be used for the filtering method")
-    parser.add_argument("--threshold", type=float, default=0.0,  
+    parser.add_argument("infile", type=str,
+                        help="File containing data to be used for the filtering method")
+    parser.add_argument("var", type=str,
+                        help="Variable to extract from file")
+    parser.add_argument("threshold", type=float,  
                         help="Threshold value against which the selection is made")
-    parser.add_argument("--selection", type=str, choices=('above', 'below'), default='above',  
+    parser.add_argument("selection", type=str, choices=('above', 'below'),  
                         help="Segment of the selection to retain (i.e. above or below threshold)")   
+
     parser.add_argument("--outfile", type='str', default='test.txt',
                         help="Name of output file")   
                         
     args = parser.parse_args()            
-
     main(args)
 
