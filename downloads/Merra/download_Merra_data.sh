43181665607b718c048cb52ee2d19aca6415a370
Damien Irving <dbirving@student.unimelb.edu.au>
2013-12-16 09:31:24 +1100

    Added us and tas wget files to download directory

--- downloads/Merra/download_Merra_data.sh
+++ downloads/Merra/download_Merra_data.sh
@@ -1,10 +1,11 @@
 
-for script in wget*
+# Download the data
+for script in $*
 do
     wget --content-disposition -i $script
 done
 
-
+# Merge all the individual files into one
 for year in {1979..2013}
 do
     cdo mergetime *.${year}????.* MERRA.prod.assim.tavg1_2d_slv_Nx.${year}.SUB.nc
