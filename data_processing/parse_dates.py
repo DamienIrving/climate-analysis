4d4948fb4c909c6e60a9b903c5e27518249e7ff2
Damien Irving <damien_irving@yahoo.com.au>
2013-12-15 15:14:56 +1100

    parse_dates.py now prints summary statistics to screen

--- data_processing/parse_dates.py
+++ data_processing/parse_dates.py
@@ -5,28 +5,104 @@ Description:  Parse a list of dates and output a simple statistic
 
 """
 
-from datetime import datetime
+import datetime
+from dateutil.rrule import *
+from matplotlib.dates import date2num
+import calendar
 
+import numpy
 import argparse
 
-filter_dir = os.path.join(os.environ['HOME'], 'phd', 'data_processing')
-sys.path.insert(0, filter_dir)
-from filter_dates import read_dates
+import os
+import sys
+module_dir = os.path.join(os.environ['HOME'], 'phd', 'modules')
+sys.path.insert(0, module_dir)
+import general_io as gio
 
-def main():
+
+def bin_dates(date_list, timescale, start, end):
+    """Take a list of dates and return totals in bins, according to 
+    the requested timescale.
+    
+    Arguments:
+      start  -- tuple: (year, month)
+      end    -- tuple: (year, month) 
+    
+    """
+        
+    dt_list = map(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'), date_list)
+    num_list = map(date2num, dt_list)
+    
+    start_dt = datetime.datetime(start[0], start[1], 1)
+    end_dt = datetime.datetime(end[0], start[1], 1) + datetime.timedelta(months=1)
+    dt_bin_edges = rrule(MONTHLY, dtstart=start_dt, until=end_dt) #interval=1
+    num_bin_edges = map(date2num, dt_bin_edges)
+    
+    hist_data, edges = numpy.histogram(num_list, bins=num_bin_edges)
+    assert len(hist_data) == len(dt_bin_edges[:-1])
+
+    histogram = {}
+    for i in range(0, len(hist_data)):
+        histogram[dt_bin_edges[i]] = hist_data[i]
+    
+    return histogram
+
+
+def print_summary(histogram, dt_labels):
+    """Print summary statistics to the screen"""
+
+    monthly_totals = dict((month, 0) for month in range(1,13))
+    for key, value in historgram.iteritems():
+        monthly_totals[key.month] = monthly_totals[key.month] + value
+
+    print 'Total days'
+    for i in range(1,13):
+        print calendar[i], monthly_totals[i]
+
+     
+def main(inargs):
     """Run the program"""
 
-    date_list = read_dates(inargs.dates)
+    # Read in the date list
+    date_list = gio.read_dates(inargs.dates)
+     
+    # Put the data into monthly or seasonal bins
+    histogram = bin_dates(date_list, inargs.totals, inargs.start, inargs.end)
+    
+    # Print summary stats to screen
+    print_summary(histogram)
+    
 
-    month_selection = {}
-    month_selection['DJF'] = (12, 1, 2)
-    month_selection['MAM'] = (3, 4, 5)
-    month_selection['JJA'] = (6, 7, 8)
-    month_selection['SON'] = (9, 10, 11)
+if __name__ == '__main__':
 
-    dt_list = map(lambda x: datetime.strptime(x, '%Y-%m-%d'), date_list)
+    extra_info =""" 
+example:
+    python parse_dates.py 
+    hov-vrot-env-w567_Merra_250hPa_daily-anom-wrt-1979-2012_y181x360_np20-260_absolute14_lon225-335_dates.txt 
 
-    # months = times_dt.map(lambda x: x.month)
-    # season_selection = (months.map(lambda val: val in month_selection[season]))
-    # combined_selection = combined_selection & season_selection
+note:
+    Assumes daily or higher timescale input data
+    
+author:
+    Damien Irving, d.irving@student.unimelb.edu.au
+
+"""
+
+    description='Parse a list of dates and output various statistics'
+    parser = argparse.ArgumentParser(description=description,
+                                     epilog=extra_info, 
+                                     argument_default=argparse.SUPPRESS,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    # Required arguments
+    parser.add_argument("dates", type=str, help="Name of input file containing the list of dates")
+    
+    # Time filters
+    parser.add_argument("--start", type=int, nargs=2, metavar=('YEAR', 'MONTH'), default=(1979, 1), 
+                        help="Time start filter (e.g. 1979 1)")
+    parser.add_argument("--end", type=int, nargs=2, metavar=('YEAR', 'MONTHS'), default=(2012, 12),
+                        help="Time end filter (e.g. 2012 12)")
+
+    args = parser.parse_args()            
+    main(args)
     
