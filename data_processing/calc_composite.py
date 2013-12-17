209e3d21e2e82672195b22fb42363da7657e2b1b
Damien Irving <dbirving@student.unimelb.edu.au>
2013-12-17 16:56:14 +1100

    Initial commit of re-configured calc_composite (not complete)

--- data_processing/calc_composite.py
+++ data_processing/calc_composite.py
@@ -14,9 +14,30 @@ import os
 
 import argparse
 
-module_dir = os.path.join(os.environ['HOME'], 'modules')
+module_dir = os.path.join(os.environ['HOME'], 'phd, 'modules')
 sys.path.insert(0, module_dir)
+import general_io as gio
 import netcdf_io as nio
+
+
+def calc_composite():
+    """Calculate the composite"""
+    
+    # Perform the t-test 
+    # To perform a Welch's t-test (two independent samples, unequal variances), need the 
+    # latest version of scipy in order to use the equal_var keyword argument
+	
+    t, p_vals = stats.ttest_ind(data_in_composite, data_out_composite, axis=0) #equal_var=False) 
+
+    # Perform necessary averaging #
+
+#    count = numpy.shape(composite)[0]
+#        
+#    final_composite = MV2.average(composite, axis=0) if average else composite
+#    final_composite.count = count
+
+    return data_in_composite, p_vals
+
 
 
 def main(inargs):
@@ -26,23 +47,22 @@ def main(inargs):
 
     indata = nio.InputData(inargs.infile, inargs.var, 
                            **nio.dict_filter(vars(inargs), ['time', 'region']))
-    index  = nio.InputData(inargs.index_file, inargs.index_var, 
-                           **nio.dict_filter(vars(inargs), ['time', 'region']))
 
-    # Calculate the composites #
+    date_list = gio.read_dates(inargs.dates)
 
-    if inargs.bound == 'between':
-        assert len(inargs.limit) == 2, \
-        """If the bound is 'between', two limits (LOWER, UPPER) must be supplied"""
-        limit = (inargs.limit[0], inargs.limit[1])
-    else:
-        limit = inargs.limit[0]
-
+    # Filter the data #
     
-    
-    composite, p_val = indata.temporal_composite(index.data, average=True, season=inargs.season, limit=limit, 
-                                                 bound=inargs.bound, method=inargs.method,
-					         normalise=inargs.normalise, remove_ave=inargs.remove_ave)
+    matching_dates_included = nio.match_dates(date_list, indata.data.getTime().asComponentTime())
+    matching_dates_excluded = nio.match_dates(date_list, indata.data.getTime().asComponentTime(), invert=True)
+
+    data_included = nio.temporal_extract(indata.data, matching_dates_included, indexes=False)
+    data_excluded = nio.temporal_extract(indata.data, matching_dates_excluded, indexes=False)
+
+    # Calculate the composite #
+        
+    composite, p_val = calc_composite(data_included, data_excluded)
+	
+    # Write the output file #
 
     composite_atts = {'id': inargs.var,
                       'long_name': indata.data.long_name,
@@ -54,8 +74,6 @@ def main(inargs):
                  'units': ' ',
                  'history': """Standard independent two sample t-test comparing the data sample that meets the composite criteria to a sample containing the remaining data""",
                  'reference': 'scipy.stats.ttest_ind(a, b, axis=t, equal_var=True)'}
-   
-    # Write output file #
 
     indata_list = [indata, index]
     outdata_list = [composite, p_val]
@@ -76,11 +94,8 @@ if __name__ == '__main__':
 
     extra_info =""" 
 example (abyss.earthsci.unimelb.edu.au):
-  /usr/local/uvcdat/1.2.0rc1/bin/cdat calc_composite.py 
-  /work/dbirving/datasets/Merra/data/processed/ts_Merra_surface_monthly-anom-wrt-1981-2010_native.nc ts 
-  /work/dbirving/processed/indices/data/sf_Merra_250hPa_EOF_monthly-1979-2011_native-sh.nc pc2 
-  /work/dbirving/processed/composites/data/ts-sf_Merra_surface-250hPa_monthly-anom-wrt-1981-2010-pc2_native_composite-1979-2011.nc 
-  --time 1979-01-01 2011-12-31 --method std --limit -1.0 --bound lower
+  cdat calc_composite.py 
+
 
 author:
   Damien Irving, d.irving@student.unimelb.edu.au
@@ -95,28 +110,13 @@ author:
 
     parser.add_argument("infile", type=str, help="Input file name")
     parser.add_argument("var", type=str, help="Input file variable")
-    parser.add_argument("index_file", type=str, help="Input index file name")
-    parser.add_argument("index_var", type=str, help="Input index file variable")
+    parser.add_argument("date_file", type=str, help="List of dates to be included in composite")
     parser.add_argument("outfile", type=str, help="Output file name")
 
-    parser.add_argument("--season", type=str, choices=['ann', 'djf', 'mam', 'jja', 'son'], default='ann',
-                        help="Season for which to calculate the composite [default: ann]")	
     parser.add_argument("--region", type=str, choices=nio.regions.keys(),
                         help="Region over which to calculate the composite [default: entire]")
-    parser.add_argument("--time", type=str, nargs=2, metavar=('START_DATE', 'END_DATE'),
-                        help="Time period over which to calculate the composite [default: entire]")
-
-    parser.add_argument("--method", type=str, choices=[None, 'std',], default=None,
-                        help="Method for determining the composite threshold [default: None]")
-    parser.add_argument("--limit", type=float, nargs='*', default=1.0,
-                        help="Value applied to that method (e.g. 1.0 standard deviations)")
-    parser.add_argument("--bound", type=str, choices=['upper', 'lower', 'between'], default = 'upper',
-                        help="Indicates what type of bound the limit is [default: upper]")
-
-    parser.add_argument("--normalise", action='store_true', default=False,
-                        help="normalise the data before calculating the composite [default: False]")
-    parser.add_argument("--remove_ave", action='store_true', default=False,
-                        help="remove average in the normalisation procedure [default: False]")
+    parser.add_argument("--time", type=str, nargs=3, metavar=('START_DATE', 'END_DATE', 'MONTHS'),
+                        help="Time period over which to calculate the composite [default = entire]")
 
     args = parser.parse_args()            
 
