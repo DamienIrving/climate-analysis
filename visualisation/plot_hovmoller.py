c15c5d6e3a217ee36ed711954826df4b9012beec
Damien Irving <dbirving@student.unimelb.edu.au>
2014-01-07 09:20:03 +1100

    Added longitude bounds option to plot_hovmoller.py

--- visualisation/plot_hovmoller.py
+++ visualisation/plot_hovmoller.py
@@ -13,7 +13,6 @@ import iris.unit
 import datetime
 
 import argparse
-import pdb 
 
 import math
 
@@ -28,103 +27,6 @@ timescales = {'YEARLY': ['%y', 'mdates.YearLocator'],
 
 recognised_units = {'ms-1': '$m s^{-1}$',
                     'm s-1': '$m s^{-1}$'}
-
-
-#def _decimal_places(diff):
-#    """Determine the decimal place rounding for a 
-#    given difference between min and max ticks."""
-#    
-#    if diff > 100.0:
-#        dec = 0
-#    elif diff > 5.0:
-#        dec = 1
-#    else:
-#        dec = math.floor(math.fabs(math.log10(diff)) + 2)
-#    
-#    return int(dec)
-#
-#
-#def _set_colourbar(data, colourbar_colour, ticks, discrete_segments, extend):
-#    """Set the colourbar."""
-#
-#    #Setup colour map (unless custom which is created later)
-#    continuous_colourbar = False if discrete_segments or ticks else True
-#    
-#    if colourbar_colour:
-#        if hasattr(plt.cm, colourbar_colour):
-#            colourmap = getattr(plt.cm, colourbar_colour)
-#        elif(isinstance(colourbar_colour, matplotlib.colors.LinearSegmentedColormap)):
-#            colourmap = colourbar_colour
-#        else:
-#            print "Error, color option '", colourbar_colour, "' not a valid option"
-#            sys.exit(1)
-#            
-#    #Get the min/max level for colourbar settings
-#    if ticks:
-#        min_level = ticks[0]
-#        max_level = ticks[-1]
-#    else:
-#        max_level = numpy.max(data)
-#        min_level = numpy.min(data)
-#        diff = max_level - min_level
-#        if isinstance(discrete_segments, list):
-#            step = diff / float(len(discrete_segments))
-#        elif discrete_segments:
-#            step = diff / float(discrete_segments)
-#        else:
-#            step = diff / 10.0
-#            
-#        ticks = list(numpy.arange(min_level, max_level+(step/2), step))
-#
-#    dec = _decimal_places(max_level - min_level)
-#    space = abs(ticks[0] - ticks[1])*0.2
-#    
-#    if extend == 'both':
-#        min_level = min_level - space
-#        max_level = max_level + space
-#        ticks.append(max_level)
-#        ticks.insert(0, min_level)
-#
-#    if extend == 'max':
-#        max_level = max_level + space
-#        ticks.append(max_level)
-#            
-#    if extend == 'min':
-#        min_level = min_level - space
-#        ticks.insert(0, min_level)
-#
-#
-#    #Colourbar can be converted to a number of discete segments or a custom
-#    #colourmap can be defined forcing it to a discrete map
-#    if isinstance(discrete_segments, list):
-#        if discrete_segments[0] in globals():
-#            for i in range(0, len(discrete_segments)):
-#                discrete_segments[i] = globals()[discrete_segments[i]]
-#
-#    if isinstance(discrete_segments, list):
-#        if len(discrete_segments) != (len(ticks) - 1):
-#            print 'Number of input colour segments does not match number of ticks - check extension setting'
-#            sys.exit(1)
-#        colourmap = matplotlib.colors.ListedColormap(discrete_segments[:], 'indexed')
-#    elif discrete_segments:
-#        if discrete_segments != (len(ticks) - 1):
-#            print 'Number of input colour segments does not match number of ticks - check extension setting'
-#            sys.exit(1)
-#        colourmap = _generate_colourmap(colourmap, discrete_segments)
-#    elif not continuous_colourbar:
-#        colourmap = _generate_colourmap(colourmap, len(ticks))        
-#        
-#    #Remove added elements as we dont wont these displayed
-#    if extend == 'both':
-#        ticks = ticks[1: -1]
-#    
-#    if extend == 'max':
-#        ticks = ticks[0: -1]
-#
-#    if extend == 'min':
-#        ticks = ticks[1:]
-#    
-#    return ticks, colourmap, min_level, max_level, dec
 
 
 def periodConstraint(cube, t1, t2):
@@ -145,7 +47,8 @@ def _main(inargs):
     # Load the cube
     fname = inargs.infile
     #cube = iris.load_cube(fname, inargs.variable)
-    cube = iris.load_cube(fname, iris.Constraint(inargs.variable, longitude=lambda v: 195 < v < 340))
+    cube = iris.load_cube(fname, iris.Constraint(inargs.variable, 
+                                                 longitude=lambda v: inargs.lon_bounds[0] < v < inargs.lon_bounds[1]))
 
     # Apply time constraint
     if inargs.time_bounds:
@@ -177,20 +80,6 @@ def _main(inargs):
     # And format the ticks to just show the year-month-day
     plt.gca().yaxis.set_major_formatter(mdates.DateFormatter(timescales[inargs.timescale][0]))
 
-    # Plot the colourbar
-#    if inargs.ticks or inargs.discrete_segments:
-#        ticks, colourmap, dec = _set_colourbar(cube.data, 
-#                                               inargs.colourbar_colour, 
-#                                               inargs.ticks, 
-#                                               inargs.discrete_segments, 
-#                                               inargs.extend)
-#        
-#        cb = plt.colorbar(mappable=im,
-#                          orientation=inargs.orientation, 
-#                          ticks=ticks, 
-#                          extend=inargs.extend, 
-#                          format='%.'+str(dec)+'f')
-  
     cb = plt.colorbar(mappable=im, orientation=inargs.orientation)
 
     if inargs.units in recognised_units.keys():
@@ -234,6 +123,8 @@ example (irvingnix@earthsci.unimelb.edu.au)
                         help="Internal between time tick labels")
     parser.add_argument("--colorbar_bounds", type=float, nargs=2, default=(0, 20), metavar=('MIN', 'MAX'),
                         help="Maximum and minimum values on colorbar") 
+    parser.add_argument("--lon_bounds", type=float, nargs=2, default=(0, 360), metavar=('START', 'END'),
+                        help="start & end longitude for the x-axis [default = 0, 360]")			
 
     parser.add_argument("--ofile", type=str, default=None,
                         help="name of output file [default: show]")
@@ -248,13 +139,6 @@ example (irvingnix@earthsci.unimelb.edu.au)
                         help="Colourbar name [defualt: jet]")
     parser.add_argument("--units", type=str, default=None,
                         help="Units (recognised units: ms-1)")
-#    parser.add_argument("--ticks", type=float, nargs='*',
-#                        help="list of tick marks to appear on the colour bar")
-#    parser.add_argument("--discrete_segments", type=str, nargs='*',
-#                        help="list of colours to appear on the colour bar")
-#    parser.add_argument("--extend", type=str, choices=('both', 'neither', 'min', 'max'), default='neither',
-#                        help="selector for arrow points at either end of colourbar [default: 'neither']")
-
 
 
     args = parser.parse_args()              
