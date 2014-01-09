d233821dee6f9ff13e23f5230dc068c0908fcfb3
Damien Irving <dbirving@student.unimelb.edu.au>
2014-01-10 09:47:33 +1100

    Added a variable related loop

--- data_processing/calc_composite.sh
+++ data_processing/calc_composite.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 data_path=/mnt/meteo0/data/simmonds/dbirving/Merra/data/processed
-tas_file=${data_path}/tas_Merra_surface_daily-anom-wrt-1979-2012_native.nc
-start=${data_path}/stats/hov-vrot-env-w567_Merra_250hPa_daily-anom-wrt-1979-2012_y181x360_np20-260_absolute14_lon225-335_dates-filter
+base=hov-vrot-env-w567_Merra_250hPa_daily-anom-wrt-1979-2012_y181x360_np20-260_absolute14_lon225-335_dates-filter
 
-
-for region in antarctic-peninsula marie-byrd-land ; do
-    for season in DJF MAM JJA SON ; do
+for var in sf ; do
+    for region in marie-byrd-land antarctic-peninsula ; do
+        for season in DJF MAM JJA SON ; do
     
-        /usr/local/uvcdat/1.3.0/bin/cdat calc_composite.py ${tas_file} tas ${start}-${region}-northerly-va.txt ${start}-${region}-northerly-va_composite-${season}.nc --time 1979-01-01 2012-12-31 ${season}
-        echo ${region} ${season}
+            if [ "${var}" = "sf" ]; then
+                infile=${data_path}/sf_Merra_250hPa_daily-anom-wrt-all_native.nc
+            elif [ "${var}" -eq "tas" ]; then
+	        infile=${data_path}/tas_Merra_surface_daily-anom-wrt-1979-2012_native.nc
+            fi
+    
+            /usr/local/uvcdat/1.3.0/bin/cdat calc_composite.py ${infile} ${var} ${data_path}/stats/${base}-${region}-northerly-va.txt ${data_path}/stats/${var}-${base}-${region}-northerly-va_composite-${season}.nc --time 1979-01-01 2012-12-31 ${season}
 
+        done
     done
 done
+
+
+
