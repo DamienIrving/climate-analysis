fdacc7d2cc4880127695d7a85e95336743cb5827
Damien Irving <dbirving@student.unimelb.edu.au>
2014-01-21 16:19:03 +1100

    Added single plot to plot_envelope.sh

--- visualisation/plot_envelope.sh
+++ visualisation/plot_envelope.sh
@@ -18,4 +18,10 @@ end2=1996-04-29
 #done
 
 
-/usr/local/uvcdat/1.3.0/bin/cdat plot_map.py ${envfile} env ${start1} ${end1} none --projection spstere --draw_axis --contour --delat 15 --contour_file ${sffile} sf ${start1} ${end1} none --colourbar_colour Oranges --ticks 0 1.5 3 4.5 6 7.5 9 10.5 --contour_ticks -140 -120 -100 -80 -60 -40 -20 0 20 40 60 80 100 120 140  --image_size 9 --ofile test-two.png --inline_row_headings April_2000 April_1996 --infiles ${envfile} env ${start2} ${end2} none --contour_file ${sffile} sf ${start2} ${end2} none --dimensions 2 1 --units ms-1 --region world-dateline-duplicate360
+#/usr/local/uvcdat/1.3.0/bin/cdat plot_map.py ${envfile} env ${start1} ${end1} none --projection spstere --draw_axis --contour --delat 15 --contour_file ${sffile} sf ${start1} ${end1} none --colourbar_colour Oranges --ticks 0 1.5 3 4.5 6 7.5 9 10.5 --contour_ticks -140 -120 -100 -80 -60 -40 -20 0 20 40 60 80 100 120 140  --image_size 9 --ofile test-two.png --inline_row_headings April_2000 April_1996 --infiles ${envfile} env ${start2} ${end2} none --contour_file ${sffile} sf ${start2} ${end2} none --dimensions 2 1 --units ms-1 --region world-dateline-duplicate360
+
+
+start=1996-04-01
+end=1996-04-29
+
+/usr/local/uvcdat/1.3.0/bin/cdat plot_map.py ${envfile} env ${start} ${end} none --projection spstere --draw_axis --contour --delat 15 --contour_file ${sffile} sf ${start} ${end} none --colourbar_colour Oranges --ticks 0 1.5 3 4.5 6 7.5 9 10.5 --contour_ticks -140 -120 -100 -80 -60 -40 -20 0 20 40 60 80 100 120 140  --image_size 9 --ofile test_env_Apr1996.png --col_headings April_1996 --units ms-1 --region world-dateline-duplicate360 --image_size 10
