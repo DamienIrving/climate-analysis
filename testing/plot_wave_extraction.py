072062547925a9cc4b24047a33ef7c05059e9184
Damien Irving <damien_irving@yahoo.com.au>
2014-03-17 12:58:34 +1100

    Added more examples to plot_wave_extraction.py

--- testing/plot_wave_extraction.py
+++ testing/plot_wave_extraction.py
@@ -42,18 +42,45 @@ def extract_envelopes(wave_data, bounds_list):
 def get_wave(source, var, time_bounds, lat):
     """Return the wave data and function name, according to the specified source"""
     
+    x = numpy.linspace(0, 2 * numpy.pi, 200)
+    nsteps = len(x)
+    amplitude = numpy.ones(nsteps)
     if source == 'simple':
-        x = numpy.linspace(0, 2 * numpy.pi, 200)
 	wave_data = numpy.sin(3*x)
         function_text = r'$y = \sin(3x)$'
 	envelope_list = [(1, 20), (3, 3), (2, 4)]
 		
     elif source == 'complex':
-        x = numpy.linspace(0, 2 * numpy.pi, 200)
 	k = 7
 	wave_data = numpy.cos((k - 1)*x) + numpy.cos(k * x) + numpy.cos((k + 1)*x)
         function_text = r'$y = \cos(6x) + \cos(7x) + \cos(8x)$'
         envelope_list = [(1, 20), (7, 9), (6, 10), (8, 8)]
+
+    elif source == 'zw3_smooth_variation':
+	wave_data = numpy.abs(numpy.sin(0.5*x)) * numpy.cos(3*x)
+        function_text = r'$y = \sin(0.5x) \cos(3x)$'
+        envelope_list = [(1, 20), (2, 4), (3, 3)]
+
+    elif source == 'zw3_breaks':
+	wave_data = numpy.abs(numpy.sin(2*x)) * numpy.cos(3*x)
+        function_text = r'$y = \sin(2x) \cos(3x)$'
+        envelope_list = [(1, 20), (2, 4), (2, 7), (3, 3)]
+
+    elif source == 'zw3_stops':
+	ramp = numpy.arange(0, 1, 0.025)
+	amplitude[80:120] = numpy.zeros(40)
+	amplitude[40:80] = ramp[::-1]
+	amplitude[120:160] = ramp
+	
+	wave_data = amplitude * numpy.cos(3*x)
+        function_text = r'$y = \cos(3x), with ramp down$'
+        envelope_list = [(1, 20), (2, 4), (2, 7), (3, 3)]
+
+    elif source == 'zw3_random':
+	wave_data = numpy.cos(3*x)
+	wave_data[70:130] = numpy.random.uniform(-1, 1, size=60)
+        function_text = r'$y = \cos(3x), with random$'
+        envelope_list = [(1, 20), (2, 4), (1, 5), (3, 3)]
 
     else:
         fin = cdms2.open(source)
@@ -83,7 +110,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=description,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("data_source", type=str, 
-                        help="""Can be "simple", "complex" or the name of a data file""")
+                        help="""Can be "simple", "complex", ... , or the name of a data file""")
     
     parser.add_argument("--ofile", type=str, default='test.png',
                         help="Name of output file [default = test.png]")
