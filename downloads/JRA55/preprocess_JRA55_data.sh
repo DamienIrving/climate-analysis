33cdc0104486dad61e4262e4b88e32a4a3681d7b
Damien Irving <damien_irving@yahoo.com.au>
2014-04-20 19:48:59 +1000

    Began to add command line interaction to the JRA55 pre-processing

--- downloads/JRA55/preprocess_JRA55_data.sh
+++ downloads/JRA55/preprocess_JRA55_data.sh
@@ -1,3 +1,26 @@
+function usage {
+    echo "USAGE: bash $0 var"
+    echo "   infile:     Input file name"
+    echo "   invar:      Input variable name"
+    echo "   e.g. bash $0  "
+}
+
+nargs=1
+
+if [[ $# -ne $nargs ]] ; then
+  usage
+fi
+
+infile=$1
+invar=$2
+  
+if [ ! -f $infile ] ; then
+    echo "Input file doesn't exist: " $infile
+    usage
+fi
+
+
+
 years=`seq 1958 1 2012`
 
 ## Merge data into a file for each year
@@ -22,5 +45,11 @@ years=`seq 1958 1 2012`
 #    done
 #done
 
-cdo mergetime anl_p125.ugrd.*_daily.nc anl_p125.ugrd.1958-2012_daily.nc
-cdo mergetime anl_p125.vgrd.*_daily.nc anl_p125.vgrd.1958-2012_daily.nc
+#cdo mergetime anl_p125.ugrd.*_daily.nc anl_p125.ugrd.1958-2012_daily.nc
+#cdo mergetime anl_p125.vgrd.*_daily.nc anl_p125.vgrd.1958-2012_daily.nc
+
+cdo chname,g0_lon_2,longitude
+cdo chname,g0_lat_1,latitude
+cdo chname,VGRD_GDS0_ISBL,va
+cdo sellonlatbox,0,359.9,-90,90   # Check if this is needed
+ncatted -O -a axis,time,c,c,T
