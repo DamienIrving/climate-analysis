87eb26fce2b2b78294b68dff968f784b85b41fa9
Damien Irving <damien_irving@yahoo.com.au>
2014-05-06 16:55:22 +1000

    Fixed typo in ZW3 makefile

--- data_processing/zw3_climatology.mk
+++ data_processing/zw3_climatology.mk
@@ -75,7 +75,7 @@ ${RWID_DIR}/env-zw3-composite-mean_Merra_250hPa_${TSCALE_LABEL}_${GRID}-${MER_ME
 
 # Zonal streamfunction anomaly
 ${RWID_DIR}/sf-zonal-anom-zw3-composite-mean_Merra_250hPa_${TSCALE_LABEL}_${GRID}-${MER_METHOD}-${LAT_LABEL}_env-${WAVE_LABEL}-va-ampmin${AMP_MIN}-extentmin${EXTENT_MIN}-${EXTENT_MAX}_${COMPOSITE_PLACEHOLDER}.nc : ${PDATA_DIR}/sf_Merra_250hPa_${TSCALE_LABEL}-zonal-anom_native.nc ${RWID_DIR}/zw3-dates_Merra_250hPa_${TSCALE_LABEL}_${GRID}-${MER_METHOD}-${LAT_LABEL}_env-${WAVE_LABEL}-va-ampmin${AMP_MIN}-extentmin${EXTENT_MIN}-${EXTENT_MAX}.txt 
-	bash ${DATA_SCRIPT_DIR}/calc_composite.sh $< env $(word 2,$^) $@ ${COMPOSITE_TIMESCALE}
+	bash ${DATA_SCRIPT_DIR}/calc_composite.sh $< sf $(word 2,$^) $@ ${COMPOSITE_TIMESCALE}
 
 
 
