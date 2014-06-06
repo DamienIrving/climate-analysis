b0742abec79165167f3f58e59f24418ec5a280b2
Damien Irving <damien_irving@yahoo.com.au>
2014-06-06 13:18:20 +1000

    Fixed error in Rossby wave source calculation

--- data_processing/calc_wind_quantities.py
+++ data_processing/calc_wind_quantities.py
@@ -1,41 +1,36 @@
-#!/usr/bin/env cdat
 """
-SVN INFO: $Id$
 Filename:     calc_wind_quantities.py
 Author:       Damien Irving, d.irving@student.unimelb.edu.au
 Description:  Takes the U and V wind and calculates various wind quantities
 Reference:    Uses the windspharm package: ajdawson.github.com/windspharm/index.html
 
-Updates | By | Description
---------+----+------------
-11 December 2012 | Damien Irving | Initial version.
-
 """
 
-import sys
-import os
+# Import general Python modules #
 
+import sys, os
 import argparse
-
 import numpy
+from windspharm.cdms import VectorWind
 
-import cdms2
-import cdutil
+# Import my modules #
 
-module_dir = os.path.join(os.environ['HOME'], 'modules')
-sys.path.insert(0, module_dir)
-import netcdf_io as nio
+cwd = os.getcwd()
+repo_dir = '/'
+for directory in cwd.split('/')[1:]:
+    repo_dir = os.path.join(repo_dir, directory)
+    if directory == 'phd':
+        break
 
-## The current release of UV-CDAT doesn't have the windspharm.cdms module (the latest version does)
-#from windspharm.cdms import VectorWind
+modules_dir = os.path.join(repo_dir, 'modules')
+sys.path.append(modules_dir)
 
-#from windspharm.standard import VectorWind
-#from windspharm.tools import prep_data, recover_data, order_latdim
+try:
+    import netcdf_io as nio
+    import coordinate_rotation as rot
+except ImportError:
+    raise ImportError('Must run this script from anywhere within the phd git repo')
 
-module_dir = os.path.join(os.environ['HOME'], 'data_processing', 'windspharm', 'windspharm', 'lib', 'windspharm')
-sys.path.insert(0, module_dir)
-from standard import VectorWind
-from tools import prep_data, recover_data, order_latdim
 
 
 var_atts = {}
@@ -47,7 +42,7 @@ var_atts['magnitude'] = {'id': 'spd',
     'history': 'windspharm magnitude() function - http://ajdawson.github.com/windspharm/index.html'}
 
 var_atts['vorticity'] = {'id': 'vrt',
-    'standard_name': 'relative_vorticity'
+    'standard_name': 'relative_vorticity',
     'long_name': 'Relative Vorticity',
     'units': 's-1',   
     'history': 'windspharm vorticity(), http://ajdawson.github.com/windspharm/index.html'}
@@ -63,12 +58,6 @@ var_atts['absolutevorticity'] = {'id': 'avrt',
     'long_name': 'Absolute Vorticity (sum of relative and planetary)',
     'units': '1.e-5 s-1',
     'history': 'windspharm absolutevorticity(), http://ajdawson.github.com/windspharm/index.html'}
-
-var_atts['absolutevorticitygradient'] = {'id': 'avrtgrad',
-    'standard_name': 'absolute_vorticity_gradient',
-    'long_name': 'Absolute Vorticity (sum of relative and planetary) gradient',
-    'units': '1.e-5 s-1',
-    'history': 'windspharm gradient(absolutevorticity()), http://ajdawson.github.com/windspharm/index.html'}
 
 var_atts['planetaryvorticity'] = {'id': 'pvrt',
     'standard_name': 'planetary_vorticity',
@@ -119,13 +108,13 @@ var_atts['rossbywavesource'] = {'id': 'rws',
     'history': 'calculated using windspharm - http://ajdawson.github.com/windspharm/index.html'}
 
 var_atts['rossbywavesource1'] = {'id': 'rws1',
-    'standard_name': 'rossby_wave_source_vortex'
+    'standard_name': 'rossby_wave_source_vortex',
     'long_name': 'Rossby wave source, vortex stretching term',
     'units': '1.e-11 s-2',  
     'history': 'calculated using windspharm - http://ajdawson.github.com/windspharm/index.html'}
 
 var_atts['rossbywavesource2'] = {'id': 'rws2',
-    'standard_name': 'rossby_wave_source_advection'
+    'standard_name': 'rossby_wave_source_advection',
     'long_name': 'Rossby wave source, advection of absolute vorticity by divergent flow term',
     'units': '1.e-11 s-2',  
     'history': 'calculated using windspharm - http://ajdawson.github.com/windspharm/index.html'}
@@ -134,49 +123,6 @@ var_atts['rossbywavesource2'] = {'id': 'rws2',
 def calc_quantity(data_u, data_v, quantity):
     """Calculates a single wind quantity using windspharm (ajdawson.github.com/windspharm/index.html)"""
     
-    # Do all the data preparation, because I'm using the windspharm.standard instead of
-    # windspharm.cdms (when the latter is included in the UV-CDAT release, I should use it
-    # because it makes the code much simplier)    
-
-#    if eddy:
-#        uwnd_zonal_ave = cdutil.averager(data_u, axis='x')
-#        vwnd_zonal_ave = cdutil.averager(data_v, axis='x')
-#	
-#	print numpy.shape(data_u)
-#	print numpy.shape(uwnd_zonal_ave)
-#	
-#	eddy_uwnd = numpy.subtract(data_u, numpy.resize(uwnd_zonal_ave, numpy.shape(data_u)))
-#	eddy_vwnd = numpy.subtract(data_v, numpy.resize(vwnd_zonal_ave, numpy.shape(data_v)))
-#	
-#	print numpy.shape(eddy_uwnd)
-#	
-#	uwnd = eddy_uwnd[:]
-#	vwnd = eddy_uwnd[:]
-#    else:
-    
-    assert isinstance(data_u, nio.InputData)
-    assert isinstance(data_v, nio.InputData)
-    
-    uwnd = data_u.data[:]
-    vwnd = data_v.data[:]
-    
-    lons = data_u.data.getLongitude()[:]
-    lats = data_u.data.getLatitude()[:]
-    
-    # The standard interface requires that latitude and longitude be the leading
-    # dimensions of the input wind components, and that wind components must be
-    # either 2D or 3D arrays. The data read in is 4D and has latitude and
-    # longitude as the last dimensions. The bundled tools can make the process of
-    # re-shaping the data a lot easier to manage.
-    uwnd, uwnd_info = prep_data(uwnd, uwnd.getOrder())  
-    vwnd, vwnd_info = prep_data(vwnd, vwnd.getOrder()) 
-    
-    # It is also required that the latitude dimension is north-to-south. Again the
-    # bundled tools make this easy.
-    lats, uwnd, vwnd = order_latdim(lats, uwnd, vwnd)
-    flip = False if (lats[0] == data_u.data.getLatitude()[0]) else True   # Flag to see if lats was flipped 
-
-    # Create a VectorWind instance (squeeze works around a bug in the code).
     uwnd = uwnd.squeeze()
     vwnd = vwnd.squeeze()
     w = VectorWind(uwnd, vwnd)
@@ -184,24 +130,20 @@ def calc_quantity(data_u, data_v, quantity):
     # Compute the desired quantity. Also use the bundled tools to re-shape the 
     # outputs to the 4D shape of the wind components as they were read off files.
     
-    if quantity[0:16] == 'rossbywavesource':
+    if quantity == 'rossbywavesource':
 	# Compute components of rossby wave source: absolute vorticity, divergence,
 	# irrotational (divergent) wind components, gradients of absolute vorticity.
-	#eta = w.vorticity() - w.planetaryvorticity()
+	
 	eta = w.absolutevorticity()
 	div = w.divergence()
 	uchi, vchi = w.irrotationalcomponent()
 	etax, etay = w.gradient(eta)
 
 	# Combine the components to form the Rossby wave source term
-	if len(quantity) == 16:
-            data_out = -eta * div - uchi * etax + vchi * etay	
-	elif quantity[-1] == '1':
-            data_out = -eta * div
-        else:
-            data_out = -uchi * etax + vchi * etay
-
-        data_out = data_out / (1.e-11)
+        data_out = {}
+	data_out['rws1'] = (-eta * div) / (1.e-11)
+        data_out['rws2'] = (-(uchi * etax + vchi * etay)) / (1.e-11)
+        data_out['rws'] = data_out['rws1'] + data_out['rws2']
 
     elif quantity == 'magnitude':
         data_out = w.magnitude()
@@ -215,13 +157,11 @@ def calc_quantity(data_u, data_v, quantity):
     
     elif quantity == 'absolutevorticity':
         data_out = w.absolutevorticity()
-	#data_out = w.vorticity() - w.planetaryvorticity()
 	data_out = data_out / (1.e-5)
     
     elif quantity == 'absolutevorticitygradient':
         avrt = w.absolutevorticity()
 	ugrad, vgrad = w.gradient(avrt)
-	#data_out = w.vorticity() - w.planetaryvorticity()
 	data_out = numpy.sqrt(numpy.square(ugrad) + numpy.square(vgrad)) 
 	data_out = data_out / (1.e-5)
     
@@ -246,24 +186,7 @@ def calc_quantity(data_u, data_v, quantity):
 	
     else:
 	sys.exit('Wind quantity not recognised')
-    
-    # Put the data back together #
-   
-    if quantity == 'irrotationalcomponent' or quantity == 'nondivergentcomponent':
-        for comp in ['u', 'v']:
-	    data_out[comp] = recover_data(data_out[comp], uwnd_info)
-	    if flip:
-	        data_out[comp] = numpy.fliplr(data_out[comp])   #data_out[comp][:, ::-1, :]    
-    else:
-	data_out = recover_data(data_out, uwnd_info)
-	if flip:
-	    data_out = numpy.fliplr(data_out)  #data_out[:, ::-1, :]
 
-#    if eddy:
-#        data_out_zonal_ave = cdutil.averager(data_out, axis='2')
-#	print numpy.shape(data_out_zonal_ave)
-#	
-#	data_out = numpy.subtract(data_out, numpy.resize(data_out_zonal_ave, numpy.shape(data_out)))
 
     return data_out
 
@@ -273,8 +196,10 @@ def main(inargs):
 
     # Read the input data #
 
-    data_u = nio.InputData(inargs.infileu, inargs.varu, **nio.dict_filter(vars(inargs), ['grid', 'time', 'region']))
-    data_v = nio.InputData(inargs.infilev, inargs.varv, **nio.dict_filter(vars(inargs), ['grid', 'time', 'region']))
+    data_u = nio.InputData(inargs.infileu, inargs.varu, 
+                           **nio.dict_filter(vars(inargs), ['time', 'region', 'latitude', 'longitude']))
+    data_v = nio.InputData(inargs.infilev, inargs.varv, 
+                           **nio.dict_filter(vars(inargs), ['time', 'region', 'latitude', 'longitude']))
 
     # Check that the input data are all on the same coordinate axes #
 
@@ -290,10 +215,16 @@ def main(inargs):
     # Write output file #
 
     indata_list = [data_u, data_v,]
-    if type(data_out) == dict:
+    if (type(data_out) == dict) and ('u' in data_out.keys()):
         outdata_list = [data_out['u'], data_out['v'],]
         outvar_atts_list = [var_atts[inargs.quantity, 'u'], var_atts[inargs.quantity, 'v'],]
-	outvar_axes_list = [data_u.data.getAxisList(),data_u.data.getAxisList(),]
+	outvar_axes_list = [data_u.data.getAxisList()] * 2
+    elif (type(data_out) == dict) and ('rws' in data_out.keys()):
+        outdata_list = [data_out['rws'], data_out['rws1'], data_out['rws2']]
+        outvar_atts_list = [var_atts['rossbywavesource'], 
+	                    var_atts['rossbywavesource1'],
+			    var_atts['rossbywavesource2']]
+	outvar_axes_list = [data_u.data.getAxisList()] * 3
     else:
         outdata_list = [data_out,]
         outvar_atts_list = [var_atts[inargs.quantity],]
@@ -320,7 +251,8 @@ wind quantities:
 	                    with speed proportional to the gradient)
   velocitypotential     ->  Velocity potential (the divergent part of the wind blows along the velocity potential contours,
 	                    with speed proportional to the gradient)
-  rossbywavesource      ->  Rossby wave source			      	    
+  rossbywavesource      ->  Rossby wave source (includes the vortex stretching and advection of absolute vorticity by divergent flow terms)
+  		      	    
 	    
 note:
   The input data can have no missing values
@@ -328,23 +260,15 @@ note:
 reference:
   Uses the windspharm package: http://ajdawson.github.com/windspharm/intro.html
 
-example (abyss.earthsci.unimelb.edu.au):
-  /usr/local/uvcdat/1.2.0rc1/bin/cdat calc_wind_quantities.py streamfunction  
-  /work/dbirving/datasets/Merra/data/ua_Merra_250hPa_monthly_native.nc ua
-  /work/dbirving/datasets/Merra/data/va_Merra_250hPa_monthly_native.nc va
-  /work/dbirving/datasets/Merra/data/processed/sf_Merra_250hPa_monthly_native.nc
-
-  /usr/local/uvcdat/1.2.0rc1/bin/cdat calc_wind_quantities.py streamfunction  
-  /work/dbirving/datasets/Merra/data/processed/ua_Merra_250hPa_monthly-anom-wrt-1981-2010_native.nc ua
-  /work/dbirving/datasets/Merra/data/processed/va_Merra_250hPa_monthly-anom-wrt-1981-2010_native.nc va
-  /work/dbirving/datasets/Merra/data/processed/sf_Merra_250hPa_monthly-anom-wrt-1981-2010_native.nc
+example (vortex.earthsci.unimelb.edu.au):
+  /usr/local/uvcdat/1.3.0/bin/cdat calc_wind_quantities.py streamfunction  
+  ua_Merra_250hPa_monthly_native.nc ua
+  va_Merra_250hPa_monthly_native.nc va
+  sf_Merra_250hPa_monthly_native.nc
 
 author:
-  Damien Irving, 10 Dec 2012.
+  Damien Irving, d.irving@student.unimelb.edu.au
 
-bugs:
-  Rossby wave source output needs to be checked
-  Some of the if statements are fairly ugly - need to clean those up
     
 """
 
@@ -355,24 +279,25 @@ bugs:
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
 
     parser.add_argument("quantity", type=str, help="Quantity to calculate",
-                        choices=['magnitude', 'vorticity', 'divergence', 'absolutevorticity', 'absolutevorticitygradient', 'planetaryvorticity',
-                                 'irrotationalcomponent', 'nondivergentcomponent', 'streamfunction', 
-				 'velocitypotential', 'rossbywavesource', 'rossbywavesource1', 'rossbywavesource2'])
+                        choices=['magnitude', 'vorticity', 'divergence', 'absolutevorticity', 
+			         'absolutevorticitygradient', 'planetaryvorticity',
+                                 'irrotationalcomponent', 'nondivergentcomponent', 
+				 'streamfunction', 'velocitypotential', 'rossbywavesource'])
     parser.add_argument("infileu", type=str, help="Input U-wind file name")
     parser.add_argument("varu", type=str, help="Input U-wind variable")
     parser.add_argument("infilev", type=str, help="Input V-wind file name")
     parser.add_argument("varv", type=str, help="Input V-wind variable")
     parser.add_argument("outfile", type=str, help="Output file name")
     
-    parser.add_argument("--grid", type=float, nargs=6, metavar=('START_LAT', 'NLAT', 'DELTALAT', 'START_LON', 'NLON', 'DELTALON'),
-                        help="Uniform regular grid to regrid data to [default = None]")
     parser.add_argument("--region", type=str, choices=nio.regions.keys(),
-                        help="Region over which to calculate EOF [default = entire]")
-    parser.add_argument("--time", type=str, nargs=2, metavar=('START_DATE', 'END_DATE'),
-                        help="Time period over which to calculate the EOF [default = entire]")
-#    parser.add_argument("--eddy", action="store_true", default=False,
-#                        help="return the eddy component of the wind quantity (i.e. remove the zonal mean before calculation)")
-    
+                        help="Region [default = entire]")
+    parser.add_argument("--latitude", type=float, nargs=2, metavar=('START', 'END'),
+                        help="Latitude range [default = entire]")
+    parser.add_argument("--longitude", type=float, nargs=2, metavar=('START', 'END'),
+                        help="Longitude range [default = entire]")
+    parser.add_argument("--time", type=str, nargs=3, metavar=('START_DATE', 'END_DATE', 'MONTHS'),
+                        help="Time period [default = entire]")
+
     args = parser.parse_args()  
     
     print 'Quantity:', args.quantity
