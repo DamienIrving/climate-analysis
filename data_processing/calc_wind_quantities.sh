b8d7079fc1df3d5fea22dfc50bcf66b9cd8bbce3
Damien Irving <damien_irving@yahoo.com.au>
2014-06-11 11:04:02 +1000

    Made year increments smaller to avoid netCDF memory fail

--- data_processing/calc_wind_quantities.sh
+++ data_processing/calc_wind_quantities.sh
@@ -5,10 +5,10 @@ vfile=${data_dir}/va_Merra_250hPa_daily_native.nc
 outfile=${data_dir}/processed/rws_Merra_250hPa_daily_native.nc
 
 
-years=(1979 1984 1989 1994 1999 2004 2009)
+years=(1979 1982 1985 1988 1991 1994 1997 2000 2003 2006 2009 2012)
 temp_files=()
 for year in "${years[@]}"; do
-    end=`expr $year + 4`
+    end=`expr $year + 2`
     temp_file=${temp_dir}/temp-rws_${year}-${end}.nc
     /usr/local/uvcdat/1.3.0/bin/cdat ~/phd/data_processing/calc_wind_quantities.py rossbywavesource $ufile ua $vfile va ${temp_file} --time ${year}-01-01 ${end}-12-31 none 
     temp_files+=(${temp_file})
