16fd26542890e2e8d6115594c9b45335f641f860
Damien Irving <damien_irving@yahoo.com.au>
2014-08-11 16:00:40 +1000

    Fixed bugs in plot_freq_spectra.py

--- visualisation/plot_freq_spectra.py
+++ visualisation/plot_freq_spectra.py
@@ -6,10 +6,12 @@ Author:       Damien Irving, d.irving@student.unimelb.edu.au
 
 # Import general Python modules
 
-import matplotlib.pyplot as plt
-import os, sys
-import numpy
+import os, sys, pdb
+import math, numpy
 import argparse
+import matplotlib.pyplot as plt
+from dateutil import parser
+import cdms2
 
 # Import my modules
 
@@ -32,13 +34,79 @@ except ImportError:
     raise ImportError('Must run this script from anywhere within the phd git repo')
     
 
+def set_title(lat_bounds, time_bounds):
+    """Set the title for the plot""" 
+
+    start_lat, end_lat = lat_bounds
+    if start_lat == end_lat:
+        lat_label = str(start_lat)
+    else:
+        lat_label = '%s to %s' %(str(start_lat), str(end_lat))
+
+    title = 'Ave spectra, %s to %s, lat: %s' %(str(time_bounds[0]), str(time_bounds[1]), lat_label) 
+
+    return title
+
+
+def common_date_format(date_list):
+    """Convert a list of dates to the %Y-%m-%d format"""
+
+    date_list = map(str, date_list)
+    date_list = map(parser.parse, date_list)
+    date_list = map(lambda x: x.strftime("%Y-%m-%d"), date_list)
+
+    return date_list
+
+
+def adjust_time_bounds(infile, time_bounds, window):
+    """Adjust the time bounds so that there is enough data
+    in the selection to calculate the running mean"""
+
+    fin = cdms2.open(infile)
+    time_axis = fin.getAxis('time').asComponentTime()  #will fail if the name of the time axis isn't time
+    fin.close()
+
+    time_axis_common = common_date_format(time_axis)
+    time_bounds_common = common_date_format(time_bounds)
+    old_start, old_end = time_bounds_common
+
+    old_start_index = 0
+    for date in time_axis_common:
+        if old_start in date:
+            break
+        old_start_index = old_start_index + 1
+    assert old_start_index != len(time_axis), "Original start index (relative to the time axis) not found"    
+
+    old_end_index = -1
+    for date in time_axis_common[::-1]:
+        if old_end in date:
+            break
+        old_end_index = old_end_index - 1
+    assert old_end_index != -1 - len(time_axis), "Original end index (relative to the time axis) not found" 
+
+    new_start_index = int(old_start_index - math.ceil((window - 1) / 2.))  # genutil leaves more off the front in uneven circumstances
+    new_end_index = int(old_end_index + math.floor((window - 1) / 2.))
+
+    return [str(time_axis[new_start_index]), str(time_axis[new_end_index])]
+
+
+
 def main(inargs):
     """Run the program."""
     
+    title = set_title(inargs.latitude, inargs.time)
     plt.figure() 
-    for step in [1, 5, 10, 30, 90, 180]:
+    if inargs.runmean:
+        runmean_windows = inargs.runmean
+    else:
+        runmean_windows = [1]
+    
+    for step in runmean_windows:
+        if inargs.time:
+            inargs.time = adjust_time_bounds(inargs.infile, inargs.time, step)
+        
         indata = nio.InputData(inargs.infile, inargs.variable, runave=step,
-                               **nio.dict_filter(vars(inargs), ['time', 'latitude']))
+                               **nio.dict_filter(vars(inargs), ['latitude', 'time']))
     
         signal = indata.data
         indep_var = signal.getLongitude()[:]
@@ -63,6 +131,7 @@ def main(inargs):
     plt.xlabel('Frequency [cycles / domain]')
     plt.ylabel('normalised amplitude')
     plt.legend()
+    plt.title(title)
     
     plt.savefig(inargs.outfile)
     plt.clf()
@@ -73,10 +142,13 @@ def main(inargs):
 if __name__ == '__main__':
 
     extra_info =""" 
-example (irvingnix.earthsci.unimelb.edu.au):
-  /usr/local/uvcdat/1.5.1/bin/cdat plot_freq_spectra.py 
-  /home/dbirving/Downloads/Data/va_Merra_250hPa_30day-runmean-2002_r360x181.nc 
-  va test.png --latitude -70 -40
+example (vortex.earthsci.unimelb.edu.au):
+  /usr/local/uvcdat/1.3.0/bin/cdat plot_freq_spectra.py 
+  /mnt/meteo0/data/simmonds/dbirving/Merra/data/processed/va_Merra_250hPa_daily_r360x181.nc va 
+  /mnt/meteo0/data/simmonds/dbirving/Merra/data/processed/rwid/zw3/figures/tscale_anal/amp-spectra-va_Merra_250hPa_daily-2000-2009_r360x181-55S.png 
+  --time 2000-01-01 2009-12-31 
+  --runmean 1 5 30 90 180 365 
+  --latitude -55 -55
 
 author:
   Damien Irving, d.irving@student.unimelb.edu.au
@@ -84,27 +156,28 @@ author:
 """
 
     description='Plot the average amplitude spectrum'
-    parser = argparse.ArgumentParser(description=description,
+    argparser = argparse.ArgumentParser(description=description,
                                      epilog=extra_info, 
                                      argument_default=argparse.SUPPRESS,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    parser.add_argument("infile", type=str, help="Input file name")
-    parser.add_argument("variable", type=str, help="Input file variable")
-    parser.add_argument("outfile", type=str, help="Output file name")
+    argparser.add_argument("infile", type=str, help="Input file name")
+    argparser.add_argument("variable", type=str, help="Input file variable")
+    argparser.add_argument("outfile", type=str, help="Output file name")
 			
     # Input data options
-    parser.add_argument("--latitude", type=float, nargs=2, metavar=('START', 'END'),
+    argparser.add_argument("--latitude", type=float, nargs=2, metavar=('START', 'END'),
                         help="Latitude range over which to perform the Fourier Transform [default = entire]")
-    parser.add_argument("--time", type=str, nargs=3, metavar=('START_DATE', 'END_DATE', 'MONTHS'),
+    argparser.add_argument("--time", type=str, nargs=2, metavar=('START_DATE', 'END_DATE'),
                         help="Time period [default = entire]")
 
     # Output options
-    parser.add_argument("--window", type=int, default=10,
+    argparser.add_argument("--window", type=int, default=10,
                         help="upper limit on the frequencies included in the plot")
-
+    argparser.add_argument("--runmean", type=int, nargs='*',
+                        help="running mean windows to include (e.g. 1 5 30 90 180)")
   
-    args = parser.parse_args()            
+    args = argparser.parse_args()            
 
     print 'Input file: ', args.infile
     print 'Output file: ', args.outfile  
