17eca5cec4d651827a66b11b3624974a9d880409
Damien Irving <damien_irving@yahoo.com.au>
2014-10-01 16:54:21 +1000

    Separated composite and significance calculations

--- data_processing/calc_composite.py
+++ data_processing/calc_composite.py
@@ -40,13 +40,15 @@ except ImportError:
 
 # Define functions #
 
-def calc_composite(data_included, data_excluded):
+def get_significance(data_included, data_excluded):
     """Calculate the composite.
     
     The approach for the significance test is to compare the mean of the included
     data sample with that of the excluded data sample via an independent, two-sample,
     parametric t-test (for details, see Wilks textbook). 
     
+    http://stackoverflow.com/questions/21494141/how-do-i-do-a-f-test-in-python
+
     FIXME: If equal_var=False then it will perform a Welch's t-test, which is for samples
     with unequal variance (early versions of scipy don't have this option). To test whether
     the variances are equal or not you use an F-test (i.e. do the test at each grid point and
@@ -56,19 +58,30 @@ def calc_composite(data_included, data_excluded):
     FIXME: I need to account for autocorrelation in the data by calculating an effective
     sample size (see Wilkes, p 147). I can get the autocorrelation using either
     genutil.autocorrelation or the acf function in the statsmodels time series analysis
-    python library, however I can't see how to alter the sample size in stats.ttest_ind. 
+    python library, however I can't see how to alter the sample size in stats.ttest_ind.
+
+    FIXME: I also need to consider whether a parametric t-test is appropriate. One of my samples
+    might be very non-normally distributed, which means a non-parametric test might be better. 
     
     """
-    
-    t, p_vals = stats.ttest_ind(data_included, data_excluded, axis=0, equal_var=True) 
+
+#    alpha = 0.05 
+#    w, p_value = scipy.stats.levene(data_included, data_excluded)
+#    if p_value > alpha:
+#        equal_var = False# Reject the null hypothesis that Var(X) == Var(Y)
+#    else:
+#        equal_var = True
+
+    t, pvals = stats.ttest_ind(data_included, data_excluded, axis=0, equal_var=True) 
     print 'WARNING: Significance test did not account for autocorrelation (and is thus overconfident) and assumed equal variances'
 
-    # Perform necessary averaging #
+    pval_atts = {'id': 'p',
+                 'long_name': 'Two-tailed p-value',
+                 'units': ' ',
+                 'history': """Standard independent two sample t-test comparing the data sample that meets the composite criteria (size=%s) to a sample containing the remaining data (size=%s)""" %(len(matching_dates), len(missing_dates)),
+                 'reference': 'scipy.stats.ttest_ind(a, b, axis=t, equal_var=False)'}
 
-    composite_mean = MV2.average(data_included, axis=0)
-
-
-    return composite_mean, p_vals
+    return pvals, pval_atts
 
 
 def date_offset(date_list, offset):
@@ -86,6 +99,42 @@ def date_offset(date_list, offset):
     return edited_date_list
 
 
+def filter_dates(data, date_file, offset):
+    """Filter the data into an included (in date_file) and excluded subset. An
+    offset can be applied to the dates in date_file"""
+
+    if not date_file:
+        data_included = data
+        data_excluded = None
+        date_metadata = None
+    else:
+        date_list, date_metadata = gio.read_dates(date_file)
+        if offset:
+            date_list = date_offset(date_list, offset)
+    
+        matching_dates = nio.match_dates(date_list, data.getTime().asComponentTime())
+        missing_dates = nio.match_dates(date_list, data.getTime().asComponentTime(), invert_matching=True)
+
+        data_included = nio.temporal_extract(data, matching_dates, indexes=False)
+        data_excluded = nio.temporal_extract(data, missing_dates, indexes=False)
+
+    return data_included, data_excluded, date_metadata
+
+
+def get_composite(data, var, long_name, units, season):
+    """Calculate the composite and it's attributes (using the desired var, long_name
+    units and season"""
+
+    composite_mean = MV2.average(data_included, axis=0)
+
+    composite_atts = {'id': inargs.var,
+                      'long_name': indata.data.long_name,
+                      'units': indata.data.units,
+                      'history': 'Composite mean for %s season' %(season)}
+
+    return composite_mean, composite_atts
+
+
 def main(inargs):
     """Run the program."""
     
@@ -94,23 +143,11 @@ def main(inargs):
     indata = nio.InputData(inargs.infile, inargs.var, 
                            **nio.dict_filter(vars(inargs), ['time', 'region']))
 
-    date_list, date_metadata = gio.read_dates(inargs.date_file)
-    if inargs.offset:
-        date_list = date_offset(date_list, inargs.offset)
+    # Filter data #
 
-    # Filter the data #
-    
-    matching_dates = nio.match_dates(date_list, indata.data.getTime().asComponentTime())
-    missing_dates = nio.match_dates(date_list, indata.data.getTime().asComponentTime(), invert_matching=True)
+    data_included, data_excluded, date_metadata = filter_dates(indata.data, inargs.date_file, inargs.offset)
 
-    data_included = nio.temporal_extract(indata.data, matching_dates, indexes=False)
-    data_excluded = nio.temporal_extract(indata.data, missing_dates, indexes=False)
-
-    # Calculate the composite #
-        
-    composite, p_val = calc_composite(data_included, data_excluded)
-	
-    # Write the output file #
+    # Calculate composite #
 
     try:
         selector = inargs.time[2]
@@ -118,21 +155,28 @@ def main(inargs):
     except AttributeError or IndexError:
         season = 'annual'
 
-    composite_atts = {'id': inargs.var,
-                      'long_name': indata.data.long_name,
-                      'units': indata.data.units,
-                      'history': 'Composite mean for %s season' %(season)}
+    composite, composite_atts = get_composite(data_included, inargs.var, 
+                                              indata.data.long_name, indata.data.units,
+                                              season)
+    outdata_list = [composite,]
+    outvar_atts_list = [composite_atts,]
+    outvar_axes_list = [composite.getAxisList(),]
 
-    pval_atts = {'id': 'p',
-                 'long_name': 'Two-tailed p-value',
-                 'units': ' ',
-                 'history': """Standard independent two sample t-test comparing the data sample that meets the composite criteria (size=%s) to a sample containing the remaining data (size=%s)""" %(len(matching_dates), len(missing_dates)),
-                 'reference': 'scipy.stats.ttest_ind(a, b, axis=t, equal_var=False)'}
+    # Perform significance test # 
+    
+    if data_excluded:
+        pval, pval_atts = get_significance(data_included, data_excluded)
+        outdata_list.append(pval)
+        outvar_atts_list.append(pval_atts)
+        outvar_axes_list.append(composite.getAxisList())	
 
-    outdata_list = [composite, p_val]
-    outvar_atts_list = [composite_atts, pval_atts]
-    outvar_axes_list = [composite.getAxisList(), composite.getAxisList()] 
-    indata.global_atts['history'] = '%s \n %s' %(date_metadata, indata.global_atts['history'])
+
+    # Write the output file #
+ 
+    if date_metadata:
+        indata.global_atts['history'] = '%s \n%s' %(date_metadata, indata.global_atts['history'])
+    else:
+        indata.global_atts['history'] = indata.global_atts['history']
 
     nio.write_netcdf(inargs.outfile, " ".join(sys.argv), 
                      indata.global_atts, 
@@ -164,13 +208,15 @@ author:
 
     parser.add_argument("infile", type=str, help="Input file name")
     parser.add_argument("var", type=str, help="Input file variable")
-    parser.add_argument("date_file", type=str, help="List of dates to be included in composite")
     parser.add_argument("outfile", type=str, help="Output file name")
 
     parser.add_argument("--region", type=str, choices=nio.regions.keys(),
                         help="Region over which to calculate the composite [default: entire]")
     parser.add_argument("--time", type=str, nargs=3, metavar=('START_DATE', 'END_DATE', 'MONTHS'),
                         help="Time period over which to calculate the composite [default = entire]")
+
+    parser.add_argument("--date_file", type=str, default=None, 
+                        help="List of dates to be included in composite")
     parser.add_argument("--offset", type=int, default=None,
                         help="Number of days to offset the input dates by (from date_file) [default = None]")
 
