2f82639df4ae86e7369ce3912ea67f8a17c308af
Damien Irving <damien_irving@yahoo.com.au>
2014-11-03 11:59:07 +1100

    Further progress - still not functional

--- visualisation/plot_map2.py
+++ visualisation/plot_map2.py
@@ -50,14 +50,26 @@ standard_names = {'sf' : 'streamfunction',
                   'va' : 'northward_wind',
                   'tas' : 'surface_air_temperature'}
 
+plot_types = ['colour', 'contour', 'uwind', 'vwind', 'stipple']
+
+projections = {'PlateCarree': ccrs.PlateCarree(),
+               'SouthPolarStereo': ccrs.SouthPolarStereo()}
+
 
 def get_time_constraint(start, end):
     """Set the time constraint"""
     
     date_pattern = '([0-9]{4})-([0-9]{1,2})-([0-9]{1,2})'
-    assert re.search(date_pattern, start) or start == ''
-    assert re.search(date_pattern, end) or end == ''
-    
+    if start.lower() == 'none':
+        start = None
+    else:
+        assert re.search(date_pattern, start)
+
+    if end.lower() == 'none':
+        end = None
+    else:
+        assert re.search(date_pattern, end)
+
     if not start and not end:
         time_constraint = iris.Constraint()
     elif (start and not end) or (start == end):
@@ -86,8 +98,97 @@ def collapse_time(cube, ntimes, timestep):
     return new_cube  
 
 
-def extract_data():
+def extract_data(infile_list, projection):
+    """Extract data"""
 
+    if projection == 'SouthPolarStereo':
+        lat_constraint = iris.Constraint(latitude=lambda y: y <= 0.0)
+    else:
+        lat_constraint = iris.Constraint()
+
+    cube_dict = {}
+    for key in plot_types:
+        cube_dict[key] = []
+        type_count[key] = 0 
+
+    for infile, var, start_date, end_date, timestep, plot_type in infile_list:
+        assert plot_type in plot_types
+        time_constraint = get_time_constraint(inargs.start, inargs.end)
+	with iris.FUTURE.context(cell_datetime_objects=True):  
+            new_cube = iris.load_cube(infile, standard_names[var] & time_constraint & lat_constraint)
+        
+        ntimes = len(new_cube.coords('time')[0].points)
+        if ntimes > 1:
+            new_cube = collapse_time(new_cube, ntimes, int(timestep))
+
+        cube_dict[plot_type].append(new_cube)
+        type_count[plot_type] += 1
+
+    nplots = numpy.max(type_count.values())
+
+    return cube_dict, nplots
+
+
+def plot_flow(x, y, u, v, ax, flow_type):
+    """Plot quivers or streamlines"""
+
+    assert flow_type in ['streamlines', 'quivers']
+
+    if flow_type == 'streamlines':
+        magnitude = (u ** 2 + v ** 2) ** 0.5
+        ax.streamplot(x, y, u, v, transform=ccrs.PlateCarree(), linewidth=2, density=2, color=magnitude)
+    elif flow_type == 'quivers':
+        ax.quiver(x, y, u, v, transform=ccrs.PlateCarree(), regrid_shape=40) 
+
+
+
+def multiplot(cube_dict, nplots,
+              projection='PlateCarree',
+              flow_type='quiver'
+              title=None,
+              ofile='test.png'):
+    """Create the plot."""
+
+    plt.figure(figsize=(8, 10))
+
+    # Setup the map
+    ax = plt.axes(projection=projections[projection])
+    ax.set_extent((x.min(), x.max(), y.min(), -30.0), crs=projections['PlateCarree'])
+
+    ax.coastlines()
+    ax.gridlines()
+    #ax.set_global()
+
+    # Plot away!
+
+    for plotnum in range(0, nplots):
+
+        # Streamline or quiverplot
+        try:
+            u_cube = cube_dict['uwind'][plotnum]
+            v_cube = cube_dict['vwind'][plotnum]
+            x = u_cube.coords('longitude')[0].points
+            y = u_cube.coords('latitude')[0].points
+            u = u_cube.data
+            v = v_cube.data
+            plot_flow(x, y, u, v, ax, flow_type)
+        except IndexError:
+            pass
+
+        # Contour lines
+        try:
+            cont_cube = cube_dict['contour'][plotnum]
+            qplt.contour(cont_cube, colors='0.3', linewidths=2)
+        except IndexError:
+            pass
+
+
+
+       
+    if title:
+        plt.title(inargs.title.replace('_',' '))
+
+    plt.savefig(ofile)
 
 
 def main(inargs):
@@ -95,60 +196,17 @@ def main(inargs):
 
     # Extract data #
     
-    time_constraint = get_time_constraint(inargs.start, inargs.end)
+    cube_dict, nplots = extract_data(inargs.infiles, inargs.projection)
     
-    if inargs.projection == 'SouthPolarStereo':
-        lat_constraint = iris.Constraint(latitude=lambda y: y <= 0.0)
-    else:
-        lat_constraint = iris.Constraint()
+    # Creat the plot
 
-    with iris.FUTURE.context(cell_datetime_objects=True):
-        u_cube = iris.load_cube(inargs.u_file, inargs.u_var & time_constraint & lat_constraint)
-        v_cube = iris.load_cube(inargs.v_file, inargs.v_var & time_constraint & lat_constraint)
-        if inargs.zg_file:
-            zg_cube = iris.load_cube(inargs.zg_file, inargs.zg_var & time_constraint & lat_constraint)
-
-    ntimes = len(u_cube.coords('time')[0].points)
-    if ntimes > 1:
-        u_cube = collapse_time(u_cube, ntimes, inargs.timestep)
-        v_cube = collapse_time(v_cube, ntimes, inargs.timestep)
-        if inargs.zg_file:
-            zg_cube = collapse_time(zg_cube, ntimes, inargs.timestep)
+    multiplot(cube_dict, nplots,
+              projection=inargs.projection,
+              flow_type=inargs.flow_type,
+              title=inargs.title,
+              ofile=inargs.ofile)
     
-    ## Define the data
-    x = u_cube.coords('longitude')[0].points
-    y = u_cube.coords('latitude')[0].points
-    u = u_cube.data
-    v = v_cube.data
 
-    plt.figure(figsize=(8, 10))
-
-    ## Select the map projection
-    ax = plt.axes(projection=ccrs.SouthPolarStereo())
-   
-    ax.set_extent((x.min(), x.max(), y.min(), -30.0), crs=ccrs.PlateCarree())
-    
-    ## Plot coast and gridlines (currently an error with coastline plotting)
-    ax.coastlines()
-    ax.gridlines()
-    #ax.set_global()
-
-    ## Plot the data
-    # Streamplot
-    magnitude = (u ** 2 + v ** 2) ** 0.5
-    ax.streamplot(x, y, u, v, transform=ccrs.PlateCarree(), linewidth=2, density=2, color=magnitude)
-
-    # Wind vectors
-    #ax.quiver(x, y, u, v, transform=ccrs.PlateCarree(), regrid_shape=40) 
-
-    # Contour
-    if inargs.zg_file:
-        qplt.contour(zg_cube, colors='0.3', linewidths=2)
-
-    if inargs.title:
-        plt.title(inargs.title.replace('_',' '))
-
-    plt.savefig(inargs.ofile)
     gio.write_metadata(inargs.ofile)
 
 
@@ -168,38 +226,38 @@ improvements:
 
     # Input data
 
-    parser.add_argument("--u_file", type=str, default=None, 
-                        help="input file name for the zonal wind")
-    parser.add_argument("--u_var", type=str, default=None 
-                        help="zonal wind variable")
-    parser.add_argument("--v_file", type=str, default=None,
-                        help="input file name for the meridional wind")
-    parser.add_argument("--v_var", type=str, default=None,
-                        help="meridional wind variable")
+    parser.add_argument("infile", type=str, help="input file name")
+    parser.add_argument("variable", type=str, help="input file variable")
+    parser.add_argument("start", type=str, help="start date in YYYY-MM-DD format (can be none)")
+    parser.add_argument("end", type=str, help="end date in YYY-MM-DD, let START=END for single time step (can be None)")
+    parser.add_argument("timestep", type=str, help="for data with a time axis of len > 1 pick a timestep (can be None)")
+    parser.add_argument("type", type=str, help="plot type: can be uwind, vwind, contour, colour, stipple")
 
-    parser.add_argument("--contour_file", type=str, default=None, 
-                        help="input file name for the contours")
-    parser.add_argument("--contour_var", type=str, default=None,
-                        help="standard_name for the geopotential height zonal anomaly")
+    parser.add_argument("--infiles", type=str, action='append', default=[], nargs=5,
+                        metavar=('FILENAME', 'VAR', 'START', 'END', 'TIMESTEP', 'TYPE'),  
+                        help="additional input file name, variable, start date, end date and plot type [default: None]")
 
     # Time considerations
 
-    parser.add_argument("--start", type=str, default='',
-                        help="start date in YYYY-MM-DD format [default = None])")
-    parser.add_argument("--end", type=str, default='',
-                        help="end date in YYY-MM-DD format [default = None], let START=END for single time step (can be None)")
     parser.add_argument("--timestep", type=int, default=None,
                         help="By default multiple timesteps are averaged. This option allows the specification of a particular timestep")
  
+    # Plot options
+
+    parser.add_argument("--title", type=str, default=None,
+                        help="plot title [default: None]")
+    parser.add_argument("--projection", type=str, default='PlateCarree', choices=projections.keys(),
+                        help="map projection [default: PlateCarree]")
+    parser.add_argument("--flow_type", type=str, default='quiver', choices=('quiver', 'streamlines'),
+                        help="what to do with the uwind and vwind data")
+
     # Output options
 
     parser.add_argument("--ofile", type=str, default='test.png',
                         help="name of output file [default: test.png]")
-    parser.add_argument("--title", type=str, default=None,
-                        help="plot title [default: None]")
-    parser.add_argument("--projection", type=str, default='PlateCarree', choices=('PlateCarree', 'SouthPolarStereo'),
-                        help="map projection [default: PlateCarree]")
+    
 
     args = parser.parse_args()              
+    args.infiles.insert(0, [args.infile, args.variable, args.start, args.end, args.timestep, args.type])
 
     main(args)
