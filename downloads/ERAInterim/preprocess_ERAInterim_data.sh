0ecf2ae595dff9fdd31895e92418bc82e22addd0
DBrent <damien_irving@yahoo.com.au>
2015-01-05 03:37:31 +0000

    Added cdo fix script

--- downloads/ERAInterim/preprocess_ERAInterim_data.sh
+++ downloads/ERAInterim/preprocess_ERAInterim_data.sh
@@ -8,13 +8,13 @@
 #
 
 function usage {
-    echo "USAGE: bash $0 infile invar outfile outvar missval"
+    echo "USAGE: bash $0 infile invar outfile outvar cdofix"
     echo "   infile:      Input file name"
     echo "   invar:       Input variable name"
     echo "   outfile:     Output file name"
     echo "   outvar:      Output variable name"
-    echo "   missval:     Missing value"
-    echo "   e.g. bash $0 zfile.nc z new-zgfile.nc zg -32767"
+    echo "   cdofix:      Script for replacing attributes that cdo strips"
+    echo "   e.g. bash $0 zfile.nc z new-zgfile.nc zg ~/phd/data_processing/cdo_fix.sh"
     exit 1
 }
 
@@ -27,7 +27,7 @@ infile=$1
 invar=$2
 outfile=$3
 outvar=$4
-missval=$5
+cdofix=$5
 
 if [[ "${outvar}" = "zg" ]] ; then
     cdo invertlat -sellonlatbox,0,359.9,-90,90  -divc,9.80665 -daymean ${infile} ${outfile}   # Divude by standard gravity to go from geopotential to geopotential height
@@ -37,7 +37,7 @@ else
     cdo invertlat -sellonlatbox,0,359.9,-90,90 -daymean ${infile} ${outfile} 
 fi
 
-ncatted -O -a missing_value,${invar},o,c,${missval} ${outfile}  # CDO strips this, NCO and CDAT need it
+bash ${cdofix} ${outfile} ${invar}            # Put back the required attributes that CDO strips
 ncrename -O -v ${invar},${outvar} ${outfile}
 ncatted -O -a axis,time,c,c,T ${outfile}
 ncatted -O -a calendar,global,d,, ${outfile}  # Iris does not like this
