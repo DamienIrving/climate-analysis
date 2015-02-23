476c0b0598252c2b8d37d3d0e9954fa231d3118e
Damien Irving <damien_irving@yahoo.com.au>
2015-02-24 08:40:12 +1100

    Added python exe and code dir

--- visualisation/plot_composite_contour.sh
+++ visualisation/plot_composite_contour.sh
@@ -1,9 +1,11 @@
 function usage {
-    echo "USAGE: bash $0 contfile contvar outfile"
-    echo "   contfile:   Input file name for contour plot"
-    echo "   contvar:    Variable for contour plot"
-    echo "   outfile:    Output file name"
-    echo "   e.g. bash $0 zg_data.nc zg plot.png"
+    echo "USAGE: bash $0 contfile contvar outfile python_exe code_dir"
+    echo "   contfile:    Input file name for contour plot"
+    echo "   contvar:     Variable for contour plot"
+    echo "   outfile:     Output file name"
+    echo "   python_exe:  Python executable"
+    echo "   code_dir:    Directory that plot_map.py is in"
+    echo "   e.g. bash $0 zg_data.nc zg plot.png /usr/local/anaconda/bin/python ~/climate-analysis/visualisation/"
     exit 1
 }
 
@@ -16,6 +18,8 @@ fi
 contfile=$1
 contvar=$2
 outfile=$3
+python_exe=$4
+code_dir=$5
   
 
 if [ $contvar == 'zg' ] ; then
@@ -26,5 +30,17 @@ else
 fi
 
 
-/usr/local/anaconda/bin/python ~/phd/visualisation/plot_map2.py ${contfile} ${contvar}_annual none none none contour 1 3 2 --output_projection SouthPolarStereo --subplot_headings Annual none DJF MAM JJA SON --infiles ${contfile} ${contvar}_DJF none none none contour 3 --infiles ${contfile} ${contvar}_MAM none none none contour 4 --infiles ${contfile} ${contvar}_JJA none none none contour 5 --infiles ${contfile} ${contvar}_SON none none none contour 6 --contour_levels ${levels} --figure_size 9 16 --ofile ${outfile} --boxes zw31 blue solid --boxes zw32 blue solid --boxes zw33 blue solid 
+${python_exe} ${code_dir}/plot_map.py ${contfile} ${contvar}_annual none none none contour 1 3 2 \
+--output_projection SouthPolarStereo \
+--subplot_headings Annual none DJF MAM JJA SON \
+--infiles ${contfile} ${contvar}_DJF none none none contour 3 \
+--infiles ${contfile} ${contvar}_MAM none none none contour 4 \
+--infiles ${contfile} ${contvar}_JJA none none none contour 5 \
+--infiles ${contfile} ${contvar}_SON none none none contour 6 \
+--contour_levels ${levels} \
+--figure_size 9 16 \
+--ofile ${outfile} \
+--boxes zw31 blue solid \
+--boxes zw32 blue solid \
+--boxes zw33 blue solid
 
