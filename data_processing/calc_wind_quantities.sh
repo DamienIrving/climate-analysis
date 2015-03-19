f7d992bdf01f87d7cc71dbaa90b37863ab20393b
Damien Irving <damien_irving@yahoo.com.au>
2015-03-20 10:00:10 +1100

    Fixed typo

--- data_processing/calc_wind_quantities.sh
+++ data_processing/calc_wind_quantities.sh
@@ -4,21 +4,22 @@
 #
 
 function usage {
-    echo "USAGE: bash $0 ufile uvar vfile vvar outfile quantity python_exe code_dir temp_dir"
+    echo "USAGE: bash $0 quantity ufile uvar vfile vvar outfile cdofix python_exe code_dir temp_dir"
     echo "   quantity:    Name of quantity to calculate"
     echo "   ufile:       Input zonal wind file name"
     echo "   uvar:        Input zonal wind variable name"
     echo "   vfile:       Input zonal wind file name"
     echo "   vvar:        Input zonal wind variable name"
     echo "   outfile:     Output file name"
+    echo "   cdofix:      Script for replacing attributes that cdo strips"
     echo "   python_exe:  Python executable"
-    echo "   code_dir:    Directory that plot_map.py is in"
+    echo "   code_dir:    Directory that calc_wind_quantities.py is in"
     echo "   temp_dir:    Directory to store temporary data files"
-    echo "   e.g. bash $0 streamfunction ua_Merra_250hPa_daily_native.nc ua va_Merra_250hPa_daily_native.nc va sf_Merra_250hPa_daily_native.nc /usr/local/anaconda/bin/python ~/climate-analysis/data_processing /mnt/meteo0/data/simmonds/dbirving/temp"
+    echo "   e.g. bash $0 streamfunction ua_Merra_250hPa_daily_native.nc ua va_Merra_250hPa_daily_native.nc va sf_Merra_250hPa_daily_native.nc ~/climate-analysis/data_processing/cdo_fix.sh /usr/local/uvcdat/1.3.0/bin/python ~/climate-analysis/data_processing /mnt/meteo0/data/simmonds/dbirving/temp"
     exit 1
 }
 
-nargs=9
+nargs=10
 
 if [ $# -ne $nargs ] ; then
   usage
@@ -30,9 +31,10 @@ uvar=$3
 vfile=$4
 vvar=$5
 outfile=$6
-python_exe=$7
-code_dir=$8
-temp_dir=$9
+cdofix=$7
+python_exe=$8
+code_dir=$9
+temp_dir=${10}
 
 if [ ! -f $ufile ] ; then
     echo "Input U file doesn't exist: " $ufile
@@ -43,6 +45,14 @@ if [ ! -f $vfile ] ; then
     echo "Input V file doesn't exist: " $vfile
     usage
 fi
+
+if [ ${quantity} == 'streamfunction' ] ; then
+    outvar=sf
+else
+    echo "Unknown quantity: ${quantity}"
+    exit 1
+fi
+
 
 years=(1979 1982 1985 1988 1991 1994 1997 2000 2003 2006 2009 2012)
 temp_files=()
@@ -55,6 +65,6 @@ done
 
 cdo -O mergetime ${temp_files[@]} $outfile
 rm ${temp_files[@]}
-ncatted -O -a axis,time,c,c,T $outfile
+bash ${cdofix} ${outfile} ${outvar} 
 
 
