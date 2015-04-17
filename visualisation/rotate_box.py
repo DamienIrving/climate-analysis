a481a6eecc318ea143461fd661b15887c5c306b5
Damien Irving <damien_irving@yahoo.com.au>
2015-04-17 16:59:04 +1000

    Further work on box rotator

--- visualisation/rotate_box.py
+++ visualisation/rotate_box.py
@@ -6,7 +6,7 @@
 # Import general Python modules
 
 import os, sys, pdb
-import argparse
+import numpy, argparse
 
 # Import my modules
 
@@ -28,11 +28,25 @@ except ImportError:
 
 
 
+def main(inargs):
+    """Run the program."""
 
-phi, theta, psi = crot.north_pole_to_rotation_angles(np[0], np[1])
-rot_lats, rot_lons = crot.rotate_spherical(borders[side+'_lats'], borders[side+'_lons'], phi, theta, psi, invert=False)
-rot_lons_adjust = uconv.adjust_lon_range(rot_lons, radians=False, start=0.0)
+    fout = open(inargs.outfile, 'w')
+    phi, theta, psi = crot.north_pole_to_rotation_angles(inargs.north_pole_lat, inargs.north_plot_lon)
 
+    borders = {}
+    for border_number in range(0, len(inargs.corner) - 1):
+        start_lat, start_lon = inargs.corner[border_number]
+        end_lat, end_lon = inargs.corner[border_number + 1]
+        lats = numpy.arange(start_lat, end_lat + inargs.resolution, inargs.resolution)
+        lons = numpy.arange(start_lon, end_lon + inargs.resolution, inargs.resolution)
+        
+        rot_lats, rot_lons = crot.rotate_spherical(lats, lons, phi, theta, psi, invert=True)
+        rot_lons_adjust = uconv.adjust_lon_range(rot_lons, radians=False, start=0.0)
+
+        #fout.write(date+'\n')   write to file (maybe mesh lat and lon first?)
+
+    fout.close()
 
 
 if __name__ == '__main__':
@@ -49,9 +63,12 @@ example:
                                      argument_default=argparse.SUPPRESS,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    parser.add_argument("corners", type=float, help="corners of the shape")
     parser.add_argument("outfile", type=str, help="output file name")
-
+    parser.add_argument("north_pole_lat", type=str, "latitude of the rotated world north pole")
+    parser.add_argument("north_pole_lon", type=str, "longitude of the rotated world north pole")
+    
+    parser.add_argument("--corner", type=str, action='append', nargs=2, metavar=('LAT', 'LON'),  
+                        help="a corner of the shape")
     parser.add_argument("--resolution", type=float, default=1.0,
                         help="resolution of the shape (i.e. spacing between plotted points)")
 
