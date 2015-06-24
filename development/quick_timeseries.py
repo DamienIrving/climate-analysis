aa2156db4689fbcd0b1f3539b20bf7f09b0bfca9
Damien Irving <damien_irving@yahoo.com.au>
2015-06-24 14:21:37 +1000

    Added input argument handling

--- development/quick_timeseries.py
+++ development/quick_timeseries.py
@@ -1,16 +1,24 @@
+## Input: file_cdat file_xray var
+
+import sys
 import xray
 import matplotlib.pyplot as plt
 import numpy
 
-dset_cdat = xray.open_dataset('sam_ERAInterim_surface_030day-runmean_native.nc')
-dset_xray = xray.open_dataset('sam_ERAInterim_surface_030day-runmean_native_xray.nc')
+ifile_cdat = sys.argv[1]
+ifile_xray = sys.argv[2]
+var = sys.argv[3]
+
+dset_cdat = xray.open_dataset(ifile_cdat)
+dset_xray = xray.open_dataset(ifile_xray)
 
 
-data_cdat = dset_cdat['sam'].values[0:200]
-data_xray = dset_xray['sam'].values[0:200]
+data_cdat = dset_cdat[var].values[0:200]
+data_xray = dset_xray[var].values[0:200]
 xaxis = numpy.arange(0, len(data_cdat))
 
-plt.plot(xaxis, data_cdat)
-plt.plot(xaxis, data_xray)
+plt.plot(xaxis, data_cdat, label='cdat')
+plt.plot(xaxis, data_xray, label='xray')
+plt.legend()
 
 plt.savefig('test.png')
