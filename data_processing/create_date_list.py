69ef1ecbcfffd150e9e0e7dadacc84b852142f90
Damien Irving <damien_irving@yahoo.com.au>
2015-06-25 11:01:30 +1000

    Date list generation now xray compliant

--- data_processing/create_date_list.py
+++ data_processing/create_date_list.py
@@ -2,6 +2,7 @@
 
 import os, sys, pdb
 import argparse
+import xray
 
 # Import my modules
 
@@ -17,54 +18,44 @@ sys.path.append(modules_dir)
 
 try:
     import general_io as gio
-    import convenient_anaconda as aconv
     import convenient_universal as uconv
 except ImportError:
     raise ImportError('Must run this script from anywhere within the climate-analysis git repo')
+
 
 # Define functions
 
 def main(inargs):
     """Run the program"""
-   
-    # Read data 
-    indata, metadata = aconv.nc_to_df(inargs.infile, [inargs.metric])
-    metric_threshold = uconv.get_threshold(indata[inargs.metric], inargs.metric_threshold) 
-    
-    # Apply filters
-    dt_selector = aconv.pandas_dt_selector(indata.index, inargs.season, inargs.start, inargs.end)
-    selector = dt_selector
+
+    # Read data
+    dset_in = xray.open_dataset(inargs.infile)
+    gio.check_xrayDataset(dset_in, inargs.metric)
+
+    subset_dict = gio.get_subset_kwargs(inargs)
+    darray = dset_in[inargs.metric].sel(**subset_dict)
+
+    # Make selection
+    metric_threshold = uconv.get_threshold(darray.values, inargs.metric_threshold) 
     
     assert inargs.threshold_direction in ['greater', 'less']
     if inargs.threshold_direction == 'greater':
-        metric_selection = indata[inargs.metric] >= metric_threshold
+        indexes = darray >= metric_threshold
     elif inargs.threshold_direction == 'less':
-        metric_selection = indata[inargs.metric] <= metric_threshold
+        indexes = darray <= metric_threshold
 
-    selector = selector & metric_selection 
- 
-    data = indata[selector]
+    darray_selection = darray.loc[indexes]
 
-    # Metadata
-    metadata_dict = {inargs.infile: metadata}
-
-    gio.write_dates(inargs.outfile, data.index.tolist())
+    # Write outputs
+    gio.write_dates(inargs.outfile, darray_selection['time'].values)
+    metadata_dict = {inargs.infile: dset_in.attrs['history']}
     gio.write_metadata(inargs.outfile, file_info=metadata_dict)
 
 
 if __name__ == '__main__':
 
-    extra_info =""" 
-example:
-    
-author:
-    Damien Irving, d.irving@student.unimelb.edu.au
-
-"""
-
     description='Create a date list for a given input metric'
-    parser = argparse.ArgumentParser(description=description,
-                                     epilog=extra_info, 
+    parser = argparse.ArgumentParser(description=description, 
                                      argument_default=argparse.SUPPRESS,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
 
@@ -72,10 +63,8 @@ author:
     parser.add_argument("metric", type=str, help="Name of the input file metric to be used")
     parser.add_argument("outfile", type=str, help="Output file name")
     
-    parser.add_argument("--start", type=str, help="Time start filter (e.g. 1979-02-31)", default=None)
-    parser.add_argument("--end", type=str, help="Time end filter (e.g. 2012-12-31)", default=None)
-    parser.add_argument("--season", type=str, choices=('DJF', 'MAM', 'JJA', 'SON'), default=None,
-                        help="Season selector [default = all]")
+    parser.add_argument("--time", type=str, nargs=2, metavar=('START_DATE', 'END_DATE'),
+                        help="Time period over which to provide dates [default = entire]")
 
     parser.add_argument("--metric_threshold", type=str, default='75pct', 
                         help="Threshold metric value. Can be percentile (e.g. 75pct) or raw value.")
