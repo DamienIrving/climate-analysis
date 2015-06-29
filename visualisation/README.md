05b3a2b63f57c204ca50bb2ee980aaebd781347a
Damien Irving <damien_irving@yahoo.com.au>
2015-06-29 16:34:41 +1000

    Cleaned up visualisation directory

--- visualisation/README.md
+++ visualisation/README.md
@@ -4,4 +4,4 @@
 
 * `plot_map.py` - spatial maps
 * `plot_scatter.py` - scatter plots 
-* `plot_timeseries.py` - timeseries 
+* `plot_timeseries.py` - timeseries (old, not functional) 
--- visualisation/plot_variability_composite.sh
+++ visualisation/plot_variability_composite.sh
@@ -35,23 +35,24 @@ else
     exit 1
 fi
 
-${python_exe} ${code_dir}/plot_map.py ${posfile} ${var}_annual none none none contour0 1 3 2 \
---infiles ${posfile} ${var}_DJF none none none contour0 3 \
---infiles ${posfile} ${var}_MAM none none none contour0 4 \
---infiles ${posfile} ${var}_JJA none none none contour0 5 \
---infiles ${posfile} ${var}_SON none none none contour0 6 \
+${python_exe} ${code_dir}/plot_map.py 3 2 \
+--infile ${posfile} ${var}_annual none none none contour0 1 \
+--infile ${posfile} ${var}_DJF none none none contour0 3 \
+--infile ${posfile} ${var}_MAM none none none contour0 4 \
+--infile ${posfile} ${var}_JJA none none none contour0 5 \
+--infile ${posfile} ${var}_SON none none none contour0 6 \
 --output_projection SouthPolarStereo \
 --subplot_headings Annual none DJF MAM JJA SON \
---infiles ${neutralfile} ${var}_annual none none none contour1 1 \
---infiles ${neutralfile} ${var}_DJF none none none contour1 3 \
---infiles ${neutralfile} ${var}_MAM none none none contour1 4 \
---infiles ${neutralfile} ${var}_JJA none none none contour1 5 \
---infiles ${neutralfile} ${var}_SON none none none contour1 6 \
---infiles ${negfile} ${var}_annual none none none contour2 1 \
---infiles ${negfile} ${var}_DJF none none none contour2 3 \
---infiles ${negfile} ${var}_MAM none none none contour2 4 \
---infiles ${negfile} ${var}_JJA none none none contour2 5 \
---infiles ${negfile} ${var}_SON none none none contour2 6 \
+--infile ${neutralfile} ${var}_annual none none none contour1 1 \
+--infile ${neutralfile} ${var}_DJF none none none contour1 3 \
+--infile ${neutralfile} ${var}_MAM none none none contour1 4 \
+--infile ${neutralfile} ${var}_JJA none none none contour1 5 \
+--infile ${neutralfile} ${var}_SON none none none contour1 6 \
+--infile ${negfile} ${var}_annual none none none contour2 1 \
+--infile ${negfile} ${var}_DJF none none none contour2 3 \
+--infile ${negfile} ${var}_MAM none none none contour2 4 \
+--infile ${negfile} ${var}_JJA none none none contour2 5 \
+--infile ${negfile} ${var}_SON none none none contour2 6 \
 --contour_levels ${levels} \
 --figure_size 9 16 \
 --ofile ${outfile} \
