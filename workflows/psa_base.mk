2619e25c9f98eed15c3ac3ace2c83b817e0cbe20
Damien Irving <damien_irving@yahoo.com.au>
2015-08-10 10:35:48 +1000

    Added fourier analysis to psa workflow

--- workflows/psa_base.mk
+++ workflows/psa_base.mk
@@ -44,12 +44,14 @@ ${VROT_ANOM_RUNMEAN} : ${VROT_ORIG}
 	ncks -O -x -v time_bnds $@
 
 
-# Common indices
+# PSA identification
 
 ## Phase and amplitude of each Fourier component
 
-FOURIER_INFO=${ZW_DIR}/fourier_zw_${COE_WAVE_LABEL}-va_${DATASET}_${LEVEL}_${TSCALE_LABEL}_native.nc 
-${FOURIER_INFO} : ${V_RUNMEAN}
-	bash ${DATA_SCRIPT_DIR}/calc_fourier_transform.sh $< va $@ ${WAVE_MIN} ${WAVE_MAX} coefficients ${PYTHON} ${DATA_SCRIPT_DIR} ${TEMPDATA_DIR}
+FOURIER_COEFFICIENTS=${PSA_DIR}/fourier-vrot_${DATASET}_${LEVEL}-${LAT_LABEL}-${LON_LABEL}_${TSCALE_LABEL}-anom-wrt-all_native-${NPLABEL}.nc 
+${FOURIER_COEFFICIENTS} : ${VROT_ANOM_RUNMEAN}
+	${PYTHON} ${DATA_SCRIPT_DIR}/calc_fourier_transform.py $< vrot $@ 1 10 coefficients --latitude ${LAT_SEARCH_MIN} ${LAT_SEARCH_MAX} --valid_lon ${LON_SEARCH_MIN} ${LON_SEARCH_MAX} --avelat
 
-
+INVERSE_FT=${PSA_DIR}/ift-${WAVE_LABEL}-vrot_${DATASET}_${LEVEL}-${LAT_LABEL}-${LON_LABEL}_${TSCALE_LABEL}-anom-wrt-all_native-${NPLABEL}.nc  
+${INVERSE_FT} : ${VROT_ANOM_RUNMEAN}
+	${PYTHON} ${DATA_SCRIPT_DIR}/calc_fourier_transform.py $< vrot $@ ${WAVE_MIN} ${WAVE_MAX} hilbert --latitude ${LAT_SEARCH_MIN} ${LAT_SEARCH_MAX} --valid_lon ${LON_SEARCH_MIN} ${LON_SEARCH_MAX} --avelat
