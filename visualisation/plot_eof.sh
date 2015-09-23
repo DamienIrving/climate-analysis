92070c7a7fefbbc61aa7dafe215231d7d8b0553d
Damien Irving <damien_irving@yahoo.com.au>
2015-09-24 08:53:25 +1000

    Added search path to eof plot

--- visualisation/plot_eof.sh
+++ visualisation/plot_eof.sh
@@ -22,9 +22,18 @@ code_dir=$4
 ${python_exe} ${code_dir}/plot_map.py 2 2 \
 --infile ${infile} empirical_orthogonal_function_1 none none none colour0 1 PlateCarree \
 --infile ${infile} empirical_orthogonal_function_2 none none none colour0 2 PlateCarree \
---infile ${infile} empirical_orthogonal_function3 none none none colour0 3 PlateCarree \
---infile ${infile} empirical_orthogonal_function4 none none none colour0 4 PlateCarree \
+--infile ${infile} empirical_orthogonal_function_3 none none none colour0 3 PlateCarree \
+--infile ${infile} empirical_orthogonal_function_4 none none none colour0 4 PlateCarree \
 --palette RdBu_r \
 --subplot_headings EOF_1 EOF_2 EOF_3 EOF_4 \
---figure_size 9 16 \
+--figure_size 10 5 \
 --ofile ${outfile} \
+--region sh \
+--units correlation \
+--colourbar_ticks -1 -0.8 -0.6 -0.4 -0.2 0 0.2 0.4 0.6 0.8 1.0 \
+--line -10 -10 115 230 green solid RotatedPole_260E_20N low \
+--line 10 10 115 230 green solid RotatedPole_260E_20N low \
+--line -10 10 115 115 green solid RotatedPole_260E_20N low \
+--line -10 10 230 230 green solid RotatedPole_260E_20N low \
+
+#--output_projection SouthPolarStereo \
