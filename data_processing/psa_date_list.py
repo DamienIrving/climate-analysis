4504c321ccb8644a555721a2a8378836605542d5
Damien Irving <irving.damien@gmail.com>
2015-09-29 16:42:01 +1000

    psa_date_list will now feed csv to plot_psa_stats

--- data_processing/psa_date_list.py
+++ data_processing/psa_date_list.py
@@ -6,13 +6,7 @@ import numpy, pandas
 import xray
 import operator
 from itertools import groupby
-from scipy import stats
 from scipy.stats import rankdata
-
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-from matplotlib.collections import LineCollection
 
 # Import my modules
 
@@ -34,37 +28,7 @@ except ImportError:
 
 
 # Define functions
-
-#
-#def add_duration(DataFrame, metric, metric_threshold):
-#    """Add a duration column to the input DataFrame. 
-#
-#    An event is defined by the number of consecutive timesteps greater than 
-#    the metric_threshold.
-#    
-#    Note that every timestep is assigned a duration value equal to the number of
-#    days in the entire event.
-#    
-#    """
-#
-#    events = DataFrame[metric].map(lambda x: x > metric_threshold)
-#    event_list = events.tolist()
-#    grouped_events = [(k, sum(1 for i in g)) for k,g in groupby(event_list)]
-#
-#    duration = []
-#    for event in grouped_events:
-#        if event[0]:
-#            data = [event[1]] * event[1]
-#        else:
-#            data = [0] * event[1]
-#        duration.append(data)
-#
-#    DataFrame['duration'] = reduce(operator.add, duration)
-#    DataFrame['event'] = events
-#
-#    return DataFrame  
-
-
+ 
 season_months = {'DJF': (12, 1, 2), 'MAM': (3, 4, 5), 
                  'JJA': (6, 7, 8), 'SON': (9, 10, 11)}
 
@@ -78,61 +42,6 @@ def fix_boundary_data(data):
 
     return data
         
-
-def plot_phase(df, min_duration, ofile):
-    """Plot a line graph showing phase throughout PSA event lifecycle."""
-
-    # Collect up all the event phase info
-    event_phase_list = []
-    event_amp_list = []
-    current_event_phase = []
-    current_event_amp = []
-    for index, row in df.iterrows():
-        if row['duration'] == 0:
-            event_phase_list.append(current_event_phase)
-            event_amp_list.append(current_event_amp)
-            current_event_phase = [row['wave6_phase']]
-            current_event_amp = [row['env_max']]
-        else:
-            current_event_phase.append(row['wave6_phase'])
-            current_event_amp.append(row['env_max'])
-    event_phase_list.pop(0)    
-    event_amp_list.pop(0)
-
-    amp_max = numpy.max(sum(event_amp_list, []))
-    amp_min = numpy.min(sum(event_amp_list, []))
-
-    # Create the plot
-    fig = plt.figure(figsize=(8,11))
-    for phases, amps in zip(event_phase_list, event_amp_list):
-        if len(phases) > min_duration:
-            x_axis = numpy.arange(0, len(phases))
-            phase_data = fix_boundary_data(phases)
-            amp_data = numpy.array(amps)
-
-            slope, intercept, r_value, p_value, std_err = stats.linregress(x_axis, phase_data)
-            if slope > 0.2:
-                cmap = 'Reds'
-            elif slope < -0.2:
-                cmap = 'Blues'
-            else:
-                cmap = 'Greys'
-            
-            points = numpy.array([x_axis, phase_data]).T.reshape(-1, 1, 2)
-            segments = numpy.concatenate([points[:-1], points[1:]], axis=1)
-            lc = LineCollection(segments, cmap=plt.get_cmap(cmap), norm=plt.Normalize(amp_min, amp_max))
-            lc.set_array(amp_data)
-            lc.set_linewidth(3)
-            plt.gca().add_collection(lc)
-
-    plt.xlim(0, df['duration'].max())
-    plt.ylim(0, 83)
-
-    plt.xlabel('day')
-    plt.ylabel('wave 6 phase')
-   
-    plt.savefig(ofile, bbox_inches='tight')
-
 
 def main(inargs):
     """Run the program"""
@@ -172,30 +81,31 @@ def main(inargs):
         duration.append(data)
     final['duration'] = reduce(operator.add, duration)
 
-    if inargs.event_plot:
-        assert not inargs.phase_filter and not inargs.season_filter, \
-        "Have not added functionality to filter by phase or season for event plot"
-        plot_phase(final, inargs.duration_filter, inargs.output_file)
-    else: 
-	# Optional filtering by duration
-	if inargs.duration_filter:
-	    final = final.loc[final['duration'] > inargs.duration_filter]
+    if inargs.full_stats:
+        assert not inargs.phase_filter and not inargs.season_filter and not inargs.duration_filter, \
+        "Cannot filter by phase, season or duration for full stats, because zero duration info needed to separate events"
+        final.to_csv(inargs.output_file)
 
-	# Optional filtering by season
-	if inargs.season_filter:
+    else: 
+        # Optional filtering by duration
+        if inargs.duration_filter:
+            final = final.loc[final['duration'] > inargs.duration_filter]
+
+        # Optional filtering by season
+        if inargs.season_filter:
             season = inargs.season_filter
             months_subset = pandas.to_datetime(final.index.values).month
             bools_subset = (months_subset == season_months[season][0]) + (months_subset == season_months[season][1]) + (months_subset == season_months[season][2])
             final = final.loc[bools_subset]
 
-	# Optional filtering by wave phase
-	if inargs.phase_filter:
+        # Optional filtering by wave phase
+        if inargs.phase_filter:
             freq, phase_min, phase_max = inargs.phase_filter
             target_phase = 'wave%i_phase' %(freq)
             final = final.loc[(final[target_phase] > phase_min) & (final[target_phase] < phase_max)]
 
-	# Write date file
-	gio.write_dates(inargs.output_file, final.index.values)
+        # Write date file
+        gio.write_dates(inargs.output_file, final.index.values)
 
     metadata_dict = {inargs.fourier_file: dset_in.attrs['history']}
     gio.write_metadata(inargs.output_file, file_info=metadata_dict)
@@ -203,7 +113,7 @@ def main(inargs):
 
 if __name__ == '__main__':
 
-    description='Create a psa date list (or event summary plot) from Fourier information'
+    description='Create a psa date list (or stats csv) from Fourier information'
     parser = argparse.ArgumentParser(description=description, 
                                      argument_default=argparse.SUPPRESS,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -214,8 +124,8 @@ if __name__ == '__main__':
     parser.add_argument("--max_sign_change", type=int, default=5, 
                         help="maximum number of times the signal can change sign over the search domain [default = 5]")
 
-    parser.add_argument("--event_plot", action="store_true", default=False,
-                        help="switch for creating a phase/amplitude summary plot instead [default: False]")
+    parser.add_argument("--full_stats", action="store_true", default=False,
+                        help="switch for outputting a full stats file instead of just dates [default: False]")
 
     parser.add_argument("--duration_filter", type=int, default=None, 
                         help="minimum duration for a PSA event [default = no filter]")
@@ -223,7 +133,6 @@ if __name__ == '__main__':
                         help="only keep the selected season [default = no season filter]")
     parser.add_argument("--phase_filter", type=float, nargs=3, metavar=['FREQ', 'MIN_PHASE', 'MAX_PHASE'], default=None, 
                         help="phase range to retain at the given frequecy. [default = no filter]")
-
 
     args = parser.parse_args()            
     main(args)
---
+++ visualisation/plot_psa_stats.py
@@ -0,0 +1,227 @@
+# Import general Python modules
+
+import os, sys, pdb
+import math
+import numpy
+import pandas
+import argparse
+from scipy import stats
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+
+# Import my modules
+
+cwd = os.getcwd()
+repo_dir = '/'
+for directory in cwd.split('/')[1:]:
+    repo_dir = os.path.join(repo_dir, directory)
+    if directory == 'climate-analysis':
+        break
+
+modules_dir = os.path.join(repo_dir, 'modules')
+sys.path.append(modules_dir)
+
+try:
+    import general_io as gio
+    import convenient_universal as uconv
+except ImportError:
+    raise ImportError('Must run this script from anywhere within the climate-analysis git repo')
+
+
+# Define functions
+
+season_months = {'annual': None, 'DJF': (12, 1, 2), 'MAM': (3, 4, 5), 
+                 'JJA': (6, 7, 8), 'SON': (9, 10, 11)}
+
+
+def plot_event_summary(df, phase_freq, min_duration, ofile):
+    """Line graph showing phase/amplitude throughout the lifecycle of each PSA event."""
+
+    # Collect up all the event phase info
+    event_phase_list = []
+    event_amp_list = []
+    current_event_phase = []
+    current_event_amp = []
+    for index, row in df.iterrows():
+        if row['duration'] == 0:
+            event_phase_list.append(current_event_phase)
+            event_amp_list.append(current_event_amp)
+            current_event_phase = [row[phase_freq]]
+            current_event_amp = [row['env_max']]
+        else:
+            current_event_phase.append(row[phase_freq])
+            current_event_amp.append(row['env_max'])
+    event_phase_list.pop(0)    
+    event_amp_list.pop(0)
+
+    amp_max = numpy.max(sum(event_amp_list, []))
+    amp_min = numpy.min(sum(event_amp_list, []))
+
+    # Create the plot
+    fig = plt.figure(figsize=(8,11))
+    for phases, amps in zip(event_phase_list, event_amp_list):
+        if len(phases) > min_duration:
+            x_axis = numpy.arange(0, len(phases))
+            phase_data = fix_boundary_data(phases)
+            amp_data = numpy.array(amps)
+
+            slope, intercept, r_value, p_value, std_err = stats.linregress(x_axis, phase_data)
+            if slope > 0.2:
+                cmap = 'Reds'
+            elif slope < -0.2:
+                cmap = 'Blues'
+            else:
+                cmap = 'Greys'
+            
+            points = numpy.array([x_axis, phase_data]).T.reshape(-1, 1, 2)
+            segments = numpy.concatenate([points[:-1], points[1:]], axis=1)
+            lc = LineCollection(segments, cmap=plt.get_cmap(cmap), norm=plt.Normalize(amp_min, amp_max))
+            lc.set_array(amp_data)
+            lc.set_linewidth(3)
+            plt.gca().add_collection(lc)
+
+    plt.xlim(0, df['duration'].max())
+    plt.ylim(0, 83)
+
+    plt.xlabel('day')
+    plt.ylabel(phase_freq)
+   
+    plt.savefig(ofile, bbox_inches='tight')
+
+
+def running_mean(data, window):
+    """Calculate the running mean.
+
+    This is especially for the case where the start and end points join up.
+
+    """
+    
+    temp = numpy.append(data, data)
+    padded_data = numpy.append(temp, data)
+
+    ds = pandas.Series(padded_data)
+    runmean = pandas.rolling_mean(ds, window, center=True).values  #labels are right edge if center=False
+
+    central_runmean = runmean[len(data):len(data)*2]
+
+    return central_runmean
+
+
+def plot_phase_distribution(df, phase_freq, phase_res, window, seasonal=False):
+    """Plot a phase distribution histogram."""    
+    
+    if seasonal:
+        season_list = ['DJF', 'MAM', 'JJA', 'SON']
+        nrows, ncols = 2, 2
+        figure_size = (12, 10)
+    else:
+        season_list = ['annual']
+        nrows, ncols = 1, 1
+        figure_size = None
+
+    fig = plt.figure(figsize=figure_size)
+    for plot_num, season in enumerate(season_list):
+
+        if not season == 'annual':
+            months_subset = pandas.to_datetime(df['time'].values).month
+            bools_subset = (months_subset == season_months[season][0]) + (months_subset == season_months[season][1]) + (months_subset == season_months[season][2])
+            df_subset = df.loc[bools_subset]
+        else:
+            df_subset = df
+
+        hist, smooth_hist, bin_centers = phase_histogram(df_subset[phase_freq].values, window)
+        plot_histogram(hist, smooth_hist, bin_centers, nrows, ncols, season, plot_num + 1)	
+    
+    fig.savefig(inargs.ofile, bbox_inches='tight')
+
+
+def phase_histogram(phase_data, smoothing_window, phase_res=1.0):
+    """Calculate the phase data and bin it (i.e. create histogram)."""
+
+    assert type(phase_data) == numpy.ndarray
+
+    bin_edge_start = phase_data.min() - (phase_res / 2.)
+    bin_edge_end = phase_data.max() + phase_res + (phase_res / 2.)
+    bin_centers = numpy.arange(phase_data.min(), phase_data.max() + phase_res, phase_res)
+    hist, bin_edges = numpy.histogram(phase_data, bins=numpy.arange(bin_edge_start, bin_edge_end, phase_res))
+    smooth_hist = running_mean(hist, smoothing_window)
+
+    return hist, smooth_hist, bin_centers
+
+
+def plot_histogram(hist, smooth_hist, bin_centers, nrows, ncols, season, plotnum):
+    """Plot the phase histogram."""
+
+    ax = plt.subplot(nrows, ncols, plotnum)
+    plt.sca(ax)
+
+    ax.bar(bin_centers, hist, color='0.7')
+    ax.plot(bin_centers, smooth_hist) 
+    plt.title(season)
+    ax.set_ylabel('Frequency', fontsize='x-small')
+    ax.set_xlabel('Longitude', fontsize='x-small')
+
+
+def main(inargs):
+    """Run program."""
+
+    # Read the data
+    df = pandas.read_csv(inargs.infile)
+    phase_freq = 'wave%i_phase' %(inargs.freq)
+    
+    # Create the desired plot
+    if inargs.type == 'phase_distribution':
+        plot_phase_distribution(df, phase_freq, inargs.phase_res, 
+                                inargs.window, seasonal=inargs.seasonal)
+    elif inargs.type == 'event_summary':
+        plot_event_summary(df, phase_freq, inargs.min_duration, inargs.ofile)
+
+    # Sort out metadata
+    file_body = inargs.infile.split('.')[0]
+    with open (file_body+'.met', 'r') as metfile:
+        date_metadata=metfile.read()
+    metadata_dict = {inargs.infile: date_metadata}
+    gio.write_metadata(inargs.ofile, file_info=metadata_dict)
+
+
+if __name__ == '__main__':
+
+    extra_info =""" 
+example:
+
+author:
+    Damien Irving, d.irving@student.unimelb.edu.au
+
+"""
+
+    description='Plot PSA pattern statistics'
+    parser = argparse.ArgumentParser(description=description,
+                                     epilog=extra_info, 
+                                     argument_default=argparse.SUPPRESS,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    # Required data
+    parser.add_argument("infile", type=str, help="PSA statistics file from psa_date_list.py")
+    parser.add_argument("type", type=str, choices=("phase_distribution", "event_summary"), 
+                        help="Desired plot")    
+    parser.add_argument("ofile", type=str, help="Output file name")
+
+    parser.add_argument("--phase_freq", type=int, default=6, 
+                        help="Frequency to use to indicate the wave phase [default: 6]")
+    parser.add_argument("--phase_res", type=float, default=1.0, 
+                        help="Phase resolution (e.g. if phase data is deg lon, res is spacing between lons [default: 1.0]")
+    parser.add_argument("--window", type=int, default=10, 
+                        help="Running mean window [default: 10]")
+    parser.add_argument("--min_duration", type=int, default=0, 
+                        help="Minimum event duration [default: 0]")
+
+    parser.add_argument("--seasonal", action="store_true", default=False,
+                        help="switch for plotting the 4 seasons for phase distribution plot [default: False]")
+
+    
+    
+    args = parser.parse_args()            
+    main(args)
