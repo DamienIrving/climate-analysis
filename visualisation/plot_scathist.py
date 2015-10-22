654c9f1799fd5a91610a73c51ff070505f7f624c
Damien Irving <irving.damien@gmail.com>
2015-10-22 14:09:43 +1100

    scathist overlay works

--- visualisation/plot_scathist.py
+++ visualisation/plot_scathist.py
@@ -63,9 +63,7 @@ def main(inargs):
     """Run program."""
 
     metadata_dict = {}
-    inargs.subset.insert(0, None)
 
-    # Read the data
     x_dataframe, metadata_dict[inargs.xfile] = read_data(inargs.xfile, inargs.xvar) 
     y_dataframe, metadata_dict[inargs.yfile] = read_data(inargs.yfile, inargs.yvar) 
 
@@ -75,34 +73,26 @@ def main(inargs):
     dataframe = pandas.concat(dataframe_list, join='inner', axis=1)
     dataframe.columns = headers
     dataframe = dataframe.dropna()
-
-    # Read the data
-#    dset_x = xray.open_dataset(inargs.xfile)
-#    dset_y = xray.open_dataset(inargs.yfile)
-#    gio.check_xrayDataset(dset_x, inargs.xvar)
-#    gio.check_xrayDataset(dset_y, inargs.yvar)
-
-#    subset_dict = gio.get_subset_kwargs(inargs)
-#    darray_x = dset_x[inargs.xvar].sel(**subset_dict)
-#    darray_y = dset_y[inargs.yvar].sel(**subset_dict)
+    dataframe = dataframe.iloc[::7, :]
 
     colors = ['black', 'red', 'blue']
+    g = seaborn.JointGrid(x=inargs.xvar, y=inargs.yvar, data=dataframe)
     for index, subset_file in enumerate(inargs.subset):
+        print subset_file
+        if subset_file == 'all':
+            subset_file = None
         dt_list, metadata = calc_composite.get_datetimes(dataframe, subset_file)
         if subset_file:
             metadata_dict[subset_file] = metadata
         dataframe_selection = dataframe[dataframe.index.isin(dt_list)]
-        
-        #dt_list, metadata = calc_composite.get_datetimes(darray_x, subset_file)
-        #if subset_file:
-        #    metadata_dict[subset_file] = metadata
-        #darray_x_selection = darray_x.sel(time=dt_list)
-        #darray_y_selection = darray_y.sel(time=dt_list)
 
-        g = seaborn.jointplot(x=inargs.xvar, y=inargs.yvar, data=dataframe_selection.iloc[::10, :], color=colors[index])
- 
-        plt.savefig(inargs.ofile, bbox_inches='tight')
- 
+        g.x = dataframe_selection[inargs.xvar].values
+        g.y = dataframe_selection[inargs.yvar].values
+
+        g = g.plot_joint(plt.scatter, color=colors[index])
+        g = g.plot_marginals(seaborn.distplot, kde=True, color=colors[index])
+
+    plt.savefig(inargs.ofile, bbox_inches='tight')
     gio.write_metadata(inargs.ofile, file_info=metadata_dict)
 
 
@@ -110,13 +100,11 @@ if __name__ == '__main__':
 
     extra_info =""" 
 example:
-   /usr/local/anaconda/bin/python plot_scatter.py 
-   /mnt/meteo0/data/simmonds/dbirving/Indices/tos_CPC_surface_monthly_nino34.nc NINO34 
-   /mnt/meteo0/data/simmonds/dbirving/Indices/psl_Marshall_surface_monthly_SAM.nc SAM 
-   test_scatter.png 
+    python plot_scathist.py sam_data.nc sam nino34_data.nc nino34 outfile.png 
+    --subset all --subset dates_phase8-18.txt --subset dates_phase39-49.txt
 
 author:
-    Damien Irving, d.irving@student.unimelb.edu.au
+    Damien Irving, irving.damien@gmail.com
 
 """
 
@@ -134,7 +122,7 @@ author:
     parser.add_argument("ofile", type=str, help="Output file name")
     
     # Optional data
-    parser.add_argument("--subset", type=str, action='append', default=[],
+    parser.add_argument("--subset", type=str, action='append',
                         help="Date file for subset") 
 
     # Plot options
