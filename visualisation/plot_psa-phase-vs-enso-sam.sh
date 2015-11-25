d6aec087c092fe6ab67277055ff11c0e5b79d18d
Damien Irving <damien_irving@yahoo.com.au>
2015-11-25 14:07:34 +1100

    Added dpi

--- visualisation/plot_psa-phase-vs-enso-sam.sh
+++ visualisation/plot_psa-phase-vs-enso-sam.sh
@@ -63,6 +63,6 @@ for idx in "${!start_phases[@]}"; do
 done
 
 
-${python_exe} ${vis_dir}/plot_scathist.py ${sam_file} sam ${enso_file} ${enso_var} ${outfile} --subset all --subset ${temp_dir}/dates_phase0.txt --subset ${temp_dir}/dates_phase1.txt --ylabel Nino_3.4 --xlabel SAM
+${python_exe} ${vis_dir}/plot_scathist.py ${sam_file} sam ${enso_file} ${enso_var} ${outfile} --subset all --subset ${temp_dir}/dates_phase0.txt --subset ${temp_dir}/dates_phase1.txt --ylabel Nino_3.4 --xlabel SAM --dpi 300
 
 rm ${temp_files[@]}
