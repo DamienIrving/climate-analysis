e333afab34512ddd1b2b4f15f7163c8a332a16bb
Damien Irving <irving.damien@gmail.com>
2016-04-06 14:31:30 +1000

    Full draft of OHC script

--- data_processing/calc_heat_content.py
+++ data_processing/calc_heat_content.py
@@ -40,6 +40,21 @@ def simple_integration(vert_vector, level_steps):
     return areas.sum()
 
 
+def domain_text(level_axis, user_top, user_bottom):
+    """Text describing the vertical bounds over which the integration was performed."""
+    
+    if user_top and user_bottom:
+        level_text = '%f down to %f' %(user_top, user_bottom)
+    elif user_bottom:
+        level_text = 'input data surface (%f) down to %f' %(level_axis[0], user_bottom)
+    elif user_top:
+        level_text = '%f down to input data bottom (%f)' %(user_top, level_axis[-1])
+    else:
+        level_text = 'full depth of input data (%f down to %f)' %(level_axis[0], level_axis[-1])
+    
+    return level_text
+        
+
 def vertical_constraint(top_level, bottom_level):
     """Define vertical constraint for cube data loading."""
     
@@ -73,17 +88,17 @@ def main(inargs):
     time_coord = cube.coord('time')
     lat_coord = cube.coord('latitude')
     lon_coord = cube.coord('longitude')
+    lev_coord = cube.coord('depth')
 
     lev_bounds = cube.coord('depth').bounds
     #if no bounds: cube.coord('latitude').guess_bounds()
     lev_diffs = numpy.apply_along_axis(lambda x: x[1] - x[0], 1, lev_bounds)
       
     # Integrate vertically      
-    ohc_per_m2 = numpy.ma.apply_along_axis(simple_integration, 1, cube.data, lev_diffs)
-    #might want to divide by 10^9
+    integral = numpy.ma.apply_along_axis(simple_integration, 1, cube.data, lev_diffs)
+    ohc_per_m2 = (integral * inargs.density * inargs.specific_heat) / (10^inargs.scaling)
       
     # Write the output file
-    
     d = {}
     d['time'] = ('time', time_coord.points)
     d['latitude'] = ('latitude', lat_coord.points)
@@ -91,10 +106,13 @@ def main(inargs):
     d['ohc'] = (['time', 'latitude', 'longitude'], ohc_per_m2.data)
 
     dset_out = xray.Dataset(d)
+    notes_text = 'OHC integrated over %s' %(domain_text(lev_coord.points, 
+                                                        inargs.top_level,
+                                                        inargs.bottom_level))
     dset_out['ohc'].attrs =   {'standard_name': 'ocean_heat_content',
                                'long_name': 'ocean_heat_content',
-                               'units': 'J m-2',
-                               'notes': 'OHC integrated over ' %(np_lat, np_lon)}  # FIXME
+                               'units': '10^%d J m-2' %(inargs.scaling),
+                               'notes': notes_text} 
     gio.set_dim_atts(dset_out, str(time_coord.units))
 
     outfile_metadata = {inargs.infile: cube.attributes['history']}
@@ -111,9 +129,9 @@ example:
 author:
     Damien Irving, irving.damien@gmail.com
 notes:
+    The default density and specific heat of seawater are from:
+    Hobbs et al (2016). Journal of Climate, 29(5), 1639–1653. doi:10.1175/JCLI-D-15-0477.1
 
-references:
-    
 """
 
     description='Calculate ocean heat content'
@@ -130,6 +148,14 @@ references:
                         help="Only include data below this vertical level")
     parser.add_argument("--bottom_level", type=float, default=None,
                         help="Only include data above this vertical level")
+    
+    parser.add_argument("--density", type=float, default=1023,
+                        help="Density of seawater (in kg.m-3). Default of 1023 kg.m-3 from Hobbs2016.")
+    parser.add_argument("--specific_heat", type=float, default=4000,
+                        help="Specific heat of seawater (in J / kg.K). Default of 4000 J/kg.K from Hobbs2016")
+    
+    parser.add_argument("--scaling", type=int, default=9,
+                        help="Factor by which to scale heat content (default value of 9 gives units of 10^9 J m-2)")
     
     args = parser.parse_args()            
 
