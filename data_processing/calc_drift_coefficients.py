a0b41f541ecf03a4fe1b4d26e4c5523dd2cd5977
Damien Irving <irving.damien@gmail.com>
2016-04-15 16:50:52 +1000

    Switched to iris after dask MemeryError. Needs testing.

--- data_processing/calc_drift_coefficients.py
+++ data_processing/calc_drift_coefficients.py
@@ -10,8 +10,7 @@ Description:  Calculate the polynomial coefficents that characterise model drift
 import sys, os, pdb
 import argparse
 import numpy
-import xarray
-import dask.array as da
+import iris
 
 
 # Import my modules
@@ -34,62 +33,79 @@ except ImportError:
 
 # Define functions
 
-def polyfit(data):
+history = []
+
+def edit_attributes(cube, field, filename):
+    """Remove the attributes that typically differ between files.
+
+    (i.e. these cause iris to throw a ConstraintMismatchError when you try and merge
+    cubes arising from different files)  
+
+    """ 
+    cube.attributes.pop('creation_date', None)
+    cube.attributes.pop('tracking_id', None)
+    history.append(cube.attributes['history'])
+    cube.attributes.pop('history', None)
+
+
+def polyfit(data, time_axis):
     """Fit polynomial to data."""    
 
-    if data[0] == missing_value:
-        #return numpy.array([missing_value]*4)[:, None, None, None]
-        return numpy.array([missing_value]*4)
+    if data.mask[0]:
+        return numpy.array([1e+20]*4)  # FIXME: Missing value handling
     else:    
-        #return numpy.polynomial.polynomial.polyfit(time_axis, data.squeeze(), 3)[:, None, None, None] 
-        return numpy.polynomial.polynomial.polyfit(time_axis, data.squeeze(), 3)
+        return numpy.polynomial.polynomial.polyfit(time_axis, data, 3)
 
 
 def main(inargs):
     """Run the program."""
-    
-    # Read the data
-    dset = xarray.open_mfdataset(inargs.infiles, decode_cf=False)
-    
-    global time_axis
-    time_axis = dset.time.values
-    ntimes = len(time_axis)
-    
-    global missing_value
-    missing_value = dset[inargs.var].missing_value
 
-    # Chunk
-    #dset_rechunked = dset.chunk({'time': ntimes, 'lev' : 1, 'lat' : 1, 'lon' : 1})
-    #darray = dset_rechunked[inargs.var].data
-    darray = dset[inargs.var].values
+    # Read the data
+    cube = iris.load(inargs.infiles, inargs.var, callback=edit_attributes)
+    cube = cube.concatenate_cube()
+
+    coord_names = [coord.name() for coord in cube.coords(dim_coords=True)]
+    assert coord_names[0] == 'time', "First axis must be time"
+   
+    time_axis = cube.coord('time').points 
 
     # Calculate coefficients for cubic polynomial
-    #coefficients = darray.map_blocks(polyfit, chunks=(ntimes, 1, 1, 1))
-    coefficients = numpy.apply_along_axis(polyfit, 0, darray) 
+    #need to loop through levels here: for i, x_slice in enumerate(cube.slices(['time', 'latitude', 'longitude'])):
+    coefficients = numpy.ma.apply_along_axis(polyfit, 0, cube.data, time_axis) 
+
+    # Get all the metadata
+    cube.attributes['polynomial'] = 'a + bx + cx^2 + dx^3'
+    cube.attributes['time_unit'] = str(cube.coord('time').units)
+    cube.attributes['history'] = gio.write_metadata(file_info=history[0])  # FIXME: This needs to cater for lots of files
 
     # Write the output file
-    dims = dset[inargs.var].dims[1:]
-    d = {}
-    for dim in dims:
-        d[dim] = dset[dim]
+    dim_coords = []
+    for i, coord_name in enumerate(coord_names[1:]):
+        dim_coords.append((cube.coord(coord_name), i))
 
-    d['a'] = (dims, coefficients[0, ...])
-    d['b'] = (dims, coefficients[1, ...])
-    d['c'] = (dims, coefficients[2, ...])
-    d['d'] = (dims, coefficients[3, ...])
+    if cube.aux_coords:
+        assert len(cube.aux_coords) == 2, "Script can only deal with two auxillary coordinates"
+        dims = range(0, len(coefficients.shape) - 1)
+        aux_coords = [(cube.aux_coords[0], [dims[-2], dims[-1]]), (cube.aux_coords[1], [dims[-2], dims[-1]])]
+    else:
+        aux_coords = None
+        
+    out_cubes = []
+    for i, coef in enumerate(['a', 'b', 'c', 'd']):
+        iris.std_names.STD_NAMES['coefficient_a'] = {'canonical_units': ' '}
+        out_cubes.append(iris.cube.Cube(coefficients[i, ...],
+                                        standard_name='coefficient_%s' %(coeff),
+                                        long_name='coefficient %s' %(coeff),
+                                        var_name=coeff,
+                                        units=' ',
+                                        attributes=temperature_cube.attributes,
+                                        dim_coords_and_dims=dim_coords,
+                                        aux_coords_and_dims=aux_coords))
 
-    dset_out = xarray.Dataset(d)
+    cube_list = iris.cube.CubeList(out_cubes)
+    out_cube = cube_list.concatenate()
 
-    for outvar in ['a', 'b', 'c', 'd']: 
-        dset_out[outvar].attrs['long_name'] = outvar
-        dset_out[outvar].attrs['standard_name'] = outvar
-        dset_out[outvar].attrs['units'] = ' '
-        dset_out[outvar].encoding['_FillValue'] = missing_value
-
-    dset.attrs['polynomial'] = 'a + bx + cx^2 + dx^3'
-    gio.set_global_atts(dset_out, dset.attrs, {inargs.infiles[-1]: dset.attrs['history'],})
-
-    dset_out.to_netcdf(inargs.outfile)
+    iris.save(out_cube, inargs.outfile)
 
 
 if __name__ == '__main__':
@@ -110,7 +126,7 @@ notes:
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
 
     parser.add_argument("infiles", type=str, nargs='*', help="Input file names")
-    parser.add_argument("var", type=str, help="Input file variable")
+    parser.add_argument("var", type=str, help="Input file variable (standard_name)")
     parser.add_argument("outfile", type=str, help="Output file name")
 
 
