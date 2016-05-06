d1618c54160bb0b089ce47d614826a2c213321d4
Damien Irving <irving.damien@gmail.com>
2016-05-06 15:52:07 +1000

    2D OHC ready for testing

--- data_processing/calc_ohc_maps.py
+++ data_processing/calc_ohc_maps.py
@@ -12,6 +12,7 @@ import argparse
 import numpy
 import iris
 from iris.experimental.regrid import regrid_weighted_curvilinear_to_rectilinear
+from iris.analysis.cartography import cosine_latitude_weights
 
 # Import my modules
 
@@ -33,25 +34,116 @@ except ImportError:
 
 # Define functions
 
-def make_grid(lat_values, lon_values):
-    """Make a dummy cube with desired grid."""
-       
-    latitude = iris.coords.DimCoord(lat_values,
-                                    standard_name='latitude',
-                                    units='degrees_north',
-                                    coord_system=None)
-    longitude = iris.coords.DimCoord(lon_values,                    
-                                     standard_name='longitude',
-                                     units='degrees_east',
-                                     coord_system=None)
+def add_metadata(orig_cube, new_cube, dims, inargs):
+    """Add metadata to the output cube.
+    
+    dims = '3D' or '2D'
+    
+    """
 
-    dummy_data = numpy.zeros((len(lat_values), len(lon_values)))
-    new_cube = iris.cube.Cube(dummy_data, dim_coords_and_dims=[(latitude, 0), (longitude, 1)])
+    standard_name = 'ocean_heat_content_%s'  %(dims)
+    units = '10^%d J m-2' %(inargs.scaling)
+    iris.std_names.STD_NAMES[standard_name] = {'canonical_units': units}
 
-    new_cube.coord('longitude').guess_bounds()
-    new_cube.coord('latitude').guess_bounds()
+    out_cube.standard_name = standard_name
+    out_cube.long_name = 'ocean heat content %s'  %(dims)
+    out_cube.var_name = 'ohc_%s'  %(dims)
+    out_cube.units = units
+    out_cube.attributes = orig_cube.attributes
+
+    depth_text = 'OHC integrated over %s' %(gio.vertical_bounds_text(in_cube.coord('depth').points, inargs.min_depth, inargs.max_depth))
+    new_cube.attributes['depth_bounds'] = depth_text
+
+    infile_history = {inargs.infile: orig_cube.attributes['history']}
+    new_cube.attributes['history'] = gio.write_metadata(file_info=infile_history)
 
     return new_cube
+
+
+def broadcast_weights(weights, axis_index, data_shape):
+    """Broadcast the one dimesional weights array to same shape as data."""
+
+    dim = 0
+    while dim < axis_index:
+        weights = weights[numpy.newaxis, ...]
+        weights = numpy.repeat(weights, data_shape[dim], axis=0)
+        dim = dim + 1
+    
+    dim = axis_index + 1
+    while dim < len(data_shape):    
+        weights = weights[..., numpy.newaxis]
+        weights = numpy.repeat(weights, data_shape[dim], axis=-1)
+        dim = dim + 1
+
+    return weights
+
+
+def calc_ohc_2D(cube, weights, inargs):
+    """Calculate the 2D OHC field (time, latitude)"""
+
+    integral = cube.collapsed(['depth', 'longitude'], iris.analysis.SUM, weights=weights)
+    ohc_per_m = (integral * inargs.density * inargs.specific_heat) / (10**inargs.scaling)
+
+    return ohc_per_m
+
+
+def calc_ohc_3D(cube, weights, inargs):
+    """Calculate the 3D OHC field (time, latitude, longitude)"""
+
+    integral = cube.collapsed('depth', iris.analysis.SUM, weights=weights)
+    ohc_per_m2 = (integral * inargs.density * inargs.specific_heat) / (10**inargs.scaling)
+
+    return ohc_per_m2
+
+
+def calc_vertical_weights(depth_axis, coord_list, data_shape):
+    """Calculate vertical weights. 
+
+    Defined as the distance (m) between levels.
+
+    """
+
+    # Calculate weights
+    if not depth_axis.has_bounds():
+        depth_axis.guess_bounds()
+    level_bounds = depth_axis.bounds
+
+    level_diffs = numpy.apply_along_axis(lambda x: x[1] - x[0], 1, level_bounds)
+
+    #guess_bounds can produce negative bound at surface
+    if level_bounds[0][0] < 0.0:
+        level_diffs[0] = level_diffs[0] + level_bounds[0][0]
+
+    # Broadcast to size of data
+    depth_index = coord_list.index('depth')
+    level_diffs = broadcast_weights(level_diffs, depth_index, data_shape)
+
+    return level_diffs
+
+
+def calc_zonal_weights(cube, coord_list):
+    """Calculate zonal weights.
+
+    Defined as the zonal distance (m) spanned by each grid box. 
+
+    The length of a degree of longitude is a function of 
+      latitude. The formula for the length of one degree of 
+      longitude is (pi/180) a cos(lat), where a is the radius of 
+      the earth.
+
+    """
+
+    lon_axis = cube.coord('longitude')
+    lon_index = coord_list.index('depth')
+
+    coslat = cosine_latitude_weights(cube)
+    radius = iris.analysis.cartography.DEFAULT_SPHERICAL_EARTH_RADIUS
+    lon_diffs = numpy.apply_along_axis(lambda x: x[1] - x[0], 1, lon_axis.bounds)
+    lon_diffs = broadcast_weights(lon_diffs, lon_index, cube.shape)
+
+    lon_extents = (math.pi / 180.) * radius * coslat * lon_diffs
+
+    return lon_extents
 
 
 def curvilinear_to_rectilinear(cube):
@@ -90,28 +182,6 @@ def curvilinear_to_rectilinear(cube):
     return new_cube, coord_names
 
 
-def add_metadata(in_cube, out_cube, inargs):
-    """Add metadata to the output cube."""
-
-    standard_name = 'ocean_heat_content'
-    units = '10^%d J m-2' %(inargs.scaling)
-    iris.std_names.STD_NAMES[standard_name] = {'canonical_units': units}
-
-    out_cube.standard_name = standard_name
-    out_cube.long_name = 'ocean heat content'
-    out_cube.var_name = 'ohc'
-    out_cube.units = units
-    out_cube.attributes = in_cube.attributes
-
-    depth_text = 'OHC integrated over %s' %(gio.vertical_bounds_text(in_cube.coord('depth').points, inargs.min_depth, inargs.max_depth))
-    out_cube.attributes['depth_bounds'] = depth_text
-
-    infile_history = {inargs.infile: in_cube.attributes['history']}
-    out_cube.attributes['history'] = gio.write_metadata(file_info=infile_history)
-
-    return out_cube
-
-
 def get_anomaly_data(inargs):
     """Read the input data and calculate anomaly if necessary."""
 
@@ -129,74 +199,25 @@ def get_anomaly_data(inargs):
     return cube
 
 
-def calc_vertical_weights(depth_axis, data_shape):
-    """Calculate vertical weights. 
+def make_grid(lat_values, lon_values):
+    """Make a dummy cube with desired grid."""
+       
+    latitude = iris.coords.DimCoord(lat_values,
+                                    standard_name='latitude',
+                                    units='degrees_north',
+                                    coord_system=None)
+    longitude = iris.coords.DimCoord(lon_values,                    
+                                     standard_name='longitude',
+                                     units='degrees_east',
+                                     coord_system=None)
 
-    Defined as the distance (m) between levels.
+    dummy_data = numpy.zeros((len(lat_values), len(lon_values)))
+    new_cube = iris.cube.Cube(dummy_data, dim_coords_and_dims=[(latitude, 0), (longitude, 1)])
 
-    """
+    new_cube.coord('longitude').guess_bounds()
+    new_cube.coord('latitude').guess_bounds()
 
-    # Calculate weights
-    if not depth_axis.has_bounds():
-        depth_axis.guess_bounds()
-    level_bounds = depth_axis.bounds
-
-    level_diffs = numpy.apply_along_axis(lambda x: x[1] - x[0], 1, level_bounds)
-
-    #guess_bounds can produce negative bound at surface
-    if level_bounds[0][0] < 0.0:
-        level_diffs[0] = level_diffs[0] + level_bounds[0][0]
-
-    # Broadcast to size of data
-    depth_index = coord_list.index('depth')
-    level_diffs = broadcast_weights(level_diffs, depth_index, data_shape)
-
-    return level_diffs
-
-
-def broadcast_weights(weights, axis_index, data_shape):
-    """Broadcast the one dimesional weights array to same shape as data."""
-
-    dim = 0
-    while dim < axis_index:
-        weights = weights[numpy.newaxis, ...]
-        weights = numpy.repeat(weights, data_shape[dim], axis=0)
-        dim = dim + 1
-    
-    dim = axis_index + 1
-    while dim < len(data_shape):    
-        weights = weights[..., numpy.newaxis]
-        weights = numpy.repeat(weights, data_shape[dim], axis=-1)
-        dim = dim + 1
-
-    return weights
-
-
-def calc_zonal_weights(cube, ):
-    """Calculate zonal weights.
-
-
-    """
-
-    coslat = cosine_latitude_weights(cube)
-
-    radius = iris.analysis.cartography.DEFAULT_SPHERICAL_EARTH_RADIUS
-    lon_diffs = numpy.apply_along_axis(lambda x: x[1] - x[0], 1, lon_axis.bounds)
-    lon_extents = (math.pi / 180.) * radius * weights[0,:,:] * lon_diffs
-
-
-
-
-    return lon_extents
-
-
-def calc_ohc_2D():
-    """ """
-
-    integral = cube.collapsed('depth', iris.analysis.SUM, weights=weights)
-    ohc_per_m2 = (integral * inargs.density * inargs.specific_heat) / (10**inargs.scaling)
-
-    return level_diffs
+    return new_cube
 
 
 def main(inargs):
@@ -211,15 +232,16 @@ def main(inargs):
     vertical_weights = calc_vertical_weights(cube.coord('depth'), coord_names, cube.shape)
     zonal_weights = calc_zonal_weights(cube, coord_names)
 
-    ohc_per_m2 = calc_ohc_2D(cube, coord_names, vertical_weights)
-    ohc_per_m = calc_ohc_1D(cube, coord_names, vertical_weights * zonal_weights)
+    ohc_per_m2 = calc_ohc_3D(cube, vertical_weights, inargs)
+    ohc_per_m = calc_ohc_2D(cube, vertical_weights * zonal_weights, inargs)
    
-    integral = cube.collapsed('depth', iris.analysis.SUM, weights=weights)
-    ohc_per_m2 = (integral * inargs.density * inargs.specific_heat) / (10**inargs.scaling)
-
     # Write the output file
-    ohc_per_m2 = add_metadata(cube, ohc_per_m2, inargs)
-    iris.save(ohc_per_m2, inargs.outfile)
+    ohc_per_m2 = add_metadata(cube, ohc_per_m2, '3D', inargs)
+    ohc_per_m = add_metadata(cube, ohc_per_m, '2D', inargs)
+
+    cube_list = iris.cube.CubeList(ohc_per_m2, ohc_per_m)
+    out_cube = cube_list.concatenate()
+    iris.save(out_cube, inargs.outfile)
 
 
 if __name__ == '__main__':
