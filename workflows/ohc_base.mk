0d675396830c0e3d286e29fb429bfa24b1e9e0ea
Damien Irving <irving.damien@gmail.com>
2016-05-12 16:30:51 +1000

    Basic elements of ohc makefile working

--- workflows/ohc_base.mk
+++ workflows/ohc_base.mk
@@ -13,26 +13,26 @@ all : ${TARGET}
 
 # Data
 
-TEMPERATURE_FILES=${UA6_CMIP5_DIR}/${ORGANISATION}/${MODEL}/${EXPERIMENT}/mon/ocean/thetao/${RUN}/thetao_Omon_${MODEL}_${EXPERIMENT}_${RUN}_*.nc
-VOLUME_FILE=${UA6_CMIP5_DIR}/${ORGANISATION}/${MODEL}/${EXPERIMENT}/fx/ocean/volcello/r0i0p0/volcello_fx_${MODEL}_${EXPERIMENT}_r0i0p0.nc
+TEMPERATURE_FILES=$(wildcard ${UA6_CMIP5_DIR}/${ORGANISATION}/${MODEL}/${EXPERIMENT}/mon/ocean/thetao/${RUN}/thetao_Omon_${MODEL}_${EXPERIMENT}_${RUN}_*.nc)
+VOLUME_FILE=$(wildcard ${UA6_CMIP5_DIR}/${ORGANISATION}/${MODEL}/${EXPERIMENT}/fx/ocean/volcello/r0i0p0/volcello_fx_${MODEL}_${EXPERIMENT}_r0i0p0.nc)
 
 CLIMATOLOGY_FILE=${MY_CMIP5_DIR}/${ORGANISATION}/${MODEL}/${EXPERIMENT}/mon/ocean/thetao/${RUN}/thetao_Omon_${MODEL}_${EXPERIMENT}_${RUN}_annual-climatology.nc
-${CLIMATOLOGY_FILE} : ${TEMPERATURE_FILES}
-	python ${DATA_SCRIPT_DIR}/calc_climatology.py $< $@
+${CLIMATOLOGY_FILE} : 
+	python ${DATA_SCRIPT_DIR}/calc_climatology.py ${TEMPERATURE_FILES} $@
 
 
 # OHC metrics
 
 OHC_METRICS_FILE=${MY_CMIP5_DIR}/${ORGANISATION}/${MODEL}/${EXPERIMENT}/mon/ocean/ohc-metrics/${RUN}/ohc-metrics_Omon_${MODEL}_${EXPERIMENT}_${RUN}_all.nc
-${OHC_METRICS_FILE} : ${TEMPERATURE_FILES} ${CLIMATOLOGY_FILE} ${VOLUME_FILE}
-	python ${DATA_SCRIPT_DIR}/calc_ohc_metrics.py $< sea_water_potential_temperature $@ --climatology_file $(word 2,$^) --volume_file $(word 3,$^) #--max_depth ${MAX_DEPTH}
+${OHC_METRICS_FILE} : ${CLIMATOLOGY_FILE}
+	python ${DATA_SCRIPT_DIR}/calc_ohc_metrics.py ${TEMPERATURE_FILES} sea_water_potential_temperature $@ --climatology_file $< --volume_file ${VOLUME_FILE}
 
 #plot...
 
 # OHC maps
 OHC_FILE=${MY_CMIP5_DIR}/${ORGANISATION}/${MODEL}/${EXPERIMENT}/mon/ocean/ohc/${RUN}/ohc_Omon_${MODEL}_${EXPERIMENT}_${RUN}_all.nc
-${OHC_FILE} : ${TEMPERATURE_FILES} ${CLIMATOLOGY_FILE}
-	python ${DATA_SCRIPT_DIR}/calc_ohc_maps.py $< sea_water_potential_temperature $@ --climatology_file $(word 2,$^) #--max_depth ${MAX_DEPTH}
+${OHC_FILE} : ${CLIMATOLOGY_FILE}
+	python ${DATA_SCRIPT_DIR}/calc_ohc_maps.py ${TEMPERATURE_FILES} sea_water_potential_temperature $@ --climatology_file $<
 
 OHC_PLOT=${MY_CMIP5_DIR}/${ORGANISATION}/${MODEL}/${EXPERIMENT}/mon/ocean/ohc/${RUN}/ohc_Omon_${MODEL}_${EXPERIMENT}_${RUN}_${START_DATE}_${END_DATE}.${FIG_TYPE}
 ${OHC_PLOT} : ${OHC_FILE}
--- workflows/ohc_config.mk
+++ workflows/ohc_config.mk
@@ -5,8 +5,8 @@
 MY_DATA_DIR=/g/data/r87/dbi599
 UA6_DATA_DIR=/g/data/ua6
 CMIP5_DIR_START=/drstree/CMIP5/GCM
-MY_CMIP5_DIR=${MY_DATA_DIR}/${CMIP5_DIR_START}
-UA6_CMIP5_DIR=${UA6_DATA_DIR}/${CMIP5_DIR_START}
+MY_CMIP5_DIR=${MY_DATA_DIR}${CMIP5_DIR_START}
+UA6_CMIP5_DIR=${UA6_DATA_DIR}${CMIP5_DIR_START}
 
 DATA_SCRIPT_DIR=~/climate-analysis/data_processing
 VIS_SCRIPT_DIR=~/climate-analysis/visualisation
@@ -22,4 +22,4 @@ RUN=r1i1p1
 
 
 
-TARGET=
+TARGET=${MY_CMIP5_DIR}/${ORGANISATION}/${MODEL}/${EXPERIMENT}/mon/ocean/ohc-metrics/${RUN}/ohc-metrics_Omon_${MODEL}_${EXPERIMENT}_${RUN}_all.nc
