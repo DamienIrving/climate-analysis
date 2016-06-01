59ca591f6619d144f4b0f57a1f6c7cad09f6d71f
Damien Irving <irving.damien@gmail.com>
2016-06-01 12:09:50 +1000

    ohc_base.sh now functional, needs dry run options

--- workflows/ohc_base.sh
+++ workflows/ohc_base.sh
@@ -4,6 +4,8 @@
 
 function usage {
     echo "USAGE: bash $0 model experiments"
+    echo " "
+    echo " "
     echo "   e.g. bash $0 ACCESS1-0 historical rcp85 historicalMisc"
     exit 1
 }
@@ -23,19 +25,40 @@ experiments=$@
 # Determine runs based on model and experiment
 
 for experiment in "${experiments[@]}"; do
-    if [[ ${model} == 'CSIRO-' && ${experiment} == 'historical' ]] ; then
-        runs=( r1i1p1 r1i1p2 )
-        organisation='CSIRO-QCCCE'
+    if [[ ${model} == 'CSIRO-Mk3-6-0' && ${experiment} == 'historical' ]] ; then
+        runs=( r1i1p1 r2i1p1 r3i1p1 r4i1p1 r5i1p1 r6i1p1 r7i1p1 r8i1p1 r9i1p1 r10i1p1 )
+        organization='CSIRO-QCCCE'
 
-    elif [[ ${model} == 'CSIRO-' && ${experiment} == 'historical' ]] ; then
-        runs=
-        organisation=
+    elif [[ ${model} == 'CSIRO-Mk3-6-0' && ${experiment} == 'noAA' ]] ; then
+        experiment='historicalMisc'
+        runs=( r1i1p3 r2i1p3 r3i1p3 r4i1p3 r5i1p3 r6i1p3 r7i1p3 r8i1p3 r9i1p3 r10i1p3 )
+        organization='CSIRO-QCCCE'
+
+    elif [[ ${model} == 'CSIRO-Mk3-6-0' && ${experiment} == 'AA' ]] ; then
+        experiment='historicalMisc'
+        runs=( r1i1p4 r2i1p4 r3i1p4 r4i1p4 r5i1p4 r6i1p4 r7i1p4 r8i1p4 r9i1p4 r10i1p4 )
+        organization='CSIRO-QCCCE'
+
+    elif [[ ${model} == 'ACCESS1-0' && ${experiment} == 'historical' ]] ; then
+        runs=( r1i1p1 ) # incomplete
+        organization='CSIRO-BOM'
 
     else
         echo "Unrecognised model (${model}) / experiment (${experiment}) combination"
         exit 1
-fi
+    fi
+
+    for run in "${runs[@]}"; do
+        sed -i "s/^\(ORGANIZATION\s*=\s*\).*$/\ORGANIZATION=${organization}/" ohc_config.mk
+        sed -i "s/^\(MODEL\s*=\s*\).*$/\MODEL=${model}/" ohc_config.mk
+        sed -i "s/^\(PERIMENT\s*=\s*\).*$/\PERIMENT=${experiment}/" ohc_config.mk
+        sed -i "s/^\(RUN\s*=\s*\).*$/\RUN=${run}/" ohc_config.mk
+        make -f ohc_base.mk
+    done
 done
 
 
-sed -i "s/^\(RUN\s*=\s*\).*$/\RUN=$RUN/" ohc_config.mk
+
+
+
+
