3903a87f5f74e72b4723f0721481afe4520cda86
Damien Irving <irving.damien@gmail.com>
2016-06-01 13:56:27 +1000

    Added -n and -B make options

--- workflows/ohc_base.sh
+++ workflows/ohc_base.sh
@@ -4,56 +4,65 @@
 
 function usage {
     echo "USAGE: bash $0 model experiments"
-    echo " "
-    echo " "
-    echo "   e.g. bash $0 ACCESS1-0 historical rcp85 historicalMisc"
+    echo "   e.g. bash $0 CSIRO-Mk3-6-0 historical noAA"
     exit 1
 }
 
 # Read inputs
 
-nargs=2
-if [ $# -ne $nargs ] ; then
-  usage
-fi
+OPTIND=1 
+options=' '
+while getopts ":nB" opt; do
+  case $opt in
+    n)
+      options+=' -n' >&2
+      ;;
+    B)
+      options+=' -B' >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+shift $((OPTIND-1))
 
 model=$1
 shift
-experiments=$@
-
+experiments=( $@ )
 
 # Determine runs based on model and experiment
 
 for experiment in "${experiments[@]}"; do
     if [[ ${model} == 'CSIRO-Mk3-6-0' && ${experiment} == 'historical' ]] ; then
         runs=( r1i1p1 r2i1p1 r3i1p1 r4i1p1 r5i1p1 r6i1p1 r7i1p1 r8i1p1 r9i1p1 r10i1p1 )
-        organization='CSIRO-QCCCE'
+        organisation='CSIRO-QCCCE'
 
     elif [[ ${model} == 'CSIRO-Mk3-6-0' && ${experiment} == 'noAA' ]] ; then
         experiment='historicalMisc'
         runs=( r1i1p3 r2i1p3 r3i1p3 r4i1p3 r5i1p3 r6i1p3 r7i1p3 r8i1p3 r9i1p3 r10i1p3 )
-        organization='CSIRO-QCCCE'
+        organisation='CSIRO-QCCCE'
 
     elif [[ ${model} == 'CSIRO-Mk3-6-0' && ${experiment} == 'AA' ]] ; then
         experiment='historicalMisc'
         runs=( r1i1p4 r2i1p4 r3i1p4 r4i1p4 r5i1p4 r6i1p4 r7i1p4 r8i1p4 r9i1p4 r10i1p4 )
-        organization='CSIRO-QCCCE'
+        organisation='CSIRO-QCCCE'
 
     elif [[ ${model} == 'ACCESS1-0' && ${experiment} == 'historical' ]] ; then
         runs=( r1i1p1 ) # incomplete
-        organization='CSIRO-BOM'
+        organisation='CSIRO-BOM'
 
     else
         echo "Unrecognised model (${model}) / experiment (${experiment}) combination"
-        exit 1
+        usage
     fi
 
     for run in "${runs[@]}"; do
-        sed -i "s/^\(ORGANIZATION\s*=\s*\).*$/\ORGANIZATION=${organization}/" ohc_config.mk
+        sed -i "s/^\(ORGANISATION\s*=\s*\).*$/\ORGANISATION=${organisation}/" ohc_config.mk
         sed -i "s/^\(MODEL\s*=\s*\).*$/\MODEL=${model}/" ohc_config.mk
-        sed -i "s/^\(PERIMENT\s*=\s*\).*$/\PERIMENT=${experiment}/" ohc_config.mk
+        sed -i "s/^\(EXPERIMENT\s*=\s*\).*$/EXPERIMENT=${experiment}/" ohc_config.mk
         sed -i "s/^\(RUN\s*=\s*\).*$/\RUN=${run}/" ohc_config.mk
-        make -f ohc_base.mk
+        make ${options} -f ohc_base.mk
     done
 done
 
