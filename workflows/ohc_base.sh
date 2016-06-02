dced5ad849ef23bc0d42315a67245a8f97e2c7b0
Damien Irving <irving.damien@gmail.com>
2016-06-02 10:18:09 +1000

    Fixed volume run

--- workflows/ohc_base.sh
+++ workflows/ohc_base.sh
@@ -33,20 +33,26 @@ experiments=( $@ )
 
 # Determine runs based on model and experiment
 
+volrun='r0i0p0'
 for experiment in "${experiments[@]}"; do
     if [[ ${model} == 'CSIRO-Mk3-6-0' && ${experiment} == 'historical' ]] ; then
-        runs=( r1i1p1 r2i1p1 r3i1p1 r4i1p1 r5i1p1 r6i1p1 r7i1p1 r8i1p1 r9i1p1 r10i1p1 )
+        runs=(  r2i1p1 r3i1p1 ) 
+        #r1i1p1 r4i1p1 r5i1p1 r6i1p1 r7i1p1 r8i1p1 r9i1p1 r10i1p1
         organisation='CSIRO-QCCCE'
 
     elif [[ ${model} == 'CSIRO-Mk3-6-0' && ${experiment} == 'noAA' ]] ; then
         experiment='historicalMisc'
-        runs=( r1i1p3 r2i1p3 r3i1p3 r4i1p3 r5i1p3 r6i1p3 r7i1p3 r8i1p3 r9i1p3 r10i1p3 )
+        runs=( r1i1p3 r2i1p3 r3i1p3 )
+        #r4i1p3 r5i1p3 r6i1p3 r7i1p3 r8i1p3 r9i1p3 r10i1p3
         organisation='CSIRO-QCCCE'
+        volrun='r0i0p3'
 
     elif [[ ${model} == 'CSIRO-Mk3-6-0' && ${experiment} == 'AA' ]] ; then
         experiment='historicalMisc'
-        runs=( r1i1p4 r2i1p4 r3i1p4 r4i1p4 r5i1p4 r6i1p4 r7i1p4 r8i1p4 r9i1p4 r10i1p4 )
+        runs=( r1i1p4 r2i1p4 r3i1p4 )
+        #r4i1p4 r5i1p4 r6i1p4 r7i1p4 r8i1p4 r9i1p4 r10i1p4
         organisation='CSIRO-QCCCE'
+        volrun='r0i0p4'
 
     elif [[ ${model} == 'ACCESS1-0' && ${experiment} == 'historical' ]] ; then
         runs=( r1i1p1 ) # incomplete
@@ -62,7 +68,9 @@ for experiment in "${experiments[@]}"; do
         sed -i "s/^\(MODEL\s*=\s*\).*$/\MODEL=${model}/" ohc_config.mk
         sed -i "s/^\(EXPERIMENT\s*=\s*\).*$/EXPERIMENT=${experiment}/" ohc_config.mk
         sed -i "s/^\(RUN\s*=\s*\).*$/\RUN=${run}/" ohc_config.mk
+        sed -i "s/^\(VOLUME_RUN\s*=\s*\).*$/\VOLUME_RUN=${volrun}/" ohc_config.mk
         make ${options} -f ohc_base.mk
+        echo "DONE: ${model} ${experiment} ${run}: make ${options} -f ohc_base.mk"
     done
 done
 
