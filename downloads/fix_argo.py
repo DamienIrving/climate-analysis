05163d9a3ba34ccd3febdb3a23e2a7f795afa8a1
Damien Irving <irving.damien@gmail.com>
2016-09-01 12:15:28 +1000

    Added climatology option

--- downloads/fix_argo.py
+++ downloads/fix_argo.py
@@ -39,42 +39,45 @@ def main(inargs):
     """Run the program."""
 
     cubes = iris.load(inargs.infile)
-    anomaly_cube = cubes[1]
+    if inargs.climatology:
+        cube = cubes[0]
+    else:
+        cube = cubes[1]
 
     # Edit variable attributes
-    anomaly_cube.attributes = {'comment': anomaly_cube.long_name}
+    cube.attributes = {'comment': cube.long_name}
     if inargs.variable == 'temperature':
-        anomaly_cube.var_name = 'to'
-        anomaly_cube.standard_name = 'sea_water_temperature'
-        anomaly_cube.long_name = 'Sea Water Temperature'
-        anomaly_cube.units = 'K'
+        cube.var_name = 'to'
+        cube.standard_name = 'sea_water_temperature'
+        cube.long_name = 'Sea Water Temperature'
+        cube.units = 'K'
     elif inargs.variable == 'salinity':
-        anomaly_cube.var_name = 'so'
-        anomaly_cube.standard_name = 'sea_water_salinity'
-        anomaly_cube.long_name = 'Sea Water Salinity'
-        anomaly_cube.units = ' '
+        cube.var_name = 'so'
+        cube.standard_name = 'sea_water_salinity'
+        cube.long_name = 'Sea Water Salinity'
+        cube.units = 'g/kg'
 
     # Edit latitude attributes
-    argo_lat = anomaly_cube.coord('latitude')
+    argo_lat = cube.coord('latitude')
     argo_lat.var_name = 'lat'
     argo_lat.long_name = 'latitude'
 
     # Edit latitude attributes
-    argo_lon = anomaly_cube.coord('longitude')
+    argo_lon = cube.coord('longitude')
     argo_lon.var_name = 'lon'
     argo_lon.long_name = 'longitude'
 
     # Edit time attributes
-    argo_time = anomaly_cube.coord('TIME')
+    if not inargs.climatology:
+        argo_time = cube.coord('TIME')
+        new_unit = cf_units.Unit('days since 2004-01-01 00:00:00', calendar='gregorian')  
+        argo_time.convert_units(new_unit)
 
-    new_unit = cf_units.Unit('days since 2004-01-01 00:00:00', calendar='gregorian')  
-    argo_time.convert_units(new_unit)
-
-    argo_time.var_name = 'time'
-    argo_time.long_name = 'time'
+        argo_time.var_name = 'time'
+        argo_time.long_name = 'time'
 
     # Edit depth attributes
-    argo_depth = anomaly_cube.coord('PRESSURE')
+    argo_depth = cube.coord('PRESSURE')
     argo_depth.var_name = 'lev'
     argo_depth.long_name = 'ocean depth coordinate'
     argo_depth.standard_name = 'depth'
@@ -82,8 +85,8 @@ def main(inargs):
     # Write output file
     timestamp = datetime.datetime.now().strftime("%a %b %d %H:%M:%S %Y")
     old_history = timestamp + ': Scripps Institution of Oceanography gridded argo %s data downloaded from http://www.argo.ucsd.edu/Gridded_fields.html'  %(inargs.variable)
-    anomaly_cube.attributes['history'] = gio.write_metadata(file_info={inargs.infile: old_history})
-    iris.save(anomaly_cube, inargs.outfile)
+    cube.attributes['history'] = gio.write_metadata(file_info={inargs.infile: old_history})
+    iris.save(cube, inargs.outfile)
 
 
 if __name__ == '__main__':
@@ -106,5 +109,8 @@ notes:
     parser.add_argument("variable", type=str, choices=('temperature', 'salinity'), help="Input variable") 
     parser.add_argument("outfile", type=str, help="Output file name")
     
+    parser.add_argument("--climatology", action="store_true", default=False,
+                        help="switch for outputting a climatology file instead")
+
     args = parser.parse_args()             
     main(args)
