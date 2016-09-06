f98ac9220da531d71962fc9d7a71113bce53b28e
Damien Irving <irving.damien@gmail.com>
2016-09-06 17:02:43 +1000

    sa metric now functional

--- data_processing/calc_salinity_amplification.py
+++ data_processing/calc_salinity_amplification.py
@@ -28,14 +28,12 @@ sys.path.append(modules_dir)
 try:
     import general_io as gio
     import convenient_universal as uconv
-    import spatial_weights
 except ImportError:
     raise ImportError('Must run this script from anywhere within the climate-analysis git repo')
 
 # Define functions
 
 history = []
-
 
 def read_area(area_file):
     """Read the optional area file."""
@@ -83,6 +81,8 @@ def calc_mean_anomaly(cube, sign, grid_areas):
 
     cube.data.mask = new_mask
     cube = cube.collapsed(['longitude', 'latitude'], iris.analysis.MEAN, weights=grid_areas)
+    cube.remove_coord('longitude')
+    cube.remove_coord('latitude')
     
     return cube
 
@@ -107,12 +107,27 @@ def calc_amplification_metric(cube, grid_areas, atts):
 
     iris.std_names.STD_NAMES['sa'] = {'canonical_units': 'g/kg'}
     metric.var_name = 'sa'
-    #metric.standard_name = 'sea_water_salinity_amplification'
-    #metric.long_name = 'Sea Water Salinity Amplification'
+    metric.standard_name = 'sea_surface_salinity'
+    metric.long_name = 'Sea Surface Salinity'
     metric.units = 'g/kg'
     metric.attributes = atts
 
     return metric
+
+
+def get_area_weights(cube, area_cube):
+    """Get area weights for averaging"""
+
+    if area_cube:
+        area_weights = uconv.broadcast_array(area_cube.data, [1, 2], cube.shape)
+    else:
+        if not cube.coord('latitude').has_bounds():
+            cube.coord('latitude').guess_bounds()
+        if not cube.coord('longitude').has_bounds():
+            cube.coord('longitude').guess_bounds()
+        area_weights = iris.analysis.cartography.area_weights(cube)
+
+    return area_weights
 
 
 def main(inargs):
@@ -120,26 +135,18 @@ def main(inargs):
 
     in_cubes = iris.load(inargs.infiles, inargs.var, callback=save_history)
     equalise_attributes(in_cubes)
+    cube = in_cubes.concatenate_cube()
 
     area_cube = read_area(inargs.area_file) 
 
     atts = set_attributes(inargs, in_cubes[0], area_cube)
 
-    out_cubes = []
-    for cube in in_cubes:
+    cube = cube.rolling_window('time', iris.analysis.MEAN, 12)
 
-        if not area_cube:
-            #cube.coord('latitude').guess_bounds()
-            #cube.coord('longitude').guess_bounds()
-            area_cube = iris.analysis.cartography.area_weights(cube)
+    area_weights = get_area_weights(cube, area_cube)
+    metric = calc_amplification_metric(cube, area_weights, atts)
 
-        metric = calc_amplification_metric(cube, area_cube, atts)
-        out_cubes.append(metric)
-
-    out_cubes = iris.cube.CubeList(out_cubes)
-    out_cube = out_cubes.concatenate_cube()
-
-    iris.save(out_cube, inargs.outfile)
+    iris.save(metric, inargs.outfile)
 
 
 if __name__ == '__main__':
@@ -156,8 +163,8 @@ author:
                                      argument_default=argparse.SUPPRESS,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    parser.add_argument("infiles", type=str, nargs='*', help="Input temperature data files")
-    parser.add_argument("var", type=str, help="Input temperature variable name (i.e. the standard_name)")
+    parser.add_argument("infiles", type=str, nargs='*', help="Input salinity data files")
+    parser.add_argument("var", type=str, help="Input salinity variable name (i.e. the standard_name)")
     parser.add_argument("outfile", type=str, help="Output file name")
     
     parser.add_argument("--area_file", type=str, default=None, 
