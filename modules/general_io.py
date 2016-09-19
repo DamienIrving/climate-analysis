24f87f504c1555bfe76b0a1899e428456a011741
Damien Irving <irving.damien@gmail.com>
2016-09-20 08:44:22 +1000

    Wider range of valid salinity

--- modules/general_io.py
+++ modules/general_io.py
@@ -328,7 +328,7 @@ def salinity_unit_check(cube):
     data_max = cube.data.max()
     data_min = cube.data.min()
     
-    assert data_max < 55.0, 'Data max is %f' %(data_max)
+    assert data_max < 75.0, 'Data max is %f' %(data_max)
     assert data_min > 2.0 , 'Data min is %f' %(data_min)
 
     cube.units = 'g/kg'   #cf_units.Unit('unknown')
