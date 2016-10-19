afb2802022f22a7a10da00e598b9ec4de4229862
Damien Irving <irving.damien@gmail.com>
2016-10-19 16:23:30 +1100

    Small fix

--- data_processing/fix_salinity_units.sh
+++ data_processing/fix_salinity_units.sh
@@ -13,8 +13,9 @@ infiles=($@)
 
 for infile in "${infiles[@]}"; do
 
+    echo ${infile} 
     outfile=`echo ${infile} | sed s:ua6:r87/dbi599:`
-    python ~/climate-analysis/data_processing/fix_salinity_units.py ${infile} ${outfile}
+    /g/data/r87/dbi599/miniconda2/envs/default/bin/python ~/climate-analysis/data_processing/fix_salinity_units.py ${infile} ${outfile}
 
 done
 
