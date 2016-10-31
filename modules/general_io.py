6ae9b72bda1765830d5049529b9aa32e795d4d39
Damien Irving <irving.damien@gmail.com>
2016-10-31 16:29:07 +1100

    Fixed typo

--- modules/general_io.py
+++ modules/general_io.py
@@ -351,7 +351,7 @@ def salinity_unit_check(cube):
     data_min = cube.data.min()
     
     assert data_max < 100.0, 'Data max is %f' %(data_max)
-    assert data_min > 0.0 , 'Data min is %f' %(data_min)
+    assert data_min >= 0.0 , 'Data min is %f' %(data_min)
 
     cube.units = 'g/kg'   #cf_units.Unit('unknown')
 
