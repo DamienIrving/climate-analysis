c41244404829b3e81116f05a73b97e318df44e26
Damien Irving <irving.damien@gmail.com>
2016-12-09 17:03:03 +1100

    Edits

--- visualisation/plot_ocean_trend_ensemble.sh
+++ visualisation/plot_ocean_trend_ensemble.sh
@@ -30,8 +30,8 @@ ncols=$2
 group=$3
 format=$4
 
-regions="globe"  # indian pacific atlantic
-variables="thetao"  # so 
+regions="globe indian pacific atlantic" # 
+variables="so"  # thetao 
 
 python='/g/data/r87/dbi599/miniconda2/envs/default/bin/python'
 dummy_ticks="--ticks 0 10"
@@ -69,29 +69,29 @@ elif [[ ${group} == 'r1' ]] ; then
     group_label="r1"
 fi
 
-runs="${canesm_runs} ${ccsm_runs} ${csiro_runs} ${fgoals_runs} ${gfdl_cm_runs} ${gfdl_esm_runs} ${gisseh_p1_runs} ${gisseh_p3_runs} ${gisser_p1_runs} ${gisser_p3_runs} ${ipsl_runs} ${noresm_runs}"
+runs="blank ${canesm_runs} ${ccsm_runs} ${csiro_runs} ${fgoals_runs} ${gfdl_cm_runs} ${gfdl_esm_runs} ${gisseh_p1_runs} ${gisseh_p3_runs} ${gisser_p1_runs} ${gisser_p3_runs} ${ipsl_runs} ${noresm_runs}"
 
 for region in ${regions[@]}; do
 
     for variable in ${variables[@]}; do 
 
-        if [[ ${group} == 'ensemble' ]] ; then
+        if [[ ${group} == 'ensmean' ]] ; then
 
             if [[ ${variable} == 'so' ]] ; then
                 long_name=sea_water_salinity
-                obs_ticks="--ticks 2.5 0.5"
-                canesm_ticks="--ticks 2.5 0.5"
-                ccsm_ticks="--ticks 2.0 0.4"
-                csiro_ticks="--ticks 1.5 0.3"
-                fgoals_ticks="--ticks 2.5 0.5"
-                gfdl_cm_ticks="--ticks 2.5 0.5"
-                gfdl_esm_ticks="--ticks 3.75 0.75"
+                obs_ticks="--ticks 2.0 0.4"
+                canesm_ticks="--ticks 2.0 0.4"
+                ccsm_ticks="--ticks 1.5 0.3"
+                csiro_ticks="--ticks 1.25 0.25"
+                fgoals_ticks="--ticks 2.0 0.4"
+                gfdl_cm_ticks="--ticks 2.0 0.4"
+                gfdl_esm_ticks="--ticks 2.0 0.4"
                 gisseh_p1_ticks="--ticks 1.5 0.3"
                 gisseh_p3_ticks="--ticks 1.5 0.3"
                 gisser_p1_ticks="--ticks 1.5 0.3"
                 gisser_p3_ticks="--ticks 2.5 0.5"
-                ipsl_ticks="--ticks 3.75 0.75"
-                noresm_ticks="--ticks 5 1"
+                ipsl_ticks="--ticks 2.0 0.4"
+                noresm_ticks="--ticks 2.0 0.4"
                 palette='BrBG_r'
             elif [[ ${variable} == 'thetao' ]] ; then
                 long_name=sea_water_potential_temperature
@@ -111,13 +111,13 @@ for region in ${regions[@]}; do
                 palette='RdBu_r'
             fi
 
-            ticks="${canesm_ticks} ${ccsm_ticks} ${csiro_ticks} ${fgoals_ticks} ${gfdl_cm_ticks} ${gfdl_esm_ticks} ${gisseh_p1_ticks} ${gisseh_p3_ticks} ${gisser_p1_ticks} ${gisser_p3_ticks} ${ipsl_ticks} ${noresm_ticks}" # ${obs_ticks} 
+            ticks="${obs_ticks} ${canesm_ticks} ${ccsm_ticks} ${csiro_ticks} ${fgoals_ticks} ${gfdl_cm_ticks} ${gfdl_esm_ticks} ${gisseh_p1_ticks} ${gisseh_p3_ticks} ${gisser_p1_ticks} ${gisser_p3_ticks} ${ipsl_ticks} ${noresm_ticks}"  
 
         elif [[ ${group} == 'r1' ]] ; then
 
             if [[ ${variable} == 'so' ]] ; then
                 long_name=sea_water_salinity
-                ticks="--ticks 3.0 0.5"
+                ticks="--ticks 2.4 0.4"
                 palette='BrBG_r'
             elif [[ ${variable} == 'thetao' ]] ; then
                 long_name=sea_water_potential_temperature
@@ -141,7 +141,7 @@ for region in ${regions[@]}; do
         ipsl_file="/g/data/r87/dbi599/drstree/CMIP5/GCM/IPSL/IPSL-CM5A-LR/${experiment}/yr/ocean/${variable}-maps/r1i1p1/${variable}-maps-time-trend_Oyr_IPSL-CM5A-LR_${experiment}_r1i1p1_1950-01-01_2000-12-31.nc"
         noresm_file="/g/data/r87/dbi599/drstree/CMIP5/GCM/NCC/NorESM1-M/${experiment}/yr/ocean/${variable}-maps/r1i1p1/${variable}-maps-time-trend_Oyr_NorESM1-M_${experiment}_r1i1p1_1950-01-01_2000-12-31.nc"
         
-        data_files="${canesm_file} ${ccsm_file} ${csiro_file} ${fgoals_file} ${gfdl_cm_file} ${gfdl_esm_file} ${gisseh_p1_file} ${gisseh_p3_file} ${gisser_p1_file} ${gisser_p3_file} ${ipsl_file} ${noresm_file}"  # ${obs_file} 
+        data_files="${obs_file} ${canesm_file} ${ccsm_file} ${csiro_file} ${fgoals_file} ${gfdl_cm_file} ${gfdl_esm_file} ${gisseh_p1_file} ${gisseh_p3_file} ${gisser_p1_file} ${gisser_p3_file} ${ipsl_file} ${noresm_file}"  
 
         outfile=/g/data/r87/dbi599/figures/ocean_trend_ensembles/${experiment}/${variable}-maps-time-trend-zonal-mean-${region}_Oyr_ensemble_${experiment}_${group}_1950-01-01_2000-12-31_${nrows}by${ncols}.${format}
 
@@ -159,12 +159,12 @@ for region in ${regions[@]}; do
         ipsl_clim="/g/data/r87/dbi599/drstree/CMIP5/GCM/IPSL/IPSL-CM5A-LR/${experiment}/yr/ocean/${variable}-maps/r1i1p1/${variable}-maps-clim_Oyr_IPSL-CM5A-LR_${experiment}_r1i1p1_all.nc"
         noresm_clim="/g/data/r87/dbi599/drstree/CMIP5/GCM/NCC/NorESM1-M/${experiment}/yr/ocean/${variable}-maps/r1i1p1/${variable}-maps-clim_Oyr_NorESM1-M_${experiment}_r1i1p1_all.nc"
         
-        climatology_files="${canesm_clim} ${ccsm_clim} ${csiro_clim} ${fgoals_clim} ${gfdl_cm_clim} ${gfdl_esm_clim} ${gisseh_p1_clim} ${gisseh_p3_clim} ${gisser_p1_clim} ${gisser_p3_clim} ${ipsl_clim} ${noresm_clim}"  # ${obs_clim} 
+        climatology_files="${obs_clim} ${canesm_clim} ${ccsm_clim} ${csiro_clim} ${fgoals_clim} ${gfdl_cm_clim} ${gfdl_esm_clim} ${gisseh_p1_clim} ${gisseh_p3_clim} ${gisser_p1_clim} ${gisser_p3_clim} ${ipsl_clim} ${noresm_clim}"  
 
     if [[ ${dry_run} == 'yes' ]] ; then
-        echo  ${python} ~/climate-analysis/visualisation/plot_ocean_trend_ensemble.py ${data_files[@]} ${long_name} ${region} ${nrows} ${ncols} ${outfile} --palette ${palette} --climatology_files ${climatology_files[@]} ${ticks[@]} --scale_factor 3 --experiment historical --no_title --runs ${runs[@]}
+        echo  ${python} ~/climate-analysis/visualisation/plot_ocean_trend_ensemble.py ${data_files[@]} ${long_name} ${region} ${nrows} ${ncols} ${outfile} --palette ${palette} --climatology_files ${climatology_files[@]} ${ticks[@]} --scale_factor 3 --experiment historical  --runs ${runs[@]}
     else
-         ${python} ~/climate-analysis/visualisation/plot_ocean_trend_ensemble.py ${data_files[@]} ${long_name} ${region} ${nrows} ${ncols} ${outfile} --palette ${palette} --climatology_files ${climatology_files[@]} ${ticks[@]} --scale_factor 3 --experiment historical --no_title --runs ${runs[@]}
+         ${python} ~/climate-analysis/visualisation/plot_ocean_trend_ensemble.py ${data_files[@]} ${long_name} ${region} ${nrows} ${ncols} ${outfile} --palette ${palette} --climatology_files ${climatology_files[@]} ${ticks[@]} --scale_factor 3 --experiment historical  --runs ${runs[@]}
         echo ${outfile}
     fi
 
@@ -176,6 +176,9 @@ done
 # historicalGHG zonal plots
 
 experiment='historicalGHG'
+nrows=3
+ncols=4
+
 
 if [[ ${group} == 'ensmean' ]] ; then
     canesm_runs="r[1-5]i1p1"
@@ -209,20 +212,20 @@ for region in ${regions[@]}; do
 
     for variable in ${variables[@]}; do
 
-        if [[ ${group} == 'ensemble' ]] ; then
+        if [[ ${group} == 'ensmean' ]] ; then
 
             if [[ ${variable} == 'so' ]] ; then
                 long_name=sea_water_salinity
-                canesm_ticks="--ticks 3.75 0.75"
-                ccsm_ticks="--ticks 3.0 0.6"
-                csiro_ticks="--ticks 2.5 0.5"
-                fgoals_ticks="--ticks 3.0 0.6"
-                gfdl_cm_ticks="--ticks 3.75 0.75"
-                gfdl_esm_ticks="--ticks 5 1"
-                gisseh_ticks="--ticks 2.5 0.5"
-                gisser_ticks="--ticks 3.5 0.7"
-                ipsl_ticks="--ticks 3.75 0.75"
-                noresm_ticks="--ticks 5 1"
+                canesm_ticks="--ticks 2.0 0.4"
+                ccsm_ticks="--ticks 1.5 0.3"
+                csiro_ticks="--ticks 1.25 0.25"
+                fgoals_ticks="--ticks 2.0 0.4"
+                gfdl_cm_ticks="--ticks 2.0 0.4"
+                gfdl_esm_ticks="--ticks 2.0 0.4"
+                gisseh_ticks="--ticks 1.5 0.3"
+                gisser_ticks="--ticks 1.5 0.3"
+                ipsl_ticks="--ticks 2.0 0.4"
+                noresm_ticks="--ticks 2.0 0.4"
                 palette='BrBG_r'
             elif [[ ${variable} == 'thetao' ]] ; then
                 long_name=sea_water_potential_temperature
@@ -245,7 +248,7 @@ for region in ${regions[@]}; do
 
             if [[ ${variable} == 'so' ]] ; then
                 long_name=sea_water_salinity
-                ticks="--ticks 3.0 0.5"
+                ticks="--ticks 2.4 0.4"
                 palette='BrBG_r'
             elif [[ ${variable} == 'thetao' ]] ; then
                 long_name=sea_water_potential_temperature
@@ -284,9 +287,9 @@ for region in ${regions[@]}; do
         climatology_files="${canesm_clim} ${ccsm_clim} ${csiro_clim} ${fgoals_clim} ${gfdl_cm_clim} ${gfdl_esm_clim} ${gisseh_clim} blank ${gisser_clim} blank ${ipsl_clim} ${noresm_clim}"
 
     if [[ ${dry_run} == 'yes' ]] ; then
-        echo  ${python} ~/climate-analysis/visualisation/plot_ocean_trend_ensemble.py ${data_files[@]} ${long_name} ${region} ${nrows} ${ncols} ${outfile} --palette ${palette} --climatology_files ${climatology_files[@]} ${ticks[@]} --scale_factor 3 --experiment historicalGHG --no_title --runs ${runs[@]}
+        echo  ${python} ~/climate-analysis/visualisation/plot_ocean_trend_ensemble.py ${data_files[@]} ${long_name} ${region} ${nrows} ${ncols} ${outfile} --palette ${palette} --climatology_files ${climatology_files[@]} ${ticks[@]} --scale_factor 3 --experiment historicalGHG  --runs ${runs[@]}
     else
-         ${python} ~/climate-analysis/visualisation/plot_ocean_trend_ensemble.py ${data_files[@]} ${long_name} ${region} ${nrows} ${ncols} ${outfile} --palette ${palette} --climatology_files ${climatology_files[@]} ${ticks[@]} --scale_factor 3 --experiment historicalGHG --no_title --runs ${runs[@]}
+         ${python} ~/climate-analysis/visualisation/plot_ocean_trend_ensemble.py ${data_files[@]} ${long_name} ${region} ${nrows} ${ncols} ${outfile} --palette ${palette} --climatology_files ${climatology_files[@]} ${ticks[@]} --scale_factor 3 --experiment historicalGHG  --runs ${runs[@]}
         echo ${outfile}
     fi
 
@@ -335,22 +338,22 @@ for region in ${regions[@]}; do
 
     for variable in ${variables[@]}; do
 
-        if [[ ${group} == 'ensemble' ]] ; then
+        if [[ ${group} == 'ensmean' ]] ; then
 
             if [[ ${variable} == 'so' ]] ; then
                 long_name=sea_water_salinity
-                canesm_ticks="--ticks 2.5 0.5"
-                ccsm_ticks="--ticks 2.0 0.4"
-                csiro_ticks="--ticks 2.5 0.5"
+                canesm_ticks="--ticks 2.0 0.4"
+                ccsm_ticks="--ticks 1.5 0.3"
+                csiro_ticks="--ticks 1.25 0.25"
                 fgoals_ticks="--ticks 2.0 0.4"
-                gfdl_cm_ticks="--ticks 3.75 0.75"
-                gfdl_esm_ticks="--ticks 5 1"
-                gisseh_p107_ticks="--ticks 1.5 0.3"
-                gisseh_p310_ticks="--ticks 1.5 0.3"
-                gisser_p107_ticks="--ticks 2.5 0.5"
-                gisser_p310_ticks="--ticks 2.5 0.5"
-                ipsl_ticks="--ticks 3.75 0.75"
-                noresm_ticks="--ticks 5 1"
+                gfdl_cm_ticks="--ticks 2.0 0.4"
+                gfdl_esm_ticks="--ticks 2.0 0.4"
+                gisseh_p1_ticks="--ticks 1.5 0.3"
+                gisseh_p3_ticks="--ticks 1.5 0.3"
+                gisser_p1_ticks="--ticks 1.5 0.3"
+                gisser_p3_ticks="--ticks 2.5 0.5"
+                ipsl_ticks="--ticks 2.0 0.4"
+                noresm_ticks="--ticks 2.0 0.4"
                 palette='BrBG_r'
             elif [[ ${variable} == 'thetao' ]] ; then
                 long_name=sea_water_potential_temperature
@@ -375,7 +378,7 @@ for region in ${regions[@]}; do
 
             if [[ ${variable} == 'so' ]] ; then
                 long_name=sea_water_salinity
-                ticks="--ticks 3.0 0.5"
+                ticks="--ticks 2.4 0.4"
                 palette='BrBG_r'
             elif [[ ${variable} == 'thetao' ]] ; then
                 long_name=sea_water_potential_temperature
@@ -418,9 +421,9 @@ for region in ${regions[@]}; do
         climatology_files="${canesm_clim} ${ccsm_clim} ${csiro_clim} ${fgoals_clim} ${gfdl_cm_clim} ${gfdl_esm_clim} ${gisseh_p107_clim} ${gisseh_p310_clim} ${gisser_p107_clim} ${gisser_p310_clim} ${ipsl_clim} ${noresm_clim}"
 
     if [[ ${dry_run} == 'yes' ]] ; then
-        echo  ${python} ~/climate-analysis/visualisation/plot_ocean_trend_ensemble.py ${data_files[@]} ${long_name} ${region} ${nrows} ${ncols} ${outfile} --palette ${palette} --climatology_files ${climatology_files[@]} ${ticks[@]} --scale_factor 3 --experiment historicalAA --no_title --runs ${runs[@]}
+        echo  ${python} ~/climate-analysis/visualisation/plot_ocean_trend_ensemble.py ${data_files[@]} ${long_name} ${region} ${nrows} ${ncols} ${outfile} --palette ${palette} --climatology_files ${climatology_files[@]} ${ticks[@]} --scale_factor 3 --experiment historicalAA  --runs ${runs[@]}
     else
-         ${python} ~/climate-analysis/visualisation/plot_ocean_trend_ensemble.py ${data_files[@]} ${long_name} ${region} ${nrows} ${ncols} ${outfile} --palette ${palette} --climatology_files ${climatology_files[@]} ${ticks[@]} --scale_factor 3 --experiment historicalAA --no_title --runs ${runs[@]}
+         ${python} ~/climate-analysis/visualisation/plot_ocean_trend_ensemble.py ${data_files[@]} ${long_name} ${region} ${nrows} ${ncols} ${outfile} --palette ${palette} --climatology_files ${climatology_files[@]} ${ticks[@]} --scale_factor 3 --experiment historicalAA  --runs ${runs[@]}
         echo ${outfile}
     fi
 
