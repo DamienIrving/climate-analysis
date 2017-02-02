ac27e1762aaf2f8bc334764bad86321bc981dc18
Damien Irving <irving.damien@gmail.com>
2017-02-03 09:15:23 +1100

    Plot now looks good

--- visualisation/plot_integrated_ocean_trend_ensemble.py
+++ visualisation/plot_integrated_ocean_trend_ensemble.py
@@ -44,6 +44,28 @@ experiments['historicalGHG'] = 'red'
 experiments['historicalAnt'] = 'purple'
                
 
+def fix_lats(lat_coord):
+    """Edit the latitude axis so that they can be merged."""
+
+    lat_coord.long_name = 'latitude'
+    lat_coord.standard_name = 'latitude'
+    lat_coord.var_name = 'lat'        
+    try:
+        lat_coord.coord_system = None    # iris.coord_systems.GeogCS(iris.fileformats.pp.EARTH_RADIUS)
+    except iris.exceptions.CoordinateNotFoundError:
+        pass
+
+
+def scale_data(cube):
+    """Scale a cube."""
+
+    metadata = cube.metadata
+    cube = cube * 10**3
+    cube.metadata = metadata
+
+    return cube
+
+
 def main(inargs):
     """Run the program."""
     
@@ -55,22 +77,26 @@ def main(inargs):
     cube_list = iris.cube.CubeList([])
     for index, filename in enumerate(inargs.infiles):
         cube = iris.load_cube(filename, var)
-        #print filename
-        #print cube.coords[coord.name() for coord in volume_cube.dim_coords]
+        cube = scale_data(cube)
         if index == 0:
-            iplt.plot(cube, color=color, alpha=0.4, label='individual models')
+            iplt.plot(cube, color=color, alpha=0.3, label='individual models, %s' %(inargs.experiment))
         else:
-            iplt.plot(cube, color=color, alpha=0.4)
+            iplt.plot(cube, color=color, alpha=0.3)
         new_cube = cube.interpolate(common_lats, iris.analysis.Linear())
         new_cube.add_aux_coord(iris.coords.DimCoord(index, 'realization'))
+        fix_lats(new_cube.coord('latitude'))
         cube_list.append(new_cube)
         
     equalise_attributes(cube_list)
     merged_cube = cube_list.merge_cube()
     ensemble_mean = merged_cube.collapsed('realization', iris.analysis.MEAN)
-    iplt.plot(ensemble_mean, color=color, label='ensemble mean')
+    iplt.plot(ensemble_mean, color=color, linewidth=3.0, label='ensemble mean, %s' %(inargs.experiment))
     
-    plt.legend()
+    plt.xlim(-70, 70)
+    plt.legend(loc=8)
+    plt.ylabel('1950-2000 trend ($10^{-3} \enspace K \enspace yr^{-1}$)')
+    plt.xlabel('latitude')
+    plt.title('Zonal mean, vertical mean sea water potential temperature')
 
     plt.savefig(inargs.outfile, bbox_inches='tight')
     gio.write_metadata(inargs.outfile, file_info={filename: cube.attributes['history']})
