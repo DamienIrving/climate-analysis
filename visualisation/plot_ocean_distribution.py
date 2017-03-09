a52472aad21d7b61fecf5784a0143ddb4f442ea7
Damien Irving <irving.damien@gmail.com>
2017-03-09 12:06:35 +1100

    Now plots GHG and AA side by side

--- visualisation/plot_ocean_distribution.py
+++ visualisation/plot_ocean_distribution.py
@@ -13,6 +13,7 @@ import numpy
 import iris
 from iris.experimental.equalise_cubes import equalise_attributes
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
 import seaborn
 
 # Import my modules
@@ -28,7 +29,6 @@ modules_dir = os.path.join(repo_dir, 'modules')
 sys.path.append(modules_dir)
 try:
     import general_io as gio
-    import kde
     import convenient_universal as uconv
 except ImportError:
     raise ImportError('Must run this script from anywhere within the climate-analysis git repo')
@@ -36,7 +36,10 @@ except ImportError:
 
 # Define functions
 
-history = [] 
+history = []
+colors = {'historicalGHG': 'red',
+          'historicalAA': 'blue'}
+ 
 
 def read_spatial_file(spatial_file):
     """Read a spatial file (i.e. area or volume file)."""
@@ -80,28 +83,8 @@ def concat_cubes(cube_list):
     return cube
 
 
-def get_plot_characteristics(period_name, experiment):
-    """Get the legend labels and transparency"""
-
-    assert period_name in ['early', 'late', 'only']
-    if period_name == 'late':
-        alpha = 1.0
-        histtype = 'step' 
-        label = experiment + ', late'
-    elif period_name == 'early':
-        alpha = 0.3
-        histtype = 'bar'
-        label = experiment + ', early'
-    elif period_name == 'only':
-        alpha = 0.3
-        histtype = 'bar'
-        label = experiment
-
-    return alpha, histtype, label
-
-
-def plot_distribution(data_cube, spatial_cube, period_name, experiment, color, plot_kde=False):
-    """Plot the spatial cube."""
+def calc_histogram(data_cube, spatial_cube):
+    """Calculate the histogram."""
 
     dim_coord_names = [coord.name() for coord in data_cube.dim_coords]
     assert dim_coord_names[0] == 'time'
@@ -117,45 +100,76 @@ def plot_distribution(data_cube, spatial_cube, period_name, experiment, color, p
 
     broadcast_spatial_data = uconv.broadcast_array(spatial_cube.data, axis_index, data_cube.shape)
 
-    alpha, histtype, label = get_plot_characteristics(period_name, experiment)
-    bins = numpy.arange(27, 41, 0.25)
-    plt.hist(data_cube.data.compressed(), weights=broadcast_spatial_data.compressed(),
-             bins=bins, normed=True,
-             color=color, label=label, alpha=alpha, histtype=histtype)
+    bin_edges = numpy.arange(25, 45, 0.2)
+    hist, bin_edges = numpy.histogram(data_cube.data.compressed(), bins=bin_edges, normed=True, weights=broadcast_spatial_data.compressed())
 
-    if plot_kde:
-        pdf = kde.gaussian_kde(data_cube.data.compressed(), weights=broadcast_spatial_data.compressed())
-        x = numpy.arange(27, 41, 0.25)
-        y = pdf(x)
-        plt.plot(x, y, color=color)
-    
-    return spatial_type
+    return hist, bin_edges
+
+
+def create_upper_plot(ax, hist_dict, left_edges, experiment):
+    """Create an upper panel plot (shows AA and GHG distributions)"""
+
+    ax.bar(left_edges, hist_dict[(experiment, 'early')],
+           width=0.2, color='blue', label='early', alpha=0.3)
+
+    ax.bar(left_edges, hist_dict[(experiment, 'late')],
+           width=0.2, color='red', label='late', alpha=0.3)
+
+    #ax.title = experiment
  
+
+def create_lower_plot(ax, hist_dict, left_edges, experiment):
+    """Create a lower panel plot (shows AA and GHG difference)""" 
+
+    ax.bar(left_edges, hist_dict[(experiment, 'diff')],
+           width=0.2, color='black', label='late - early', alpha=0.3)
+
 
 def main(inargs):
     """Run the program."""
 
-    for group_num, file_info in enumerate(inargs.file_group):
-        files = file_info[0:-2]
-        experiment = file_info[-2]
-        color = file_info[-1]
-        spatial_cube = read_spatial_file(inargs.spatial_files[group_num])
+    data_files = {'historicalGHG': inargs.ghg_files,
+                  'historicalAA': inargs.aa_files} 
+    spatial_files = {'historicalGHG': inargs.ghg_spatial_file,
+                     'historicalAA': inargs.aa_spatial_file}
+    time_periods = {'early': inargs.early_period,
+                    'late': inargs.late_period}
+
+    hist = {}
+    for experiment in ['historicalGHG', 'historicalAA']:
+        spatial_cube = read_spatial_file(spatial_files[experiment])
         with iris.FUTURE.context(cell_datetime_objects=True):
-            data_cubes = iris.load(files, inargs.var, callback=save_history)
+            data_cubes = iris.load(data_files[experiment], inargs.var, callback=save_history)
             equalise_attributes(data_cubes)
-            for period_info in inargs.period:
-                period_dates = period_info[0:2]
-                period_name = period_info[2]
+            for period_name, period_dates in time_periods.iteritems():
                 time_constraint = gio.get_time_constraint(period_dates)
                 data_cube_list = data_cubes.extract(time_constraint)
                 data_cube = concat_cubes(data_cube_list)
-                spatial_type = plot_distribution(data_cube, spatial_cube, period_name, experiment, color, plot_kde=inargs.kde)
+                hist[(experiment, period_name)], bin_edges = calc_histogram(data_cube, spatial_cube)
+        hist[(experiment, 'diff')] = hist[(experiment, 'late')] - hist[(experiment, 'early')]
 
-    plt.title('Salinity distribution')
-    plt.xlabel('Salinity (g/kg)')
-    plt.ylabel(spatial_type + ' density')
-    plt.legend()
-    plt.xlim(27, 41)
+    
+    fig = plt.figure(figsize=[15, 10])
+    gs = gridspec.GridSpec(2, 2, height_ratios=[4,1])
+
+    ax0 = plt.subplot(gs[0])
+    #plt.sca(ax0)
+    create_upper_plot(ax0, hist, bin_edges[0:-1], 'historicalAA')
+
+    ax1 = plt.subplot(gs[1])
+    create_upper_plot(ax1, hist, bin_edges[0:-1], 'historicalGHG')
+
+    ax2 = plt.subplot(gs[2])
+    create_lower_plot(ax2, hist, bin_edges[0:-1], 'historicalAA')
+
+    ax3 = plt.subplot(gs[3])
+    create_lower_plot(ax3, hist, bin_edges[0:-1], 'historicalGHG')
+
+#    plt.title('Salinity distribution')
+#    plt.xlabel('Salinity (g/kg)')
+#    plt.ylabel(spatial_type + ' density')
+#    plt.legend()
+#    plt.xlim(27, 41)
 
     plt.savefig(inargs.outfile, bbox_inches='tight')
     #write_met_file(inargs, spatial_cube, inargs.outfile)
@@ -179,15 +193,19 @@ author:
     parser.add_argument("outfile", type=str, help="Output file name")
     parser.add_argument("var", type=str, help="Input variable name (the standard_name)")
 
-    parser.add_argument("--file_group", type=str, nargs='*', action='append',
-                        help="Input data files, followed by experiment name and color")
-    parser.add_argument("--spatial_files", type=str, nargs='*', default=None, 
-                        help="Input volume files (or area files for surface variable)")
-    parser.add_argument("--period", type=str, nargs=3, action='append', metavar=('START_DATE', 'END_DATE', 'NAME'),
-                        help="Time bounds for a period (name can be early, late or only")
+    parser.add_argument("--aa_files", type=str, nargs='*',
+                        help="Input historicalAA data files")
+    parser.add_argument("--ghg_files", type=str, nargs='*',
+                        help="Input historicalGHG data files")
+    parser.add_argument("--aa_spatial_file", type=str, default=None, 
+                        help="Input historicalAA volume file (or area file for surface variable)")
+    parser.add_argument("--ghg_spatial_file", type=str, default=None, 
+                        help="Input historicalGHG volume file (or area file for surface variable)")
 
-    parser.add_argument("--kde", action="store_true", default=False,
-                        help="Plot the kernel density estimate")
+    parser.add_argument("--early_period", type=str, nargs=2, metavar=('START_DATE', 'END_DATE'), default=('1850-01-01', '1869-12-31'),
+                        help="Time bounds for the early period")
+    parser.add_argument("--late_period", type=str, nargs=2, metavar=('START_DATE', 'END_DATE'), default=('1986-01-01', '2005-12-31'),
+                        help="Time bounds for the late period")
 
     args = parser.parse_args()             
     main(args)
