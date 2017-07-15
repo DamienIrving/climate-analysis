37c6d4c557493815b9b8b6af0ebfc39e180aeec6
Damien Irving <irving.damien@gmail.com>
2017-07-15 20:36:28 +1000

    Bash script for wisconsin composite process

---
+++ downloads/wisconsin/wisconsin_composites.sh
@@ -0,0 +1,19 @@
+region=ellsworth
+title=Ellsworth_Land
+
+data_dir=/g/data/r87/dbi599/data_eraint/wisconsin_cmmt
+vis_dir=/home/599/dbi599/climate-analysis/visualisation
+processing_dir=/home/599/dbi599/climate-analysis/data_processing
+wisconsin_dir=/home/599/dbi599/climate-analysis/downloads/wisconsin
+python=/g/data/r87/dbi599/miniconda2/envs/wisconsin/bin/python
+
+${python} ${wisconsin_dir}/cmmt_date_list.py ${wisconsin_dir}/${region}_CMMTdatetimes.csv ${data_dir}/${region}_CMMTdatetimes.txt
+
+${python} ${processing_dir}/calc_composite.py ${data_dir}/sf_eraint_500hPa_daily-anom-wrt-all_native-reoriented.nc sf ${data_dir}/sf-composite_eraint_500hPa_daily-anom-wrt-all_native-reoriented_${region}.nc --date_file ${data_dir}/${region}_CMMTdatetimes.txt --no_sig
+
+${python} ${processing_dir}/calc_composite.py ${data_dir}/ua_eraint_500hPa_daily_native-reoriented.nc ua ${data_dir}/ua-composite_eraint_500hPa_daily_native-reoriented_${region}.nc --date_file ${data_dir}/${region}_CMMTdatetimes.txt --no_sig
+
+${python} ${processing_dir}/calc_composite.py ${data_dir}/va_eraint_500hPa_daily_native-reoriented.nc va ${data_dir}/va-composite_eraint_500hPa_daily_native-reoriented_${region}.nc --date_file ${data_dir}/${region}_CMMTdatetimes.txt --no_sig
+
+${python} ${vis_dir}/plot_map.py 3 2 --infile ${data_dir}/sf-composite_eraint_500hPa_daily-anom-wrt-all_native-reoriented_${region}.nc streamfunction_annual none none none contour0 1 PlateCarree --infile ${data_dir}/sf-composite_eraint_500hPa_daily-anom-wrt-all_native-reoriented_${region}.nc streamfunction_DJF none none none contour0 3 PlateCarree --infile ${data_dir}/sf-composite_eraint_500hPa_daily-anom-wrt-all_native-reoriented_${region}.nc streamfunction_MAM none none none contour0 4 PlateCarree --infile ${data_dir}/sf-composite_eraint_500hPa_daily-anom-wrt-all_native-reoriented_${region}.nc streamfunction_JJA none none none contour0 5 PlateCarree --infile ${data_dir}/sf-composite_eraint_500hPa_daily-anom-wrt-all_native-reoriented_${region}.nc streamfunction_SON none none none contour0 6 PlateCarree --output_projection SouthPolarStereo --subplot_headings Annual none DJF MAM JJA SON --infile ${data_dir}/ua-composite_eraint_500hPa_daily_native-reoriented_${region}.nc eastward_wind_annual none none none uwind0 1 PlateCarree --infile ${data_dir}/ua-composite_eraint_500hPa_daily_native-reoriented_${region}.nc eastward_wind_DJF none none none uwind0 3 PlateCarree --infile ${data_dir}/ua-composite_eraint_500hPa_daily_native-reoriented_${region}.nc eastward_wind_MAM none none none uwind0 4 PlateCarree --infile ${data_dir}/ua-composite_eraint_500hPa_daily_native-reoriented_${region}.nc eastward_wind_JJA none none none uwind0 5 PlateCarree --infile ${data_dir}/ua-composite_eraint_500hPa_daily_native-reoriented_${region}.nc eastward_wind_SON none none none uwind0 6 PlateCarree --figure_size 9 16 --infile ${data_dir}/va-composite_eraint_500hPa_daily_native-reoriented_${region}.nc northward_wind_annual none none none vwind0 1 PlateCarree --infile ${data_dir}/va-composite_eraint_500hPa_daily_native-reoriented_${region}.nc northward_wind_DJF none none none vwind0 3 PlateCarree --infile ${data_dir}/va-composite_eraint_500hPa_daily_native-reoriented_${region}.nc northward_wind_MAM none none none vwind0 4 PlateCarree --infile ${data_dir}/va-composite_eraint_500hPa_daily_native-reoriented_${region}.nc northward_wind_JJA none none none vwind0 5 PlateCarree --infile ${data_dir}/va-composite_eraint_500hPa_daily_native-reoriented_${region}.nc northward_wind_SON none none none vwind0 6 PlateCarree --flow_type streamlines --contour_levels -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 10 --exclude_blanks --streamline_colour 0.7 --title ${title} --ofile /g/data/r87/dbi599/figures/wisconsin/sf-composite_eraint_500hPa_daily-anom-wrt-all_native-reoriented_${region}.png
+
