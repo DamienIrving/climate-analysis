78ad24c803b082a3dab1f482b479d165bc30c0a1
Damien Irving <irving.damien@gmail.com>
2017-07-16 13:25:46 +1000

    Added more contours

--- downloads/wisconsin/wisconsin_composites.mk
+++ downloads/wisconsin/wisconsin_composites.mk
@@ -6,9 +6,9 @@
 #   make -n -B -f wisconsin_composites.mk  (-n is a dry run) (-B is a force make)
 #
 
-all : /g/data/r87/dbi599/figures/wisconsin/sf-composite_eraint_500hPa_daily-anom-wrt-all_native-reoriented_marie-byrd.png
-REGION=marie-byrd
-TITLE=Marie_Byrd_Land
+all : /g/data/r87/dbi599/figures/wisconsin/sf-composite_eraint_500hPa_daily-anom-wrt-all_native-reoriented_wilkes.png
+REGION=wilkes
+TITLE=Wilkes_Land
 
 DATA_DIR=/g/data/r87/dbi599/data_eraint/wisconsin_cmmt
 VIS_DIR=/home/599/dbi599/climate-analysis/visualisation
@@ -39,6 +39,6 @@ ${VA_COMPOSITE} : ${DATE_LIST}
 
 COMPOSITE_PLOT=/g/data/r87/dbi599/figures/wisconsin/sf-composite_eraint_500hPa_daily-anom-wrt-all_native-reoriented_${REGION}.png
 ${COMPOSITE_PLOT} : ${SF_COMPOSITE} ${UA_COMPOSITE} ${VA_COMPOSITE}
-	${PYTHON} ${VIS_DIR}/plot_map.py 3 2 --infile $(word 1,$^) streamfunction_annual none none none contour0 1 PlateCarree --infile $(word 1,$^) streamfunction_DJF none none none contour0 3 PlateCarree --infile $(word 1,$^) streamfunction_MAM none none none contour0 4 PlateCarree --infile $(word 1,$^) streamfunction_JJA none none none contour0 5 PlateCarree --infile $(word 1,$^) streamfunction_SON none none none contour0 6 PlateCarree --output_projection SouthPolarStereo --subplot_headings Annual none DJF MAM JJA SON --infile $(word 2,$^) eastward_wind_annual none none none uwind0 1 PlateCarree --infile $(word 2,$^) eastward_wind_DJF none none none uwind0 3 PlateCarree --infile $(word 2,$^) eastward_wind_MAM none none none uwind0 4 PlateCarree --infile $(word 2,$^) eastward_wind_JJA none none none uwind0 5 PlateCarree --infile $(word 2,$^) eastward_wind_SON none none none uwind0 6 PlateCarree --figure_size 9 16 --infile $(word 3,$^) northward_wind_annual none none none vwind0 1 PlateCarree --infile $(word 3,$^) northward_wind_DJF none none none vwind0 3 PlateCarree --infile $(word 3,$^) northward_wind_MAM none none none vwind0 4 PlateCarree --infile $(word 3,$^) northward_wind_JJA none none none vwind0 5 PlateCarree --infile $(word 3,$^) northward_wind_SON none none none vwind0 6 PlateCarree --flow_type streamlines --contour_levels -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 10 --exclude_blanks --streamline_colour 0.7 --title ${TITLE} --ofile ${COMPOSITE_PLOT}
+	${PYTHON} ${VIS_DIR}/plot_map.py 3 2 --infile $(word 1,$^) streamfunction_annual none none none contour0 1 PlateCarree --infile $(word 1,$^) streamfunction_DJF none none none contour0 3 PlateCarree --infile $(word 1,$^) streamfunction_MAM none none none contour0 4 PlateCarree --infile $(word 1,$^) streamfunction_JJA none none none contour0 5 PlateCarree --infile $(word 1,$^) streamfunction_SON none none none contour0 6 PlateCarree --output_projection SouthPolarStereo --subplot_headings Annual none DJF MAM JJA SON --infile $(word 2,$^) eastward_wind_annual none none none uwind0 1 PlateCarree --infile $(word 2,$^) eastward_wind_DJF none none none uwind0 3 PlateCarree --infile $(word 2,$^) eastward_wind_MAM none none none uwind0 4 PlateCarree --infile $(word 2,$^) eastward_wind_JJA none none none uwind0 5 PlateCarree --infile $(word 2,$^) eastward_wind_SON none none none uwind0 6 PlateCarree --figure_size 9 16 --infile $(word 3,$^) northward_wind_annual none none none vwind0 1 PlateCarree --infile $(word 3,$^) northward_wind_DJF none none none vwind0 3 PlateCarree --infile $(word 3,$^) northward_wind_MAM none none none vwind0 4 PlateCarree --infile $(word 3,$^) northward_wind_JJA none none none vwind0 5 PlateCarree --infile $(word 3,$^) northward_wind_SON none none none vwind0 6 PlateCarree --flow_type streamlines --contour_levels -20 -19 -18 -17 -16 -15 -14 -13 -12 -11 -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 --exclude_blanks --streamline_colour 0.7 --title ${TITLE} --ofile ${COMPOSITE_PLOT}
 
 
